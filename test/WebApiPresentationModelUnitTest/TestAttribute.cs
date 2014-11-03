dd97eb8933857eb0d4ac3067650e4ce2c47e3575
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-03 14:09:37 +0900

    TestAttribute 구현.

---
+++ test/WebApiPresentationModelUnitTest/TestAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Jwc.CIBuild
+{
+    using Experiment;
+    using Experiment.AutoFixture;
+    using Experiment.Xunit;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+
+    public class TestAttribute : TestBaseAttribute
+    {
+        protected override ITestFixture Create(ITestMethodContext context)
+        {
+            var customization = new CompositeCustomization(
+                new AutoMoqCustomization(),
+                new TestParametersCustomization(context.ActualMethod.GetParameters()));
+
+            return new TestFixture(new Fixture().Customize(customization));
+        }
+    }
+}
