081b657fdd9074b8dff62a092ae7106a5695f7c2
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-03 15:35:30 +0900

    ReadWritablePropertyAssertion 구현.

---
+++ test/DomainModelUnitTest/ReadWritablePropertyAssertion.cs
@@ -0,0 +1,34 @@
+﻿namespace DomainModelUnitTest
+{
+    using System;
+    using System.Reflection;
+    using Jwc.Experiment;
+    using Jwc.Experiment.Idioms;
+    using Xunit;
+
+    public class ReadWritablePropertyAssertion : IdiomaticAssertion
+    {
+        private readonly ITestFixture fixture;
+
+        public ReadWritablePropertyAssertion(ITestFixture fixture)
+        {
+            if (fixture == null)
+                throw new ArgumentNullException("fixture");
+
+            this.fixture = fixture;
+        }
+
+        public override void Verify(PropertyInfo property)
+        {
+            if (property == null)
+                throw new ArgumentNullException("property");
+
+            var value = fixture.Create(property.MemberType);
+            var target = fixture.Create(property.ReflectedType);
+            property.SetValue(target, value);
+            var atcual = property.GetValue(target);
+
+            Assert.Equal(value, atcual);
+        }
+    }
+}
