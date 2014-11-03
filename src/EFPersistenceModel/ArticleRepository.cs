8c696aac8d09176df6ecb98feb13f3fc2188d927
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-03 23:27:58 +0900

    ArticleRepository 생성.

---
+++ src/EFPersistenceModel/ArticleRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace EFPersistenceModel
+{
+    using DomainModel;
+
+    public class ArticleRepository : IArticleRepository
+    {
+        public void Insert(Article article)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/ArticleRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿namespace EFPersistenceModelUnitTest
+{
+    using DomainModel;
+    using EFPersistenceModel;
+    using Xunit;
+
+    public class ArticleRepositoryTest
+    {
+        [Test]
+        public void SutIsArticleRepository(ArticleRepository sut)
+        {
+            Assert.IsAssignableFrom<IArticleRepository>(sut);
+        } 
+    }
+}
