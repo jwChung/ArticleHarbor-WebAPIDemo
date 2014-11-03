020d9ed87b022ad09ad7b50ce9f267edcdca41e9
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-03 16:33:42 +0900

    test: 리팩토링.

--- test/DomainModelUnitTest/ArticleServiceTest.cs
+++ test/DomainModelUnitTest/ArticleServiceTest.cs
@@ -1,19 +1,16 @@
 ﻿namespace DomainModelUnitTest
 {
     using DomainModel;
-    using Ploeh.AutoFixture.Xunit;
 
     public class ArticleServiceTest : IdiomaticTest<ArticleService>
     {
         [Test]
         public void AddCorrectlyAddsArticle(
-            [Frozen] IArticleRepository repository,
             ArticleService sut,
             Article article)
         {
             sut.Add(article);
-
-            repository.ToMock().Verify(x => x.Insert(article));
+            sut.Repository.ToMock().Verify(x => x.Insert(article));
         }
     }
 }
