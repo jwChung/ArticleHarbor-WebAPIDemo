9701db383fe2563bcbc804c23d9376e31f4df0e9
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-03 23:07:32 +0900

    DatabaseContext 소개.

---
+++ src/EFPersistenceModel/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿namespace EFPersistenceModel
+{
+    using DomainModel;
+
+    public class DatabaseContext : IDatabaseContext
+    {
+        public IArticleRepository Articles
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/DatabaseContextTest.cs
@@ -0,0 +1,15 @@
+﻿namespace EFPersistenceModelUnitTest
+{
+    using DomainModel;
+    using EFPersistenceModel;
+    using Xunit;
+
+    public class DatabaseContextTest
+    {
+        [Test]
+        public void SutIsDatabaseContext(DatabaseContext sut)
+        {
+            Assert.IsAssignableFrom<IDatabaseContext>(sut);
+        }
+    }
+}
