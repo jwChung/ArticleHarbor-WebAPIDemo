815a25df3c1099079ffef19f15a51e3958dcfa5c
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 07:58:54 +0900

    Implemented LogContext class.

---
+++ src/WebApiPresentationModel/LogContext.cs
@@ -0,0 +1,50 @@
+﻿namespace WebApiPresentationModel
+{
+    using System;
+    using System.Globalization;
+
+    public class LogContext
+    {
+        private readonly string requestUrl;
+        private readonly string message;
+        private readonly DateTime date = DateTime.Now;
+
+        public LogContext(string requestUrl, string message)
+        {
+            if (requestUrl == null)
+                throw new ArgumentNullException("requestUrl");
+
+            if (message == null)
+                throw new ArgumentNullException("message");
+
+            this.requestUrl = requestUrl;
+            this.message = message;
+        }
+
+        public string RequestUrl
+        {
+            get { return this.requestUrl; }
+        }
+
+        public string Message
+        {
+            get { return this.message; }
+        }
+
+        public DateTime Date
+        {
+            get { return this.date; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                "Request URL: {1}{0}Date: {2}{0}Message:{0}{3}",
+                Environment.NewLine,
+                this.requestUrl,
+                this.date,
+                this.message);
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/LogContextTest.cs
@@ -0,0 +1,45 @@
+﻿namespace WebApiPresentationModelUnitTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading;
+    using WebApiPresentationModel;
+    using Xunit;
+
+    public class LogContextTest : IdiomaticTest<LogContext>
+    {
+        [Test]
+        public void DateReturnsNow(LogContext sut)
+        {
+            var actual = sut.Date;
+
+            TimeSpan timeSpane = DateTime.Now - actual;
+            Assert.True(timeSpane.Milliseconds < 10);
+        }
+
+        [Test]
+        public void DateAlwaysReturnsSameValue(LogContext sut)
+        {
+            var actual = sut.Date;
+            Thread.Sleep(10);
+            Assert.Equal(sut.Date, actual);
+        }
+
+        [Test]
+        public void ToStringReturnsCorrectMessage(LogContext sut)
+        {
+            var expected = "Request URL: " + sut.RequestUrl + Environment.NewLine
+                + "Date: " + sut.Date + Environment.NewLine
+                + "Message:" + Environment.NewLine + sut.Message;
+
+            var actual = sut.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Date);
+        }
+    }
+}
