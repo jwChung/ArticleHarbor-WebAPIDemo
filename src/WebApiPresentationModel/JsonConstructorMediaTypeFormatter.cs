ac961bfac3baff377664c9ee28d78d6d143c3630
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 12:45:59 +0900

    Introduced JsonConstructorMediaTypeFormatter.

---
+++ src/WebApiPresentationModel/JsonConstructorMediaTypeFormatter.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiPresentationModel
+{
+    using System.Net.Http.Formatting;
+
+    public class JsonConstructorMediaTypeFormatter : MediaTypeFormatter
+    {
+        public override bool CanReadType(System.Type type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool CanWriteType(System.Type type)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- test/WebApiPresentationModelUnitTest/JsonConstructorMediaTypeFormatterTest.cs
+++ test/WebApiPresentationModelUnitTest/JsonConstructorMediaTypeFormatterTest.cs
@@ -1,6 +1,16 @@
 ﻿namespace WebApiPresentationModelUnitTest
 {
+    using System.Net.Http.Formatting;
+    using WebApiPresentationModel;
+    using Xunit;
+
     public class JsonConstructorMediaTypeFormatterTest
     {
+        [Test]
+        public void SubIsMediaTypeFormatter(
+            JsonConstructorMediaTypeFormatter sut)
+        {
+            Assert.IsAssignableFrom<MediaTypeFormatter>(sut);
+        }
     }
 }
