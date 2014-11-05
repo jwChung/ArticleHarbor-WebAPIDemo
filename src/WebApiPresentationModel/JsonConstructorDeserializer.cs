2cc832ad38bbf476cab58b6adbdb4f2f321e57bb
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 22:02:39 +0900

    Implemented JsonConstructorDeserializer.

--- src/WebApiPresentationModel/JsonConstructorDeserializer.cs
+++ src/WebApiPresentationModel/JsonConstructorDeserializer.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Globalization;
     using System.Linq;
     using System.Reflection;
     using Newtonsoft.Json;
@@ -10,88 +11,237 @@
 
     public static class JsonConstructorDeserializer
     {
+        private static readonly DeserializerContext context = new DeserializerContext(
+            new JsonObjectDeserializer(),
+            new JsonValueDeserializer(),
+            new JsonArrayDeserializer(),
+            new JsonEnumerableDeserializer(),
+            new JsonListDeserializer(),
+            new JsonIListDeserializer(),
+            new JsonCollectionDeserializer());
+
         public static object Deserialize(Type type, string json)
         {
             if (type == null)
                 throw new ArgumentNullException("type");
 
-            return new JsonConstructorDeserializerImplementor(
-                type, GetJsonDictionary(json)).Read();
+            return context.GetValue(
+                type,
+                JsonConvert.DeserializeObject<JToken>(json));
         }
 
-        private static IDictionary<string, JToken> GetJsonDictionary(string json)
+        private class DeserializerContext
         {
-            var jsonObject = new Dictionary<string, JToken>();
+            public static readonly object None = new object();
+            private readonly IDeserializer[] deserializers;
 
-            foreach (KeyValuePair<string, JToken> keyValuePair
-                in JsonConvert.DeserializeObject<JObject>(json))
-                jsonObject[keyValuePair.Key.ToUpper()] = keyValuePair.Value;
-
-            return jsonObject;
-        }
-
-        private class JsonConstructorDeserializerImplementor
-        {
-            private readonly Type type;
-            private readonly IDictionary<string, JToken> jsonObject;
-            
-            public JsonConstructorDeserializerImplementor(
-                Type type, IDictionary<string, JToken> jsonObject)
+            public DeserializerContext(params IDeserializer[] deserializers)
             {
-                this.type = type;
-                this.jsonObject = jsonObject;
+                this.deserializers = deserializers;
             }
 
-            public object Read()
+            public object GetValue(Type type, JToken token)
             {
-                var constructor = this.GetConstructor();
-                var arguments = this.GetArguments(constructor.GetParameters());
-                var result = constructor.Invoke(arguments.ToArray());
-                this.SetProperties(result);
-                return result;
-            }
-
-            private static object GetValue(Type type, JToken token)
-            {
-                var converter = TypeDescriptor.GetConverter(type);
-                return converter.CanConvertFrom(typeof(string))
-                    ? converter.ConvertFromString(token.Value<string>())
-                    : null;
-            }
-
-            private ConstructorInfo GetConstructor()
-            {
-                return this.type.GetConstructors()
-                    .OrderByDescending(c => c.GetParameters().Length)
-                    .Where(c => c.GetParameters()
-                        .Select(p => p.Name.ToUpper())
-                        .All(n => this.jsonObject.ContainsKey(n)))
-                    .First();
-            }
-
-            private IEnumerable<object> GetArguments(ParameterInfo[] parameters)
-            {
-                foreach (var parameter in parameters)
+                foreach (var deserializer in this.deserializers)
                 {
-                    var key = parameter.Name.ToUpper();
-                    var token = this.jsonObject[key];
-                    this.jsonObject.Remove(key);
-
-                    yield return GetValue(parameter.ParameterType, token);
+                    var value = deserializer.Deserialize(type, token, this);
+                    if (value != None)
+                        return value;
                 }
+
+                throw new NotSupportedException(string.Format(
+                    CultureInfo.CurrentCulture,
+                    "There is no deserializer for the type '{0}'.",
+                    type));
+            }
+        }
+
+        private interface IDeserializer
+        {
+            object Deserialize(Type type, JToken token, DeserializerContext context);
+        }
+
+        private class JsonValueDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                var val = token as JValue;
+                if (val == null)
+                    return DeserializerContext.None;
+
+                if (!TypeDescriptor.GetConverter(type).CanConvertFrom(typeof(string)))
+                    return null;
+
+                return this.GetType().GetMethod("Value").MakeGenericMethod(type)
+                    .Invoke(this, new object[] { val });
             }
 
-            private void SetProperties(object result)
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "To support reflection")]
+            public object Value<T>(JValue val)
             {
-                foreach (var property in this.type.GetProperties()
-                    .Where(x => x.GetGetMethod() != null && x.GetSetMethod() != null))
-                {
-                    JToken token;
-                    if (!this.jsonObject.TryGetValue(property.Name.ToUpper(), out token))
-                        continue;
+                return val.Value<T>();
+            }
+        }
 
-                    var value = GetValue(property.PropertyType, token);
-                    property.SetValue(result, value);
+        private class JsonArrayDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                var arr = token as JArray;
+                if (arr == null || !type.IsArray)
+                    return DeserializerContext.None;
+
+                var jarr = arr.ToArray();
+                var elementType = type.GetElementType();
+                var array = Array.CreateInstance(elementType, jarr.Length);
+
+                for (int i = 0; i < jarr.Length; i++)
+                    array.SetValue(context.GetValue(elementType, jarr[i]), i);
+
+                return array;
+            }
+        }
+
+        private class JsonEnumerableDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                if (type.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+                    return DeserializerContext.None;
+
+                var arrayType = type.GetGenericArguments()[0].MakeArrayType();
+                return new JsonArrayDeserializer().Deserialize(arrayType, token, context);
+            }
+        }
+
+        private class JsonListDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                if (type.GetGenericTypeDefinition() != typeof(List<>))
+                    return DeserializerContext.None;
+
+                var elementType = type.GetGenericArguments()[0];
+                var enumerableType = typeof(IEnumerable<>).MakeGenericType(elementType);
+                var enumerable = new JsonEnumerableDeserializer()
+                    .Deserialize(enumerableType, token, context);
+                return type.GetConstructor(new Type[] { enumerableType })
+                    .Invoke(new object[] { enumerable });
+            }
+        }
+
+        private class JsonIListDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                if (type.GetGenericTypeDefinition() != typeof(IList<>))
+                    return DeserializerContext.None;
+
+                var listType = typeof(List<>).MakeGenericType(type.GetGenericArguments()[0]);
+                return new JsonListDeserializer().Deserialize(listType, token, context);
+            }
+        }
+
+        private class JsonCollectionDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                if (type.GetGenericTypeDefinition() != typeof(ICollection<>))
+                    return DeserializerContext.None;
+
+                var listType = typeof(List<>).MakeGenericType(type.GetGenericArguments()[0]);
+                return new JsonListDeserializer().Deserialize(listType, token, context);
+            }
+        }
+
+        private class JsonObjectDeserializer : IDeserializer
+        {
+            public object Deserialize(Type type, JToken token, DeserializerContext context)
+            {
+                var obj = token as JObject;
+                if (obj == null)
+                    return DeserializerContext.None;
+
+                return new InnerJsonObjectDeserializer(
+                    type, GetJsonObject(obj), context).Deserialize();
+            }
+
+            private static IDictionary<string, JToken> GetJsonObject(JObject obj)
+            {
+                var jsonObject = new Dictionary<string, JToken>();
+                foreach (KeyValuePair<string, JToken> pair in obj)
+                    jsonObject[pair.Key.ToUpper(CultureInfo.CurrentCulture)] = pair.Value;
+
+                return jsonObject;
+            }
+
+            private class InnerJsonObjectDeserializer
+            {
+                private readonly Type type;
+                private readonly IDictionary<string, JToken> jsonObject;
+                private readonly DeserializerContext context;
+
+                public InnerJsonObjectDeserializer(
+                    Type type,
+                    IDictionary<string, JToken> jsonObject,
+                    DeserializerContext context)
+                {
+                    this.type = type;
+                    this.jsonObject = jsonObject;
+                    this.context = context;
+                }
+
+                public object Deserialize()
+                {
+                    var constructor = this.GetConstructor();
+                    var arguments = this.GetArguments(constructor.GetParameters());
+                    var result = constructor.Invoke(arguments.ToArray());
+                    this.SetProperties(result);
+                    return result;
+                }
+
+                private ConstructorInfo GetConstructor()
+                {
+                    var constructor = this.type.GetConstructors()
+                        .OrderByDescending(c => c.GetParameters().Length)
+                        .Where(c => c.GetParameters()
+                            .Select(p => p.Name.ToUpper(CultureInfo.CurrentCulture))
+                            .All(n => this.jsonObject.ContainsKey(n)))
+                        .FirstOrDefault();
+
+                    if (constructor == null)
+                        throw new ArgumentException("There is no constructor matched with the given jason.");
+
+                    return constructor;
+                }
+
+                private IEnumerable<object> GetArguments(ParameterInfo[] parameters)
+                {
+                    foreach (var parameter in parameters)
+                    {
+                        var key = parameter.Name.ToUpper();
+                        var token = this.jsonObject[key];
+                        this.jsonObject.Remove(key);
+
+                        yield return this.context.GetValue(parameter.ParameterType, token);
+                    }
+                }
+
+                private void SetProperties(object target)
+                {
+                    var properties = this.type.GetProperties()
+                        .Where(x => x.GetGetMethod() != null && x.GetSetMethod() != null);
+
+                    foreach (var property in properties)
+                    {
+                        JToken token;
+                        if (!this.jsonObject.TryGetValue(
+                            property.Name.ToUpper(CultureInfo.CurrentCulture), out token))
+                            continue;
+
+                        var value = this.context.GetValue(property.PropertyType, token);
+                        property.SetValue(target, value);
+                    }
                 }
             }
         }
--- test/WebApiPresentationModelUnitTest/JsonConstructorDeserializerTest.cs
+++ test/WebApiPresentationModelUnitTest/JsonConstructorDeserializerTest.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Jwc.Experiment;
     using Jwc.Experiment.Idioms;
     using Jwc.Experiment.Xunit;
@@ -21,67 +22,184 @@
         }
 
         [Test]
-        public void DeserializeReturnsCorrectResultUsingModestConstructor(Foo foo)
+        public void DeserializeReturnsCorrectResultUsingModestConstructor(TypeA typeA)
         {
-            var json = JsonConvert.SerializeObject(foo);
+            var json = JsonConvert.SerializeObject(typeA);
             
-            var actual = (Foo)JsonConstructorDeserializer.Deserialize(typeof(Foo), json);
+            var actual = (TypeA)JsonConstructorDeserializer.Deserialize(typeof(TypeA), json);
 
-            Assert.Equal(foo.Name, actual.Name);
+            Assert.Equal(typeA.Name, actual.Name);
             Assert.Equal(-1, actual.Age);
-            Assert.Equal(foo.Date, actual.Date);
+            Assert.Equal(typeA.Value, actual.Value);
         }
 
         [Test]
-        public void DeserializeReturnsCorrectResultUsingGreedyConstructor([Greedy] Foo foo)
+        public void DeserializeReturnsCorrectResultUsingGreedyConstructor([Greedy] TypeA typeA)
         {
-            var json = JsonConvert.SerializeObject(foo);
+            var json = JsonConvert.SerializeObject(typeA);
 
-            var actual = (Foo)JsonConstructorDeserializer.Deserialize(typeof(Foo), json);
+            var actual = (TypeA)JsonConstructorDeserializer.Deserialize(typeof(TypeA), json);
 
-            Assert.Equal(foo.Age, actual.Age);
-            Assert.Equal(foo.Name, actual.Name);
-            Assert.Equal(foo.Date, actual.Date);
+            Assert.Equal(typeA.Age, actual.Age);
+            Assert.Equal(typeA.Name, actual.Name);
+            Assert.Equal(typeA.Value, actual.Value);
         }
 
         [Test]
-        public void DeserializeDoesNotUseSameValueTwice(Bar bar)
+        public void DeserializeDoesNotUseSameValueTwice(TypeB typeB)
         {
-            var json = JsonConvert.SerializeObject(bar);
+            var json = JsonConvert.SerializeObject(typeB);
 
-            var actual = (Bar)JsonConstructorDeserializer.Deserialize(typeof(Bar), json);
+            var actual = (TypeB)JsonConstructorDeserializer.Deserialize(typeof(TypeB), json);
 
             Assert.Null(actual.Name);
-            Assert.Equal(bar.Name, actual.Name2);
+            Assert.Equal(typeB.Name, actual.Name2);
         }
 
         [Test]
-        public void DeserializeSetsDefaultValueToCtoArgumentWhenTypeCannotBeConvertedFromString(Baz baz)
+        public void DeserializeSetsDefaultValueToCtorArgumentWhenTypeCannotBeConvertedFromString(TypeC typeC)
         {
-            var json = JsonConvert.SerializeObject(baz);
-            var actual = (Baz)JsonConstructorDeserializer.Deserialize(typeof(Baz), json);
-            Assert.Null(actual.Type);
+            var json = JsonConvert.SerializeObject(typeC);
+            var actual = (TypeC)JsonConstructorDeserializer.Deserialize(typeof(TypeC), json);
+            Assert.Null(actual.TargetType);
         }
 
         [Test]
-        public void DeserializeSetsDefaultValueToPropertyWhenTypeCannotBeConvertedFromString(Abc baz)
+        public void DeserializeSetsDefaultValueToPropertyWhenTypeCannotBeConvertedFromString(TypeD typeD)
         {
-            var json = JsonConvert.SerializeObject(baz);
-            var actual = (Abc)JsonConstructorDeserializer.Deserialize(typeof(Abc), json);
-            Assert.Null(actual.Type);
+            var json = JsonConvert.SerializeObject(typeD);
+            var actual = (TypeD)JsonConstructorDeserializer.Deserialize(typeof(TypeD), json);
+            Assert.Null(actual.TargetType);
         }
 
-        public class Foo
+        [Test]
+        public void DeserializeCorrectlyDeserializesRecursiveObject(TypeE typeE)
+        {
+            var json = JsonConvert.SerializeObject(typeE);
+
+            var actual = (TypeE)JsonConstructorDeserializer.Deserialize(typeof(TypeE), json);
+
+            Assert.Equal(typeE.Value, actual.Value);
+            Assert.Equal(typeE.TypeA.Name, actual.TypeA.Name);
+            Assert.Equal(-1, actual.TypeA.Age);
+        }
+
+        [Test]
+        public void DeserializeArrayReturnsCorrectResult(TypeA[] typeAs)
+        {
+            var json = JsonConvert.SerializeObject(typeAs);
+
+            var actual = (TypeA[])JsonConstructorDeserializer.Deserialize(typeof(TypeA[]), json);
+
+            Assert.Equal(typeAs.Length, actual.Length);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.Equal(typeAs[i].Name, actual[i].Name);
+                Assert.Equal(-1, actual[i].Age);
+                Assert.Equal(typeAs[i].Value, actual[i].Value);    
+            }
+        }
+
+        [Test]
+        public void DeserializeEnumerableReturnsCorrectResult(IEnumerable<TypeA> typeA)
+        {
+            var json = JsonConvert.SerializeObject(typeA);
+
+            var actual = ((IEnumerable<TypeA>)JsonConstructorDeserializer.Deserialize(
+                typeof(IEnumerable<TypeA>), json)).ToArray();
+
+            var fooArray = typeA.ToArray();
+            Assert.Equal(fooArray.Length, actual.Length);
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.Equal(fooArray[i].Name, actual[i].Name);
+                Assert.Equal(-1, actual[i].Age);
+                Assert.Equal(fooArray[i].Value, actual[i].Value);
+            }
+        }
+
+        [Test]
+        public void DeserializeListReturnsCorrectResult(List<TypeA> typeA)
+        {
+            var json = JsonConvert.SerializeObject(typeA);
+
+            var actual = (List<TypeA>)JsonConstructorDeserializer.Deserialize(typeof(List<TypeA>), json);
+
+            Assert.Equal(typeA.Count, actual.Count);
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.Equal(typeA[i].Name, actual[i].Name);
+                Assert.Equal(-1, actual[i].Age);
+                Assert.Equal(typeA[i].Value, actual[i].Value);
+            }
+        }
+
+        [Test]
+        public void DeserializeIListReturnsCorrectResult(IList<TypeA> typeA)
+        {
+            var json = JsonConvert.SerializeObject(typeA);
+
+            var actual = (IList<TypeA>)JsonConstructorDeserializer.Deserialize(typeof(IList<TypeA>), json);
+
+            Assert.Equal(typeA.Count, actual.Count);
+            for (int i = 0; i < actual.Count; i++)
+            {
+                Assert.Equal(typeA[i].Name, actual[i].Name);
+                Assert.Equal(-1, actual[i].Age);
+                Assert.Equal(typeA[i].Value, actual[i].Value);
+            }
+        }
+
+        [Test]
+        public void DeserializeRecursiveArrayReturnsCorrectResult(TypeA[][] typeAs)
+        {
+            var json = JsonConvert.SerializeObject(typeAs);
+            
+            var actual = (TypeA[][])JsonConstructorDeserializer.Deserialize(typeof(TypeA[][]), json);
+
+            Assert.NotNull(actual);
+            foreach (TypeA[] arry in actual)
+                Assert.NotNull(arry);
+        }
+
+        [Test]
+        public void DeserializeCorrectionReturnsCorrectResult(ICollection<TypeA> typeA)
+        {
+            var json = JsonConvert.SerializeObject(typeA);
+            var actual = (ICollection<TypeA>)JsonConstructorDeserializer
+                .Deserialize(typeof(ICollection<TypeA>), json);
+            Assert.Equal(typeA.Count, actual.Count);
+        }
+
+        [Test]
+        public void DeserializeWithInvalidTypeThrows(TypeA typeA)
+        {
+            var json = JsonConvert.SerializeObject(typeA);
+
+            Assert.Throws<ArgumentException>(
+                () => JsonConstructorDeserializer.Deserialize(typeof(TypeE), json));
+        }
+
+        [Test]
+        public void DeserializeWithInvalidArrayTypeThrows(TypeA[] typeAs)
+        {
+            var json = JsonConvert.SerializeObject(typeAs);
+
+            Assert.Throws<NotSupportedException>(
+                () => JsonConstructorDeserializer.Deserialize(typeof(Dictionary<string, TypeA>), json));
+        }
+
+        public class TypeA
         {
             private readonly int age = -1;
             private readonly string name;
 
-            public Foo(string name)
+            public TypeA(string name)
             {
                 this.name = name;
             }
 
-            public Foo(int age, string name)
+            public TypeA(int age, string name)
             {
                 this.age = age;
                 this.name = name;
@@ -97,14 +215,14 @@
                 get { return this.age; }
             }
 
-            public int Date { get; set; }
+            public int Value { get; set; }
         }
 
-        public class Bar
+        public class TypeB
         {
             private string name;
 
-            public Bar(string name)
+            public TypeB(string name)
             {
                 this.name = name;
             }
@@ -124,27 +242,49 @@
             }
         }
 
-        public class Baz
+        public class TypeC
         {
-            private readonly Type type;
+            private readonly Type targetType;
 
-            public Baz(Type type)
+            public TypeC(Type targetType)
             {
-                this.type = type;
+                this.targetType = targetType;
             }
 
-            public Type Type
+            public Type TargetType
             {
-                get { return this.type; }
+                get { return this.targetType; }
             }
         }
 
-        public class Abc
+        public class TypeD
         {
-            public Type Type
+            public Type TargetType
             {
                 get;
                 set;
+            }
+        }
+
+        public class TypeE
+        {
+            private readonly string value;
+            private readonly TypeA typeA;
+
+            public TypeE(string value, TypeA typeA)
+            {
+                this.value = value;
+                this.typeA = typeA;
+            }
+
+            public string Value
+            {
+                get { return this.value; }
+            }
+
+            public TypeA TypeA
+            {
+                get { return this.typeA; }
             }
         }
     }
