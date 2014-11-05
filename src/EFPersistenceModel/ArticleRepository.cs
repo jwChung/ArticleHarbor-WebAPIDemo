7ae04cdb2b81f73124993fed154b48a80042f724
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-06 00:25:22 +0900

    Introduced the new ArticleRepository.

---
+++ src/EFPersistenceModel/ArticleRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace EFPersistenceModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DomainModel;
+    using EFDataAccess;
+
+    public class ArticleRepository : IArticleRepository
+    {
+        private readonly ArticleHarborContext context;
+
+        public ArticleRepository(ArticleHarborContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            this.context = context;
+        }
+
+        public ArticleHarborContext Context
+        {
+            get { return this.context; }
+        }
+
+        public Task<IEnumerable<Article>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> SelectAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Article Insert(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/ArticleRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿namespace EFPersistenceModelUnitTest
+{
+    using DomainModel;
+    using EFPersistenceModel;
+    using Xunit;
+
+    public class ArticleRepositoryTest : IdiomaticTest<ArticleRepository>
+    {
+        [Test]
+        public void SutIsArticleRepository(ArticleRepository sut)
+        {
+            Assert.IsAssignableFrom<IArticleRepository>(sut);
+        }
+    }
+}
