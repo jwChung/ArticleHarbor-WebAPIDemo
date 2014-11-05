7d97264785b8a45e12d51a982c518b3d1cf1b99f
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 22:15:46 +0900

    Introduced IArticleService.

---
+++ src/DomainModel/IArticleService.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainModel
+{
+    using System.Collections.Generic;
+
+    public interface IArticleService
+    {
+        IEnumerable<Article> Get();
+
+        Article AddOrModify(Article article);
+
+        Article Remove(Article article);
+    }
+}
