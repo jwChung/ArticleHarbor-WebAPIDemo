c68f4e9abf41f2230b3791690139e94c4acdfb77
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 10:21:25 +0900

    Implemented acceptance tests for logging.

---
+++ test/AcceptanceTest/ThrowControllerTest.cs
@@ -0,0 +1,46 @@
+﻿#if !CI
+namespace AcceptanceTest
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Http.Results;
+    using Xunit;
+
+    public class ThrowControllerTest
+    {
+        [Test]
+        public async Task ThrowArgumentExceptionDoesNotLog()
+        {
+            using (var client = HttpClientFactory.Create())
+            {
+                var response = await client.GetAsync("api/throw/ArgumentException");
+                Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+                Assert.False(Directory.GetFiles(Environment.CurrentDirectory, "*.log").Any());
+            }
+        }
+
+        [Test]
+        public async Task ThrowNonArgumentExceptionCorrectlyLogs()
+        {
+            try
+            {
+                using (var client = HttpClientFactory.Create())
+                {
+                    var response = await client.GetAsync("api/throw/InvalidOperationException");
+                    Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+                    Assert.DoesNotThrow(
+                        () => Directory.GetFiles(Environment.CurrentDirectory, "*.log").Single());
+                }
+            }
+            finally
+            {
+                foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, "*.log"))
+                    File.Delete(file);
+            }
+        }
+    }
+}
+#endif
