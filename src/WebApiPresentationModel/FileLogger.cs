a0513b6de4abe34debf41231ae54853c0a56c9c5
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 09:23:03 +0900

    Introduced FileLogger.

---
+++ src/WebApiPresentationModel/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApiPresentationModel
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class FileLogger : ILogger
+    {
+        public Task LogAsync(LogContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/FileLoggerTest.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiPresentationModelUnitTest
+{
+    using WebApiPresentationModel;
+    using Xunit;
+
+    public class FileLoggerTest : IdiomaticTest<FileLogger>
+    {
+        [Test]
+        public void SutIsLogger(FileLogger sut)
+        {
+            Assert.IsAssignableFrom<ILogger>(sut);
+        }
+    }
+}
