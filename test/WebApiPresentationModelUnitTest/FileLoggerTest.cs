95df2b180570c409ce109220b0a3d46fc8249022
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-05 09:48:11 +0900

    Implemented FileLogger.

--- test/WebApiPresentationModelUnitTest/FileLoggerTest.cs
+++ test/WebApiPresentationModelUnitTest/FileLoggerTest.cs
@@ -1,5 +1,10 @@
 ﻿namespace WebApiPresentationModelUnitTest
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Ploeh.AutoFixture;
     using WebApiPresentationModel;
     using Xunit;
 
@@ -9,6 +14,26 @@
         public void SutIsLogger(FileLogger sut)
         {
             Assert.IsAssignableFrom<ILogger>(sut);
+        }
+
+        [Test]
+        public async Task LogAsyncCollectlyLogs(LogContext context, IFixture fixture)
+        {
+            try
+            {
+                fixture.Inject<string>(Environment.CurrentDirectory);
+                var sut = fixture.Create<FileLogger>();
+
+                await sut.LogAsync(context);
+
+                var files = Directory.GetFiles(Environment.CurrentDirectory, "*.log");
+                Assert.Equal(File.ReadAllText(files.Single()), context.ToString());
+            }
+            finally
+            {
+                foreach (var file in Directory.GetFiles(Environment.CurrentDirectory, "*.log"))
+                    File.Delete(file);
+            }
         }
     }
 }
