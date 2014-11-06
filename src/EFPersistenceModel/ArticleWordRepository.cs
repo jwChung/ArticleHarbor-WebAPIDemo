0bb238e8f71d893d19150bc8ef23858a21d4b2db
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-06 13:57:06 +0900

    Introduced ArticleWordRepository.

---
+++ src/EFPersistenceModel/ArticleWordRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace EFPersistenceModel
+{
+    using System;
+    using DomainModel;
+    using EFDataAccess;
+
+    public class ArticleWordRepository : IArticleWordRepository
+    {
+        private readonly ArticleHarborContext context;
+
+        public ArticleWordRepository(ArticleHarborContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            this.context = context;
+        }
+
+        public ArticleHarborContext Context
+        {
+            get { return this.context; }
+        }
+
+        public void Insert(ArticleWord articleWord)
+        {
+            if (articleWord == null)
+                throw new ArgumentNullException("articleWord");
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/ArticleWordRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿namespace EFPersistenceModelUnitTest
+{
+    using DomainModel;
+    using EFPersistenceModel;
+    using Xunit;
+
+    public class ArticleWordRepositoryTest : IdiomaticTest<ArticleWordRepository>
+    {
+        [Test]
+        public void SutIsArticleWordRepository(ArticleWordRepository sut)
+        {
+            Assert.IsAssignableFrom<IArticleWordRepository>(sut);
+        }
+    }
+}
