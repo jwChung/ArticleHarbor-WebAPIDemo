1cc56776af0d78ec61ee01fb9bf827204e51f296
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-06 14:11:53 +0900

    Rectified key constraints on EFArticleWord.

--- src/EFDataAccess/EFArticleWord.cs
+++ src/EFDataAccess/EFArticleWord.cs
@@ -1,12 +1,16 @@
 ﻿namespace EFDataAccess
 {
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class EFArticleWord
     {
         [Key]
+        [Column(Order = 0)]
         public string Word { get; set; }
 
+        [Key]
+        [Column(Order = 1)]
         public int EFArticleId { get; set; }
 
         public virtual EFArticle EFArticle { get; set; }
