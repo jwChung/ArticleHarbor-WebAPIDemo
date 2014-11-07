4b572c801ebffa1530c83d08ffa0dfd9bc01e9aa
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-07 21:39:22 +0900

    Implemented SaveUnitOfWorkActionFilter.

--- src/WebApiPresentationModel/SaveUnitOfWorkActionFilter.cs
+++ src/WebApiPresentationModel/SaveUnitOfWorkActionFilter.cs
@@ -1,6 +1,7 @@
 ﻿namespace WebApiPresentationModel
 {
     using System;
+    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Web.Http.Filters;
@@ -13,6 +14,13 @@
         {
             if (actionExecutedContext == null)
                 throw new ArgumentNullException("actionExecutedContext");
+
+            var dependencyScope = actionExecutedContext.Request.GetDependencyScope();
+            var lazyUnitOfWork = (LazyUnitOfWork)dependencyScope
+                .GetService(typeof(LazyUnitOfWork));
+
+            if (lazyUnitOfWork.Optional != null)
+                lazyUnitOfWork.Optional.SaveAsync();
 
             return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
         }
--- test/WebApiPresentationModelUnitTest/SaveUnitOfWorkActionFilterTest.cs
+++ test/WebApiPresentationModelUnitTest/SaveUnitOfWorkActionFilterTest.cs
@@ -1,6 +1,15 @@
 ﻿namespace WebApiPresentationModelUnitTest
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http.Dependencies;
     using System.Web.Http.Filters;
+    using System.Web.Http.Hosting;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Xunit;
     using WebApiPresentationModel;
     using Xunit;
 
@@ -11,6 +20,39 @@
             SaveUnitOfWorkActionFilter sut)
         {
             Assert.IsAssignableFrom<ActionFilterAttribute>(sut);
+        }
+
+        [Test]
+        public async Task OnActionExecutedAsyncSavesUnitOfWorkWhenUnitOfWorkWasConstructedInActionMethod(
+            SaveUnitOfWorkActionFilter sut,
+            HttpActionExecutedContext actionExecutedContext,
+            IDependencyScope depedencyScope,
+            LazyUnitOfWork layUnitOfWork)
+        {
+            var unitOfWork = layUnitOfWork.Value;
+            depedencyScope.Of(x => x.GetService(typeof(LazyUnitOfWork)) == layUnitOfWork);
+            actionExecutedContext.Request.Properties[HttpPropertyKeys.DependencyScope]
+                = depedencyScope;
+
+            await sut.OnActionExecutedAsync(actionExecutedContext, CancellationToken.None);
+
+            unitOfWork.ToMock().Verify(x => x.SaveAsync());
+        }
+
+        [Test]
+        public async Task OnActionExecutedAsyncDoesNotSaveUnitOfWorkWhenUnitOfWorkWasNotConstructedInActionMethod(
+            SaveUnitOfWorkActionFilter sut,
+            HttpActionExecutedContext actionExecutedContext,
+            IDependencyScope depedencyScope,
+            LazyUnitOfWork layUnitOfWork)
+        {
+            depedencyScope.Of(x => x.GetService(typeof(LazyUnitOfWork)) == layUnitOfWork);
+            actionExecutedContext.Request.Properties[HttpPropertyKeys.DependencyScope]
+                = depedencyScope;
+
+            await sut.OnActionExecutedAsync(actionExecutedContext, CancellationToken.None);
+
+            layUnitOfWork.UnitOfWorkFactory().ToMock().Verify(x => x.SaveAsync(), Times.Never());
         }
     }
 }
