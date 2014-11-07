35d030bfcf460a29f23c8e19f718ff594aa8da70
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-07 21:06:05 +0900

    Introduced SaveUnitOfWorkActionFilter.

---
+++ src/WebApiPresentationModel/SaveUnitOfWorkActionFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiPresentationModel
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http.Filters;
+
+    public class SaveUnitOfWorkActionFilter : ActionFilterAttribute
+    {
+        public override Task OnActionExecutedAsync(
+            HttpActionExecutedContext actionExecutedContext,
+            CancellationToken cancellationToken)
+        {
+            if (actionExecutedContext == null)
+                throw new ArgumentNullException("actionExecutedContext");
+
+            return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/SaveUnitOfWorkActionFilterTest.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApiPresentationModelUnitTest
+{
+    using System.Web.Http.Filters;
+    using WebApiPresentationModel;
+    using Xunit;
+
+    public class SaveUnitOfWorkActionFilterTest : IdiomaticTest<SaveUnitOfWorkActionFilter>
+    {
+        [Test]
+        public void SutIsActionFilterAttribute(
+            SaveUnitOfWorkActionFilter sut)
+        {
+            Assert.IsAssignableFrom<ActionFilterAttribute>(sut);
+        }
+    }
+}
