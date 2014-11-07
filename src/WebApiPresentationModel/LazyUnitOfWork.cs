8023c6c37e4140663f6303c7497e8e3484c1507e
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-07 20:33:57 +0900

    Implemented LazyUnitOfWork.

---
+++ src/WebApiPresentationModel/LazyUnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApiPresentationModel
+{
+    using System;
+    using DomainModel;
+
+    public class LazyUnitOfWork
+    {
+        private readonly Func<IUnitOfWork> unitOfWorkFactory;
+        private IUnitOfWork unitOfWork;
+
+        public LazyUnitOfWork(Func<IUnitOfWork> unitOfWorkFactory)
+        {
+            if (unitOfWorkFactory == null)
+                throw new ArgumentNullException("unitOfWorkFactory");
+
+            this.unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public Func<IUnitOfWork> UnitOfWorkFactory
+        {
+            get { return this.unitOfWorkFactory; }
+        }
+
+        public IUnitOfWork Value
+        {
+            get
+            {
+                if (this.unitOfWork == null)
+                    this.unitOfWork = this.unitOfWorkFactory();
+
+                return this.unitOfWork;
+            }
+        }
+
+        public IUnitOfWork Optional
+        {
+            get { return this.unitOfWork; }
+        }
+    }
+}
