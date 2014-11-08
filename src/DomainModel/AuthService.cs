fcccc1c0ed2fbb4b6ae3e0b21e000d3cf1b100c5
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 21:20:58 +0900

    Introduced AuthService.

---
+++ src/DomainModel/AuthService.cs
@@ -0,0 +1,24 @@
+﻿namespace DomainModel
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class AuthService : IAuthService
+    {
+        public Task<User> FindUserAsync(string id, string password)
+        {
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/AuthServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace DomainModelUnitTest
+{
+    using DomainModel;
+    using Xunit;
+
+    public class AuthServiceTest : IdiomaticTest<AuthService>
+    {
+        [Test]
+        public void SutIsAuthService(
+            AuthService sut)
+        {
+            Assert.IsAssignableFrom<IAuthService>(sut);
+        }
+    }
+}
