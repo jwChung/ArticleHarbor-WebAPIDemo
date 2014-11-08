07dedb31eadd9bdbb3ef6014795a0686c5f49559
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 18:12:17 +0900

    Introduced User.

---
+++ src/DomainModel/User.cs
@@ -0,0 +1,32 @@
+﻿namespace DomainModel
+{
+    using System;
+
+    public class User
+    {
+        private readonly string id;
+        private readonly string password;
+
+        public User(string id, string password)
+        {
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            this.id = id;
+            this.password = password;
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+        }
+    }
+}
