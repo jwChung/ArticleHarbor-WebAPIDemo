4340b0640af1c03b428014e0ec204c85f86f2188
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 21:35:20 +0900

    Introduced the Owned property.

--- src/DomainModel/AuthService.cs
+++ src/DomainModel/AuthService.cs
@@ -6,18 +6,28 @@
     public class AuthService : IAuthService
     {
         private readonly IUserRepository users;
+        private readonly IDisposable owned;
 
-        public AuthService(IUserRepository users)
+        public AuthService(IUserRepository users, IDisposable owned)
         {
             if (users == null)
                 throw new ArgumentNullException("users");
 
+            if (owned == null)
+                throw new ArgumentNullException("owned");
+
             this.users = users;
+            this.owned = owned;
         }
 
         public IUserRepository Users
         {
             get { return this.users; }
+        }
+
+        public IDisposable Owned
+        {
+            get { return this.owned; }
         }
 
         public Task<User> FindUserAsync(string id, string password)
