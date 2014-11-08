4ba4ecafe6bb9d48b62bf92e9305a215737cdeb9
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 23:19:56 +0900

    Introduced UserRepository.

---
+++ src/EFPersistenceModel/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace EFPersistenceModel
+{
+    using System;
+    using System.Threading.Tasks;
+    using DomainModel;
+
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> SelectAsync(string id, string password)
+        {
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/UserRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿namespace EFPersistenceModelUnitTest
+{
+    using DomainModel;
+    using EFPersistenceModel;
+    using Xunit;
+
+    public class UserRepositoryTest : IdiomaticTest<UserRepository>
+    {
+        [Test]
+        public void SutIsUserRepository(
+            UserRepository sut)
+        {
+            Assert.IsAssignableFrom<IUserRepository>(sut);
+        }
+    }
+}
