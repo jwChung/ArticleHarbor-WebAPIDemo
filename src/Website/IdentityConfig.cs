47ef1ea44329129de33da7d91d2fb266e76464bd
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 00:07:47 +0900

    Implemented IdentityConfig.

---
+++ src/Website/IdentityConfig.cs
@@ -0,0 +1,33 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(Website.IdentityConfig))]
+
+namespace Website
+{
+    using System;
+    using DomainModel;
+    using EFDataAccess;
+    using EFPersistenceModel;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security.OAuth;
+    using Owin;
+    using WebApiPresentationModel;
+
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Func<IAuthService> authServiceFactory = () =>
+            {
+                var context = new ArticleHarborDbContext(
+                    new ArticleHarborDbContextTestInitializer());
+                return new AuthService(new UserRepository(context), context);
+            };
+
+            app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions
+            {
+                Provider = new ArticleHarborAuthProvider(authServiceFactory),
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Authenticate")
+            });
+        }
+    }
+}
