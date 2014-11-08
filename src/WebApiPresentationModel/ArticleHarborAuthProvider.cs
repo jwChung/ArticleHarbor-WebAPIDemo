0835c90746a47ea235356da87c96424b2693c77b
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 15:13:00 +0900

    Introduced ArticleHarborAuthProvider.

---
+++ src/WebApiPresentationModel/ArticleHarborAuthProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiPresentationModel
+{
+    using Microsoft.Owin.Security.OAuth;
+
+    public class ArticleHarborAuthProvider : OAuthAuthorizationServerProvider
+    {
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/ArticleHarborAuthProviderTest.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApiPresentationModelUnitTest
+{
+    using Microsoft.Owin.Security.OAuth;
+    using WebApiPresentationModel;
+    using Xunit;
+
+    public class ArticleHarborAuthProviderTest : IdiomaticTest<ArticleHarborAuthProvider>
+    {
+        [Test]
+        public void SutIsOAuthAuthorizationServerProvider(
+            ArticleHarborAuthProvider sut)
+        {
+            Assert.IsAssignableFrom<OAuthAuthorizationServerProvider>(sut);
+        }
+    }
+}
