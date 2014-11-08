02a4ead098d5cf698303b66044a0c0326186be17
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-08 18:19:36 +0900

    Introduced RoleTypes.

---
+++ src/DomainModel/RoleTypes.cs
@@ -0,0 +1,28 @@
+﻿namespace DomainModel
+{
+    using System;
+
+    [Flags]
+    public enum RoleTypes
+    {
+        /// <summary>
+        /// The none
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The user
+        /// </summary>
+        User = 1,
+
+        /// <summary>
+        /// The author
+        /// </summary>
+        Author = User + 2,
+
+        /// <summary>
+        /// The administrator
+        /// </summary>
+        Administrator = Author + 4
+    }
+}
