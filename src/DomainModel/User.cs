2337097b5a6cdab6dc60fdbcca831f491fe3dc65
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-10 08:23:59 +0900

    Introduced the ApiKey property.

--- src/DomainModel/User.cs
+++ src/DomainModel/User.cs
@@ -6,14 +6,19 @@
     {
         private readonly string id;
         private readonly Role role;
+        private readonly Guid apiKey;
 
-        public User(string id, Role role)
+        public User(string id, Role role, Guid apiKey)
         {
             if (id == null)
                 throw new ArgumentNullException("id");
 
+            if (apiKey == Guid.Empty)
+                throw new ArgumentException("The api-key should not be empty guid.");
+
             this.id = id;
             this.role = role;
+            this.apiKey = apiKey;
         }
 
         public string Id
@@ -24,6 +29,11 @@
         public Role Role
         {
             get { return this.role; }
+        }
+
+        public Guid ApiKey
+        {
+            get { return this.apiKey; }
         }
     }
 }
