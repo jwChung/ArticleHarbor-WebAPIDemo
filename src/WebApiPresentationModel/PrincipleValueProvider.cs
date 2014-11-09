f5078cddc54051b3d89df4027c16434662a29d7a
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 19:08:56 +0900

    Introduced PrincipleValueProvider.

---
+++ src/WebApiPresentationModel/PrincipleValueProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System;
+    using System.Web.Http.ValueProviders;
+
+    public class PrincipleValueProvider : IValueProvider
+    {
+        public bool ContainsPrefix(string prefix)
+        {
+            if (prefix == null)
+                throw new ArgumentNullException("prefix");
+
+            throw new System.NotImplementedException();
+        }
+
+        public ValueProviderResult GetValue(string key)
+        {
+            if (key == null)
+                throw new ArgumentNullException("key");
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/PrincipleValueProviderTest.cs
@@ -0,0 +1,15 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Web.Http.ValueProviders;
+    using Xunit;
+
+    public class PrincipleValueProviderTest : IdiomaticTest<PrincipleValueProvider>
+    {
+        [Test]
+        public void SutIsValueProvider(
+            PrincipleValueProvider sut)
+        {
+            Assert.IsAssignableFrom<IValueProvider>(sut);
+        }
+    }
+}
