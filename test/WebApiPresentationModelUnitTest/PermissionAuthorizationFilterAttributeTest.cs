e092494476294e8d9eb424545aa16969615b17fb
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 15:40:27 +0900

    Introduced PermissionAuthorizationFilterAttribute.

---
+++ test/WebApiPresentationModelUnitTest/PermissionAuthorizationFilterAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Web.Http.Filters;
+    using Xunit;
+
+    public class PermissionAuthorizationFilterAttributeTest
+        : IdiomaticTest<PermissionAuthorizationFilterAttribute>
+    {
+        [Test]
+        public void SutIsAuthorizationAttribute(
+            PermissionAuthorizationFilterAttribute sut)
+        {
+            Assert.IsAssignableFrom<AuthorizationFilterAttribute>(sut);
+        }
+    }
+}
