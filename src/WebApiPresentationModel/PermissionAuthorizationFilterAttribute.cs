875db3548463d6926b36fd42161de1b82381b1de
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 16:45:35 +0900

    Implemented PermissionAuthorizationFilterAttribute.

--- src/WebApiPresentationModel/PermissionAuthorizationFilterAttribute.cs
+++ src/WebApiPresentationModel/PermissionAuthorizationFilterAttribute.cs
@@ -1,5 +1,12 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel
 {
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Security.Principal;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http.Controllers;
     using System.Web.Http.Filters;
     using DomainModel;
 
@@ -15,6 +22,38 @@
         public UserPermissions Permissions
         {
             get { return this.permissions; }
+        }
+
+        public override Task OnAuthorizationAsync(
+            HttpActionContext context, CancellationToken cancellationToken)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            IPrincipal principal = context.RequestContext.Principal;
+
+            if (principal == null || !this.HasPermissions(principal))
+                context.Response = context.Request.CreateResponse(HttpStatusCode.Unauthorized);
+
+            return base.OnAuthorizationAsync(context, cancellationToken);
+        }
+
+        private static Role GetRole(IPrincipal principal)
+        {
+            foreach (var role in Enum.GetNames(typeof(Role)))
+            {
+                if (principal.IsInRole(role))
+                    return (Role)Enum.Parse(typeof(Role), role);
+            }
+
+            throw new ArgumentOutOfRangeException("principal", "The role name is not valid.");
+        }
+
+        private bool HasPermissions(IPrincipal principal)
+        {
+            var permissions = (Role)(int)this.Permissions;
+            Role role = GetRole(principal);
+            return role.HasFlag(permissions);
         }
     }
 }
