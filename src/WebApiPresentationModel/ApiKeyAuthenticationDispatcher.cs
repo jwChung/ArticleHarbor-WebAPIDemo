bc3d6dba3ca5087b4af4a9f74b3db95d869b11d3
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 23:50:16 +0900

    Implemented the AuthServiceFactory property.

--- src/WebApiPresentationModel/ApiKeyAuthenticationDispatcher.cs
+++ src/WebApiPresentationModel/ApiKeyAuthenticationDispatcher.cs
@@ -1,8 +1,24 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel
 {
+    using System;
     using System.Net.Http;
+    using DomainModel;
 
     public class ApiKeyAuthenticationDispatcher : DelegatingHandler
     {
+        private readonly Func<IAuthService> authServiceFactory;
+
+        public ApiKeyAuthenticationDispatcher(Func<IAuthService> authServiceFactory)
+        {
+            if (authServiceFactory == null)
+                throw new ArgumentNullException("authServiceFactory");
+
+            this.authServiceFactory = authServiceFactory;
+        }
+
+        public Func<IAuthService> AuthServiceFactory
+        {
+            get { return this.authServiceFactory; }
+        }
     }
 }
