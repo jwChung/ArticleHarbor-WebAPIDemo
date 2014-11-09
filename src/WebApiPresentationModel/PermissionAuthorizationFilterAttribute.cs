6995408de1cd5f56957127291475d2265800e858
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 15:41:20 +0900

    Introduced the Permissions property.

--- src/WebApiPresentationModel/PermissionAuthorizationFilterAttribute.cs
+++ src/WebApiPresentationModel/PermissionAuthorizationFilterAttribute.cs
@@ -1,8 +1,20 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel
 {
     using System.Web.Http.Filters;
+    using DomainModel;
 
     public class PermissionAuthorizationFilterAttribute : AuthorizationFilterAttribute
     {
+        private readonly UserPermissions permissions;
+
+        public PermissionAuthorizationFilterAttribute(UserPermissions permissions)
+        {
+            this.permissions = permissions;
+        }
+
+        public UserPermissions Permissions
+        {
+            get { return this.permissions; }
+        }
     }
 }
