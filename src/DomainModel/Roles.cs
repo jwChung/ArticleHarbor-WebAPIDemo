9a70f5ec5cef11e4e1f9dc88d161152a0a0e8706
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 15:06:57 +0900

    Introduced Role and Permissions enums.

--- src/DomainModel/Roles.cs
+++ src/DomainModel/Roles.cs
@@ -25,4 +25,51 @@
         /// </summary>
         Administrator = 4
     }
+
+    public enum Role
+    {
+        /// <summary>
+        /// The user
+        /// </summary>
+        User = Permissions.Authentication,
+
+        /// <summary>
+        /// The author
+        /// </summary>
+        Author = User | Permissions.WriteArticle | Permissions.DeleteOwnArticle,
+
+        /// <summary>
+        /// The administrator
+        /// </summary>
+        Administrator = Author | Permissions.DeleteAnyArticle
+    }
+
+    [Flags]
+    public enum Permissions
+    {
+        /// <summary>
+        /// The none
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The authentication permission
+        /// </summary>
+        Authentication = 1,
+
+        /// <summary>
+        /// The write article permission
+        /// </summary>
+        WriteArticle = 2,
+
+        /// <summary>
+        /// The delete own article permission
+        /// </summary>
+        DeleteOwnArticle = 4,
+
+        /// <summary>
+        /// The delete any article permission
+        /// </summary>
+        DeleteAnyArticle = 8,
+    }
 }
