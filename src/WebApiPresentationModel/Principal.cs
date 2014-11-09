4702a4b4d13aa5e452f28a3dbf30c2935d06709f
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-10 00:19:51 +0900

    Implemented Principal.

---
+++ src/WebApiPresentationModel/Principal.cs
@@ -0,0 +1,60 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System;
+    using System.Security.Principal;
+    using DomainModel;
+
+    public class Principal : IPrincipal, IIdentity
+    {
+        private readonly string userId;
+        private readonly Role role;
+
+        public Principal(string userId, Role role)
+        {
+            if (userId == null)
+                throw new ArgumentNullException("userId");
+
+            this.userId = userId;
+            this.role = role;
+        }
+
+        public string UserId
+        {
+            get { return this.userId; }
+        }
+
+        public Role Role
+        {
+            get { return this.role; }
+        }
+
+        public IIdentity Identity
+        {
+            get { return this; }
+        }
+
+        public string AuthenticationType
+        {
+            get { return "ApiKey"; }
+        }
+
+        public bool IsAuthenticated
+        {
+            get { return true; }
+        }
+
+        public string Name
+        {
+            get { return this.userId; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            if (role == null)
+                throw new ArgumentNullException("role");
+
+            return this.role.ToString().Equals(
+                role, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/PrincipalTest.cs
@@ -0,0 +1,78 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Security.Principal;
+    using DomainModel;
+    using Ploeh.AutoFixture.Xunit;
+    using Xunit;
+
+    public class PrincipalTest : IdiomaticTest<Principal>
+    {
+        [Test]
+        public void SutIsPrincipal(Principal sut)
+        {
+            Assert.IsAssignableFrom<IPrincipal>(sut);
+        }
+
+        [Test]
+        public void SutIsIdentity(Principal sut)
+        {
+            Assert.IsAssignableFrom<IIdentity>(sut);
+        }
+
+        [Test]
+        public void IndentityIsCorrect(Principal sut)
+        {
+            var actual = sut.Identity;
+            Assert.Equal(actual, sut);
+        }
+
+        [Test]
+        public void IsAuthenticatedIsCorrect(Principal sut)
+        {
+            Assert.True(sut.IsAuthenticated);
+        }
+
+        [Test]
+        public void AuthenticationTypeIsCorrect(Principal sut)
+        {
+            var actual = sut.AuthenticationType;
+            Assert.Equal("ApiKey", actual);
+        }
+
+        [Test]
+        public void IsInRoleWithMatchedRoleNameReturnsTrue(
+            [Frozen] Role role,
+            Principal sut)
+        {
+            var actual = sut.IsInRole(role.ToString());
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void IsInRoleWithNotMatchedRoleNameReturnsFalse(
+            Principal sut,
+            string roleName)
+        {
+            var actual = sut.IsInRole(roleName);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void IsInRoleIsCaseInsenstive(
+            [Frozen] Role role,
+            Principal sut)
+        {
+            var actual = sut.IsInRole(role.ToString().ToLower());
+            Assert.True(actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.AuthenticationType);
+            yield return this.Properties.Select(x => x.IsAuthenticated);
+            yield return this.Properties.Select(x => x.Identity);
+        }
+    }
+}
