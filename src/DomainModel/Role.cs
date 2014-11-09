4edcb13a1afd1c5e170d5f94d568fff3deb70a61
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 15:23:28 +0900

    Rearranged to accept file per type.

---
+++ src/DomainModel/Role.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel
+{
+    public enum Role
+    {
+        /// <summary>
+        /// The user
+        /// </summary>
+        User = Permissions.Authentication,
+
+        /// <summary>
+        /// The author
+        /// </summary>
+        Author = User | Permissions.WriteArticle | Permissions.DeleteOwnArticle,
+
+        /// <summary>
+        /// The administrator
+        /// </summary>
+        Administrator = Author | Permissions.DeleteAnyArticle
+    }
+}
