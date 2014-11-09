b4289b2ca1f741842f4203a7114c0688eebe3a70
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-09 19:23:34 +0900

    Introduced PrincipleValueProviderFactory.

---
+++ test/WebApiPresentationModelUnitTest/PrincipleValueProviderFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Web.Http.ValueProviders;
+    using Xunit;
+
+    public class PrincipleValueProviderFactoryTest : IdiomaticTest<PrincipleValueProviderFactory>
+    {
+        [Test]
+        public void SutIsValueProviderFactory(PrincipleValueProviderFactory sut)
+        {
+            Assert.IsAssignableFrom<ValueProviderFactory>(sut);
+        }
+
+        [Test]
+        public void SutIsUriValueProviderFactory(PrincipleValueProviderFactory sut)
+        {
+            Assert.IsAssignableFrom<IUriValueProviderFactory>(sut);
+        }
+    }
+}
