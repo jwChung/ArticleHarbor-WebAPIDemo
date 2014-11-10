1ab7506c4f0676263201b338914bf3968571fa76
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-10 09:09:07 +0900

    Addressed not running integration tests on CI.

---
+++ test/AcceptanceTest/TestAttribute.cs
@@ -0,0 +1,58 @@
+﻿namespace ArticleHarbor
+{
+    using Jwc.Experiment;
+    using Jwc.Experiment.AutoFixture;
+    using Jwc.Experiment.Xunit;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+
+    public class TestAttribute : TestBaseAttribute
+    {
+        private readonly RunOn runOn;
+
+        public TestAttribute()
+            : this(RunOn.Local)
+        {
+        }
+
+        public TestAttribute(RunOn runOn)
+        {
+            this.runOn = runOn;
+        }
+
+        public RunOn RunOn
+        {
+            get { return this.runOn; }
+        }
+
+        public override string Skip
+        {
+            get
+            {
+#if !CI
+                if (base.Skip == null && this.runOn == RunOn.CI)
+                    return "Run this test only on CI server.";
+#else
+            if (base.Skip == null && this.runOn == RunOn.Local)
+                return "Run this test only on Local machine.";
+#endif
+
+                return base.Skip;
+            }
+
+            set
+            {
+                base.Skip = value;
+            }
+        }
+
+        protected override ITestFixture Create(ITestMethodContext context)
+        {
+            var customization = new CompositeCustomization(
+                new AutoMoqCustomization(),
+                new TestParametersCustomization(context.ActualMethod.GetParameters()));
+
+            return new TestFixture(new Fixture().Customize(customization));
+        }
+    }
+}
