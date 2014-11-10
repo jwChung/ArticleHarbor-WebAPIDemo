33c5962a0f23d258fb14de90754a64f132af8dcc
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-10 21:50:39 +0900

    Introduced AuthRepositoryProvider.

---
+++ src/DomainModel/AuthRepositoryProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace ArticleHarbor.DomainModel
+{
+    using System;
+
+    public class AuthRepositoryProvider<T> : IAuthRepositoryProvider<T>
+    {
+        public IRepository<T> GetRepository(string userId)
+        {
+            if (userId == null)
+                throw new ArgumentNullException("userId");
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/AuthRepositoryProviderTest.cs
@@ -0,0 +1,17 @@
+﻿namespace ArticleHarbor.DomainModel
+{
+    using Xunit;
+
+    public abstract class AuthRepositoryProviderTest<T> : IdiomaticTest<AuthRepositoryProvider<T>>
+    {
+        [Test]
+        public void SutIsAuthRepositoryProvider(AuthRepositoryProvider<T> sut)
+        {
+            Assert.IsAssignableFrom<IAuthRepositoryProvider<Article>>(sut);
+        }
+    }
+
+    public class AuthArticleRepositoryProviderTest : AuthRepositoryProviderTest<Article>
+    {
+    }
+}
