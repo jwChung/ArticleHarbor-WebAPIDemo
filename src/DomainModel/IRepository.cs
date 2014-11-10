5da84ba4a0d04fc787ad6aaad22a1c0cf36c484c
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-10 16:25:56 +0900

    Introduced IRepository.

---
+++ src/DomainModel/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace ArticleHarbor.DomainModel
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IRepository<T>
+    {
+        Task<Article> FineAsync(params object[] identity);
+
+        Task<IEnumerable<T>> SelectAsync();
+
+        Task<Article> InsertAsync(T article);
+
+        Task UpdateAsync(T article);
+
+        Task DeleteAsync(params object[] identity);
+    }
+}
