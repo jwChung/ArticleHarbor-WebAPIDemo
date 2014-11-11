10d4342a1f15584b174aa229aeff2e82fdc25e1a
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 10:04:47 +0900

    Created PostArticleViewModel.

---
+++ src/WebApiPresentationModel/Models/PostArticleViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel.Models
+{
+    using System;
+
+    public class PostArticleViewModel
+    {
+        public string Provider { get; set; }
+
+        public string No { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Url { get; set; }
+    }
+}
