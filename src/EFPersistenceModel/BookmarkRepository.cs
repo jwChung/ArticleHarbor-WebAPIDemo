65eaba2b46de91c1c871de08f696ef17b6dfecec
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 01:12:28 +0900

    Introduced the BookmarkRepository class.

---
+++ src/EFPersistenceModel/BookmarkRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DomainModel.Models;
+    using DomainModel.Repositories;
+
+    public class BookmarkRepository : IBookmarkRepository
+    {
+        public Task<IEnumerable<Bookmark>> SelectAsync(string userId)
+        {
+            if (userId == null)
+                throw new ArgumentNullException("userId");
+
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(Bookmark bookmark)
+        {
+            if (bookmark == null)
+                throw new ArgumentNullException("bookmark");
+
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Bookmark bookmark)
+        {
+            if (bookmark == null)
+                throw new ArgumentNullException("bookmark");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/BookmarkRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using DomainModel.Repositories;
+    using Xunit;
+
+    public class BookmarkRepositoryTest : IdiomaticTest<BookmarkRepository>
+    {
+        [Test]
+        public void SutIsBookmarkRepository(BookmarkRepository sut)
+        {
+            Assert.IsAssignableFrom<IBookmarkRepository>(sut);
+        }
+    }
+}
