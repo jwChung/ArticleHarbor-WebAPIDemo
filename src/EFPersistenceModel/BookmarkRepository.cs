ff74de24e678feceb97a7a7225bfcde01ed67747
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 01:31:43 +0900

    Introduced the Context property.

--- src/EFPersistenceModel/BookmarkRepository.cs
+++ src/EFPersistenceModel/BookmarkRepository.cs
@@ -5,11 +5,27 @@
     using System.Threading.Tasks;
     using DomainModel.Models;
     using DomainModel.Repositories;
+    using EFDataAccess;
     using DomainBookmark = DomainModel.Models.Bookmark;
     using PersistenceBookmark = EFDataAccess.Bookmark;
 
     public class BookmarkRepository : IBookmarkRepository
     {
+        private readonly ArticleHarborDbContext context;
+
+        public BookmarkRepository(ArticleHarborDbContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            this.context = context;
+        }
+
+        public ArticleHarborDbContext Context
+        {
+            get { return this.context; }
+        }
+
         public Task<IEnumerable<DomainBookmark>> SelectAsync(string userId)
         {
             if (userId == null)
