62efce425777cf9d8c8990672d97ff9c47104f02
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 12:01:22 +0900

    Introduced the InnerService property.

--- src/DomainModel/AuthArticleService.cs
+++ src/DomainModel/AuthArticleService.cs
@@ -6,6 +6,21 @@
 
     public class AuthArticleService : IArticleService
     {
+        private readonly IArticleService innerService;
+
+        public AuthArticleService(IArticleService innerService)
+        {
+            if (innerService == null)
+                throw new ArgumentNullException("innerService");
+
+            this.innerService = innerService;
+        }
+
+        public IArticleService InnerService
+        {
+            get { return this.innerService; }
+        }
+
         public Task<IEnumerable<Article>> GetAsync()
         {
             throw new NotImplementedException();
@@ -13,16 +28,28 @@
 
         public Task<Article> AddAsync(Article article)
         {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
             throw new NotImplementedException();
         }
 
         public Task ModifyAsync(string actor, Article article)
         {
+            if (actor == null)
+                throw new ArgumentNullException("actor");
+
+            if (article == null)
+                throw new ArgumentNullException("article");
+
             throw new NotImplementedException();
         }
 
         public Task RemoveAsync(string actor, int id)
         {
+            if (actor == null)
+                throw new ArgumentNullException("actor");
+
             throw new NotImplementedException();
         }
 
@@ -33,6 +60,9 @@
 
         public Task<Article> SaveAsync(Article article)
         {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
             throw new NotSupportedException();
         }
 
