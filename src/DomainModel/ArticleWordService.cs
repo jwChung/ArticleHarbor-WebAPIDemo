a658d5eecbd5288daeed4312fed2653e2bcc4c4e
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 14:07:48 +0900

    Implemented the AddWordsAsync method.

--- src/DomainModel/ArticleWordService.cs
+++ src/DomainModel/ArticleWordService.cs
@@ -1,18 +1,26 @@
 ﻿namespace ArticleHarbor.DomainModel
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public class ArticleWordService : IArticleWordService
     {
         private readonly IArticleWordRepository articleWords;
+        private readonly Func<string, IEnumerable<string>> nounExtractor;
 
-        public ArticleWordService(IArticleWordRepository articleWords)
+        public ArticleWordService(
+            IArticleWordRepository articleWords, Func<string, IEnumerable<string>> nounExtractor)
         {
             if (articleWords == null)
                 throw new ArgumentNullException("articleWords");
 
+            if (nounExtractor == null)
+                throw new ArgumentNullException("nounExtractor");
+
             this.articleWords = articleWords;
+            this.nounExtractor = nounExtractor;
         }
 
         public IArticleWordRepository ArticleWords
@@ -20,12 +28,17 @@
             get { return this.articleWords; }
         }
 
+        public Func<string, IEnumerable<string>> NounExtractor
+        {
+            get { return this.nounExtractor; }
+        }
+
         public Task AddWordsAsync(int id, string subject)
         {
-            if (subject == null)
-                throw new ArgumentNullException("subject");
+            var tasks = this.nounExtractor(subject)
+                .Select(w => this.articleWords.InsertAsync(new ArticleWord(id, w))).ToArray();
 
-            throw new NotImplementedException();
+            return Task.WhenAll(tasks);
         }
 
         public Task ModifyWordsAsync(int id, string subject)
--- test/DomainModelUnitTest/ArticleWordServiceTest.cs
+++ test/DomainModelUnitTest/ArticleWordServiceTest.cs
@@ -1,6 +1,12 @@
 ﻿namespace ArticleHarbor.DomainModel
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
     using System.Threading.Tasks;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.SemanticComparison.Fluent;
     using Xunit;
 
     public class ArticleWordServiceTest : IdiomaticTest<ArticleWordService>
@@ -12,11 +18,46 @@
         }
 
         [Test]
-        public async Task RemoveAsyncCorrectlyRemovesWords(
-            ArticleWordService sut, int id)
+        public async Task RemoveWordsAsyncCorrectlyRemovesWords(
+            ArticleWordService sut,
+            int id)
         {
             await sut.RemoveWordsAsync(id);
             sut.ArticleWords.ToMock().Verify(x => x.DeleteAsync(id));
+        }
+
+        [Test]
+        public async Task AddWordsAsyncCorrectlyAddsWords(
+            int id,
+            string subject,
+            string[] words,
+            IFixture fixture)
+        {
+            // Fixture setup
+            fixture.Inject<Func<string, IEnumerable<string>>>(s =>
+            {
+                Assert.Equal(subject, s);
+                return words;
+            });
+            var sut = fixture.Create<ArticleWordService>();
+
+            // Excercise outcome
+            await sut.AddWordsAsync(id, subject);
+
+            // Verify system
+            foreach (var word in words)
+            {
+                var likeness = word.AsSource().OfLikeness<ArticleWord>()
+                    .With(x => x.ArticleId).EqualsWhen((a, b) => b.ArticleId == id)
+                    .With(x => x.Word).EqualsWhen((a, b) => b.Word == word);
+                sut.ArticleWords.ToMock().Verify(
+                    x => x.InsertAsync(It.Is<ArticleWord>(p => likeness.Equals(p))));
+            }
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.AddWordsAsync(0, null));
         }
     }
 }
