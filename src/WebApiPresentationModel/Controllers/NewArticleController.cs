494ebf61c9910b4b4fd5a5037457a78ceee50db6
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 09:55:47 +0900

    Introduced the ArticleService property.

--- src/WebApiPresentationModel/Controllers/NewArticleController.cs
+++ src/WebApiPresentationModel/Controllers/NewArticleController.cs
@@ -1,8 +1,24 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel.Controllers
 {
+    using System;
     using System.Web.Http;
+    using DomainModel;
 
     public class NewArticleController : ApiController
     {
+        private readonly IArticleService articleService;
+
+        public NewArticleController(IArticleService articleService)
+        {
+            if (articleService == null)
+                throw new ArgumentNullException("articleService");
+
+            this.articleService = articleService;
+        }
+
+        public IArticleService ArticleService
+        {
+            get { return this.articleService; }
+        }
     }
 }
