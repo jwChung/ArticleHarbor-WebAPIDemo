e4a0a24458a090ca6cb2d65ac3f9f2422d751c62
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 23:16:01 +0900

    Created the Bookmark class.

---
+++ test/DomainModelUnitTest/Models/BookmarkTest.cs
@@ -0,0 +1,19 @@
+﻿using Ploeh.AutoFixture;
+namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Reflection;
+    using Xunit;
+
+    public class BookmarkTest : IdiomaticTest<Bookmark>
+    {
+        [Test]
+        public void InitializeWithEmptyStringValuesThrows(
+            IFixture fixture)
+        {
+            fixture.Inject(string.Empty);
+            var e = Assert.Throws<TargetInvocationException>(() => fixture.Create<Bookmark>());
+            Assert.IsType<ArgumentException>(e.InnerException);
+        }
+    }
+}
