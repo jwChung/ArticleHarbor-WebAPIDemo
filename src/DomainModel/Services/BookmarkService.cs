ccc04d81d772f2a008a684934cbf3f93e9124566
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 00:04:44 +0900

    Introduced the BookmarkService class.

---
+++ src/DomainModel/Services/BookmarkService.cs
@@ -0,0 +1,34 @@
+﻿namespace ArticleHarbor.DomainModel.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public class BookmarkService : IBookmarkService
+    {
+        public Task<IEnumerable<Article>> GetAsync(string userId)
+        {
+            if (userId == null)
+                throw new ArgumentNullException("userId");
+
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Bookmark bookmark)
+        {
+            if (bookmark == null)
+                throw new ArgumentNullException("bookmark");
+
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(Bookmark bookmark)
+        {
+            if (bookmark == null)
+                throw new ArgumentNullException("bookmark");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Services/BookmarkServiceTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Services
+{
+    using Xunit;
+
+    public class BookmarkServiceTest : IdiomaticTest<BookmarkService>
+    {
+        [Test]
+        public void SutIsBookmarkService(BookmarkService sut)
+        {
+            Assert.IsAssignableFrom<IBookmarkService>(sut);
+        }
+    }
+}
