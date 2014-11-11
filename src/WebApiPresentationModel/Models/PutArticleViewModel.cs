611eeaf5c49f62205979a1fed19a27010960aaa4
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-11 10:28:31 +0900

    Created the PutArticleViewModel class.

---
+++ src/WebApiPresentationModel/Models/PutArticleViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel.Models
+{
+    using System;
+
+    public class PutArticleViewModel
+    {
+        public int Id { get; set; }
+
+        public string Provider { get; set; }
+
+        public string No { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Url { get; set; }
+    }
+}
