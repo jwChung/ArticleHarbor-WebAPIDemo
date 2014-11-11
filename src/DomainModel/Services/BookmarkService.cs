b419c589d020070a185f5629a3e6bf5a335674dd
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 00:34:12 +0900

    Implemented the Articles property.

--- src/DomainModel/Services/BookmarkService.cs
+++ src/DomainModel/Services/BookmarkService.cs
@@ -9,18 +9,28 @@
     public class BookmarkService : IBookmarkService
     {
         private readonly IBookmarkRepository bookmarks;
+        private readonly IArticleRepository articles;
 
-        public BookmarkService(IBookmarkRepository bookmarks)
+        public BookmarkService(IBookmarkRepository bookmarks, IArticleRepository articles)
         {
             if (bookmarks == null)
                 throw new ArgumentNullException("bookmarks");
 
+            if (articles == null)
+                throw new ArgumentNullException("articles");
+
             this.bookmarks = bookmarks;
+            this.articles = articles;
         }
 
         public IBookmarkRepository Bookmarks
         {
             get { return this.bookmarks; }
+        }
+
+        public IArticleRepository Articles
+        {
+            get { return this.articles; }
         }
 
         public Task<IEnumerable<Article>> GetAsync(string userId)
