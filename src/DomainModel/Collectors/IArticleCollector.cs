e2387af9e114c43878b7e67a0b33388d31f7bdfd
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 14:29:41 +0900

    Introduced the IArticleCollector interface.

---
+++ src/DomainModel/Collectors/IArticleCollector.cs
@@ -0,0 +1,11 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface IArticleCollector
+    {
+        Task<IEnumerable<Article>> CollectAsync();
+    }
+}
