e2839583409613f4f85e6f93b8445b987ba95000
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 13:31:51 +0900

    Implemented HaniCollector.

--- src/DomainModel/Collectors/HaniCollector.cs
+++ src/DomainModel/Collectors/HaniCollector.cs
@@ -1,7 +1,14 @@
 ﻿namespace ArticleHarbor.DomainModel.Collectors
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
     using System.Threading.Tasks;
+    using System.Xml.Linq;
+    using Models;
     using Services;
 
     /// <summary>
@@ -34,9 +41,37 @@
             get { return this.articleService; }
         }
 
-        public Task CollectAsync()
+        public async Task CollectAsync()
         {
-            return null;
+            var articles = await this.GetArticlesAsync();
+            await Task.WhenAll(articles.Select(this.articleService.AddAsync));
+        }
+
+        private static async Task<Stream> GetStreamAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://www.hani.co.kr");
+                var response = await client.GetAsync("rss");
+                return await response.Content.ReadAsStreamAsync();
+            }
+        }
+
+        private async Task<IEnumerable<Article>> GetArticlesAsync()
+        {
+            using (var reader = new StreamReader(await GetStreamAsync(), Encoding.UTF8))
+            {
+                return from item in XDocument.Load(reader).Descendants("item")
+                       select new Article(
+                           -1,
+                           "한겨레",
+                           item.Element("guid").Value,
+                           item.Element("title").Value,
+                           item.Element("description").Value,
+                           DateTime.Parse(item.Element("pubDate").Value),
+                           item.Element("link").Value,
+                           this.actor);
+            }
         }
     }
 }
--- test/DomainModelUnitTest/Collectors/HaniCollectorTest.cs
+++ test/DomainModelUnitTest/Collectors/HaniCollectorTest.cs
@@ -1,6 +1,43 @@
 ﻿namespace ArticleHarbor.DomainModel.Collectors
 {
+    using System;
+    using Models;
+    using Moq;
+    using Ploeh.SemanticComparison.Fluent;
+
     public class HaniCollectorTest : IdiomaticTest<HaniCollector>
     {
+        [Test]
+        public void CollectAsyncCollectsArticles(
+            HaniCollector sut,
+            string dummyString,
+            DateTime dummyDate)
+        {
+            // Fixture setup
+            var article = new Article(
+                id: -1,
+                provider: "한겨레",
+                no: dummyString, 
+                subject: dummyString,
+                body: dummyString, 
+                date: dummyDate, 
+                url: dummyString,
+                userId: sut.Actor);
+
+            var likeness = article.AsSource().OfLikeness<Article>()
+                .Without(x => x.No)
+                .Without(x => x.Subject)
+                .Without(x => x.Body)
+                .Without(x => x.Date)
+                .Without(x => x.Url);
+
+            // Excercise sytem;
+            sut.CollectAsync().Wait();
+
+            // Verify outcome
+            sut.ArticleService.ToMock().Verify(
+                x => x.AddAsync(It.Is<Article>(a => likeness.Equals(a))),
+                Times.Exactly(25));
+        }
     }
 }
