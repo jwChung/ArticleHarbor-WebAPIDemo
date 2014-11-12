172eab89fd31bf65506a70f672c12fd2d4e5efe1
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 17:08:37 +0900

    Implemented ArticleCollectorTransformation.

---
+++ src/DomainModel/Collectors/ArticleCollectorTransformation.cs
@@ -0,0 +1,44 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Models;
+
+    public class ArticleCollectorTransformation : IArticleCollector
+    {
+        private readonly IArticleCollector collector;
+        private readonly IArticleTransformation transformation;
+
+        public ArticleCollectorTransformation(
+            IArticleCollector collector,
+            IArticleTransformation transformation)
+        {
+            if (collector == null)
+                throw new ArgumentNullException("collector");
+
+            if (transformation == null)
+                throw new ArgumentNullException("transformation");
+
+            this.collector = collector;
+            this.transformation = transformation;
+        }
+
+        public IArticleCollector Collector
+        {
+            get { return this.collector; }
+        }
+
+        public IArticleTransformation Transformation
+        {
+            get { return this.transformation; }
+        }
+
+        public async Task<IEnumerable<Article>> CollectAsync()
+        {
+            var articles = await this.collector.CollectAsync();
+            return articles.Select(this.transformation.Transform);
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Collectors/ArticleCollectorTransformationTest.cs
@@ -0,0 +1,33 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+    using Moq;
+    using Xunit;
+
+    public class ArticleCollectorTransformationTest : IdiomaticTest<ArticleCollectorTransformation>
+    {
+        [Test]
+        public void SutIsArticleCollector(ArticleCollectorTransformation sut)
+        {
+            Assert.IsAssignableFrom<IArticleCollector>(sut);
+        }
+
+        [Test]
+        public void CollectAsyncReturnsTransformedArticles(
+            ArticleCollectorTransformation sut,
+            IEnumerable<Article> artices,
+            Article[] expected)
+        {
+            sut.Collector.Of(x => x.CollectAsync() == Task.FromResult(artices));
+            int index = 0;
+            sut.Transformation.ToMock().Setup(x => x.Transform(It.IsAny<Article>()))
+                .Returns(() => expected[index++]);
+
+            var actual = sut.CollectAsync().Result;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
