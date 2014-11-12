2d404406b325dc609e3aefac3443ac3d791342ef
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 14:46:06 +0900

    Introduced the Actor and FacebookId properties.

--- src/DomainModel/Collectors/FacebookRssCollector.cs
+++ src/DomainModel/Collectors/FacebookRssCollector.cs
@@ -7,6 +7,31 @@
 
     public class FacebookRssCollector : IArticleCollector
     {
+        private readonly string actor;
+        private readonly string facebookId;
+
+        public FacebookRssCollector(string actor, string facebookId)
+        {
+            if (actor == null)
+                throw new ArgumentNullException("actor");
+
+            if (facebookId == null)
+                throw new ArgumentNullException("facebookId");
+
+            this.actor = actor;
+            this.facebookId = facebookId;
+        }
+
+        public string Actor
+        {
+            get { return this.actor; }
+        }
+
+        public string FacebookId
+        {
+            get { return this.facebookId; }
+        }
+
         public Task<IEnumerable<Article>> CollectAsync()
         {
             throw new NotImplementedException();
