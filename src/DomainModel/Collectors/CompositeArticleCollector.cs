e0922580eb7f5061918043e0f2d53ae4c4491039
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 15:24:52 +0900

    Introduced the Collectors property.

--- src/DomainModel/Collectors/CompositeArticleCollector.cs
+++ src/DomainModel/Collectors/CompositeArticleCollector.cs
@@ -7,6 +7,21 @@
 
     public class CompositeArticleCollector : IArticleCollector
     {
+        private readonly IEnumerable<IArticleCollector> collectors;
+
+        public CompositeArticleCollector(IEnumerable<IArticleCollector> collectors)
+        {
+            if (collectors == null)
+                throw new ArgumentNullException("collectors");
+
+            this.collectors = collectors;
+        }
+
+        public IEnumerable<IArticleCollector> Collectors
+        {
+            get { return this.collectors; }
+        }
+
         public Task<IEnumerable<Article>> CollectAsync()
         {
             throw new NotImplementedException();
