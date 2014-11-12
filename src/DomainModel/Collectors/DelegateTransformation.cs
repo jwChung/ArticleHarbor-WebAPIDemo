0231b11068b63eb88270bd616edb6f2b66794c1b
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 20:08:08 +0900

    Implemented the DelegateTransformation class.

---
+++ src/DomainModel/Collectors/DelegateTransformation.cs
@@ -0,0 +1,31 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System;
+    using Models;
+
+    public class DelegateTransformation : IArticleTransformation
+    {
+        private readonly Func<Article, Article> transformer;
+
+        public DelegateTransformation(Func<Article, Article> transformer)
+        {
+            if (transformer == null)
+                throw new ArgumentNullException("transformer");
+
+            this.transformer = transformer;
+        }
+
+        public Func<Article, Article> Transformer
+        {
+            get { return this.transformer; }
+        }
+
+        public Article Transform(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            return this.transformer(article);
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Collectors/DelegateTransformationTest.cs
@@ -0,0 +1,34 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System;
+    using Models;
+    using Ploeh.AutoFixture;
+    using Xunit;
+
+    public class DelegateTransformationTest : IdiomaticTest<DelegateTransformation>
+    {
+        [Test]
+        public void SutIsArticleTransformation(DelegateTransformation sut)
+        {
+            Assert.IsAssignableFrom<IArticleTransformation>(sut);
+        }
+
+        [Test]
+        public void TransformReturnsCorrectResult(
+            Article article,
+            Article newArticle,
+            IFixture fixture)
+        {
+            fixture.Inject<Func<Article, Article>>(x =>
+            {
+                Assert.Equal(article, x);
+                return newArticle;
+            });
+            var sut = fixture.Create<DelegateTransformation>();
+
+            var actual = sut.Transform(article);
+
+            Assert.Equal(newArticle, actual);
+        }
+    }
+}
