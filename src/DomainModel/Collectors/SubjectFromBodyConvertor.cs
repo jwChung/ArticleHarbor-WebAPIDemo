71a10a1f1ee105ac6ce85a0f11f190304c15e9a9
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 15:20:13 +0900

    Implemented the SubjectFromBodyConvertor class.

--- src/DomainModel/Collectors/SubjectFromBodyConvertor.cs
+++ src/DomainModel/Collectors/SubjectFromBodyConvertor.cs
@@ -2,13 +2,33 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Models;
 
     public class SubjectFromBodyConvertor : IArticleConvertor
     {
+        private readonly int subjectLength;
+
+        public SubjectFromBodyConvertor(int subjectLength)
+        {
+            this.subjectLength = subjectLength;
+        }
+
+        public int SubjectLength
+        {
+            get { return this.subjectLength; }
+        }
+
         public IEnumerable<Article> Convert(IEnumerable<Article> articles)
         {
-            throw new NotImplementedException();
+            if (articles == null)
+                throw new ArgumentNullException("articles");
+
+            return from article in articles
+                   let newSubject = article.Body.Substring(
+                       0,
+                       Math.Min(article.Body.Length, this.subjectLength))
+                   select article.WithSubject(newSubject);
         }
     }
 }
