27fe0adc4f846f831255eb60461b566c71e71aa1
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 15:04:22 +0900

    Introduced the SubjectFromBodyConvertor class.

---
+++ src/DomainModel/Collectors/SubjectFromBodyConvertor.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System;
+    using System.Collections.Generic;
+    using Models;
+
+    public class SubjectFromBodyConvertor : IArticleConvertor
+    {
+        public IEnumerable<Article> Convert(IEnumerable<Article> articles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Collectors/SubjectFromBodyConvertorTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using Xunit;
+
+    public class SubjectFromBodyConvertorTest : IdiomaticTest<SubjectFromBodyConvertor>
+    {
+        [Test]
+        public void SutIsArticleConverter(SubjectFromBodyConvertor sut)
+        {
+            Assert.IsAssignableFrom<IArticleConvertor>(sut);
+        }
+    }
+}
