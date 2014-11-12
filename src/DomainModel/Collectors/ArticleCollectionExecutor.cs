39717abd7251727857805ee0cd27fbbb1c74cdc6
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-12 16:11:29 +0900

    Implemented ArticleCollectionExecutor.

--- src/DomainModel/Collectors/ArticleCollectionExecutor.cs
+++ src/DomainModel/Collectors/ArticleCollectionExecutor.cs
@@ -1,14 +1,32 @@
 ﻿namespace ArticleHarbor.DomainModel.Collectors
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Services;
 
     public class ArticleCollectionExecutor
     {
         private readonly IArticleCollector collector;
         private readonly IArticleService service;
+        private readonly int delay;
 
-        public ArticleCollectionExecutor(IArticleCollector collector, IArticleService service)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleCollectionExecutor"/> class.
+        /// </summary>
+        /// <param name="collector">
+        /// The collector.
+        /// </param>
+        /// <param name="service">
+        /// The service.
+        /// </param>
+        /// <param name="delay">
+        /// The delay for adding between each article (milliseconds).
+        /// </param>
+        public ArticleCollectionExecutor(
+            IArticleCollector collector,
+            IArticleService service,
+            int delay)
         {
             if (collector == null)
                 throw new ArgumentNullException("collector");
@@ -18,6 +36,7 @@
 
             this.collector = collector;
             this.service = service;
+            this.delay = delay;
         }
 
         public IArticleCollector Collector
@@ -28,6 +47,32 @@
         public IArticleService Service
         {
             get { return this.service; }
+        }
+
+        public int Delay
+        {
+            get { return this.delay; }
+        }
+
+        /// <summary>
+        /// Executes the asynchronous.
+        /// </summary>
+        /// <returns>
+        /// The number of collected articles.
+        /// </returns>
+        public async Task<int> ExecuteAsync()
+        {
+            var articles = await this.collector.CollectAsync();
+
+            int count = 0;
+            foreach (var article in articles)
+            {
+                Thread.Sleep(this.delay);
+                count++;
+                await this.service.AddAsync(article);
+            }
+
+            return count;
         }
     }
 }
--- test/DomainModelUnitTest/Collectors/ArticleCollectionExecutorTest.cs
+++ test/DomainModelUnitTest/Collectors/ArticleCollectionExecutorTest.cs
@@ -1,6 +1,28 @@
 ﻿namespace ArticleHarbor.DomainModel.Collectors
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Models;
+    using Ploeh.AutoFixture;
+    using Xunit;
+
     public class ArticleCollectionExecutorTest : IdiomaticTest<ArticleCollectionExecutor>
     {
+        [Test]
+        public void ExecuteAsyncAddsArticles(
+            IEnumerable<Article> articles,
+            IFixture fixture)
+        {
+            fixture.Inject(0);
+            var sut = fixture.Create<ArticleCollectionExecutor>();
+            sut.Collector.Of(x => x.CollectAsync() == Task.FromResult(articles));
+
+            var actual = sut.ExecuteAsync().Result;
+
+            Assert.Equal(articles.Count(), actual);
+            foreach (var article in articles)
+                sut.Service.ToMock().Verify(x => x.AddAsync(article));
+        }
     }
 }
