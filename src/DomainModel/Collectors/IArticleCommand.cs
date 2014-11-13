f66b682ac047e50cc2dac91aca81ef0853414699
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-13 11:31:47 +0900

    Introduced the IArticleCommand interface.

---
+++ src/DomainModel/Collectors/IArticleCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ArticleHarbor.DomainModel.Collectors
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface IArticleCommand
+    {
+        Task ExecuteAsync(IEnumerable<Article> articles);
+    }
+}
