f3597e2322da7be2f112501f6e6511c464ea52bf
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 11:43:00 +0900

    Introduced members of IModelElementCommand.

--- src/DomainModel/Models/IModelElementCommand.cs
+++ src/DomainModel/Models/IModelElementCommand.cs
@@ -2,5 +2,8 @@ namespace ArticleHarbor.DomainModel.Models
 {
     public interface IModelElementCommand<TResult>
     {
+        TResult Result { get; }
+
+        IModelElementCommand<TResult> Execute(IModelElement<TResult> element);
     }
 }
