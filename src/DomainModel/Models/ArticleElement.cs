d63f7f5c5ae2cfe8aaa1f9f9bd5318134e6c4be5
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 12:03:56 +0900

    Introduced ArticleElement.

---
+++ src/DomainModel/Models/ArticleElement.cs
@@ -0,0 +1,31 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+
+    public class ArticleElement : IModelElement<Article>
+    {
+        private readonly Article article;
+
+        public ArticleElement(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            this.article = article;
+        }
+
+        public Article Model
+        {
+            get { return this.article; }
+        }
+
+        public IModelElementCommand<TResult> Execute<TResult>(
+            IModelElementCommand<TResult> command)
+        {
+            if (command == null)
+                throw new ArgumentNullException("command");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/ArticleElementTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class ArticleElementTest : IdiomaticTest<ArticleElement>
+    {
+        [Test]
+        public void SutIsModelElement(ArticleElement sut)
+        {
+            Assert.IsAssignableFrom<IModelElement<Article>>(sut);
+        }
+    }
+}
