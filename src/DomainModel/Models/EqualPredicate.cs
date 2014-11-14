bba77098c028a11cca80db7cb6e350b7ec489fe4
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 13:33:14 +0900

    Implemented the EqualPredicate class.

--- src/DomainModel/Models/EqualPredicate.cs
+++ src/DomainModel/Models/EqualPredicate.cs
@@ -5,14 +5,44 @@
 
     public class EqualPredicate : IPredicate
     {
+        private readonly string name;
+        private readonly object value;
+        private readonly IParameter[] parameters;
+
+        public EqualPredicate(string name, object value)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            if (name.Length == 0)
+                throw new ArgumentException("The name should not be empty string.");
+
+            this.name = name;
+            this.value = value;
+            this.parameters = new IParameter[] { new Parameter(this.name, this.value) };
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public object Value
+        {
+            get { return this.value; }
+        }
+
         public string Condition
         {
-            get { throw new NotImplementedException(); }
+            get { return this.name + " = @" + this.name; }
         }
 
         public IEnumerable<IParameter> Parameters
         {
-            get { throw new NotImplementedException(); }
+            get { return this.parameters; }
         }
     }
 }
--- test/DomainModelUnitTest/Models/EqualPredicateTest.cs
+++ test/DomainModelUnitTest/Models/EqualPredicateTest.cs
@@ -1,5 +1,10 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Ploeh.AutoFixture;
     using Xunit;
 
     public class EqualPredicateTest : IdiomaticTest<EqualPredicate>
@@ -8,6 +13,43 @@
         public void SutIsPredicate(EqualPredicate sut)
         {
             Assert.IsAssignableFrom<IPredicate>(sut);
+        }
+
+        [Test]
+        public void InitializeWithEmptyNameThrows(IFixture fixture)
+        {
+            fixture.Inject(string.Empty);
+            var e = Assert.Throws<TargetInvocationException>(
+                () => fixture.Create<EqualPredicate>());
+            Assert.IsType<ArgumentException>(e.InnerException);
+        }
+
+        [Test]
+        public void ConditionIsCorrect(EqualPredicate sut)
+        {
+            var expected = sut.Name + " = @" + sut.Name;
+            var actual = sut.Condition;
+            Assert.Equal(expected, actual);
+        }
+
+        [Test]
+        public void ParametersIsCorrect(EqualPredicate sut)
+        {
+            var actual = sut.Parameters.Single();
+            Assert.Equal(new Parameter(sut.Name, sut.Value), actual);
+        }
+
+        [Test]
+        public void ParametersAlwaysReturnsSameInstance(EqualPredicate sut)
+        {
+            var actual = sut.Parameters;
+            Assert.Same(sut.Parameters, actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Condition);
+            yield return this.Properties.Select(x => x.Parameters);
         }
     }
 }
