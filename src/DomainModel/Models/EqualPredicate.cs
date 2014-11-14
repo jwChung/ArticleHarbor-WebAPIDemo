1d4f7a2862611286ed16565f7a09bdccea60cbb9
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 13:11:07 +0900

    Introduced EqualPredicate.

---
+++ src/DomainModel/Models/EqualPredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EqualPredicate : IPredicate
+    {
+        public string Condition
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<IParameter> Parameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/EqualPredicateTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class EqualPredicateTest : IdiomaticTest<EqualPredicate>
+    {
+        [Test]
+        public void SutIsPredicate(EqualPredicate sut)
+        {
+            Assert.IsAssignableFrom<IPredicate>(sut);
+        }
+    }
+}
