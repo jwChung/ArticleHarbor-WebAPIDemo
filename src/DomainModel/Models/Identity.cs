a8e0ce7e0f6f4c30e3a100e4901b61be4babaacb
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 10:59:02 +0900

    Implemented Identity<TKey1, TKey2>

--- src/DomainModel/Models/Identity.cs
+++ src/DomainModel/Models/Identity.cs
@@ -48,4 +48,65 @@
             return EqualityComparer<TKey>.Default.Equals(this.key, other.key);
         }
     }
+
+    public class Identity<TKey1, TKey2> : IIndentity
+    {
+        private readonly TKey1 key1;
+        private readonly TKey2 key2;
+        private readonly object[] objects;
+
+        public Identity(TKey1 key1, TKey2 key2)
+        {
+            if (key1 == null)
+                throw new ArgumentNullException("key1");
+
+            if (key2 == null)
+                throw new ArgumentNullException("key2");
+
+            this.key1 = key1;
+            this.key2 = key2;
+            this.objects = new object[] { this.key1, this.key2 };
+        }
+
+        public TKey1 Key1
+        {
+            get { return this.key1; }
+        }
+
+        public TKey2 Key2
+        {
+            get { return this.key2; }
+        }
+
+        public object[] Keys
+        {
+            get { return this.objects; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return this.Equals((Identity<TKey1, TKey2>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (EqualityComparer<TKey1>.Default.GetHashCode(this.key1) * 397)
+                    ^ EqualityComparer<TKey2>.Default.GetHashCode(this.key2);
+            }
+        }
+
+        protected bool Equals(Identity<TKey1, TKey2> other)
+        {
+            return EqualityComparer<TKey1>.Default.Equals(this.key1, other.key1)
+                && EqualityComparer<TKey2>.Default.Equals(this.key2, other.key2);
+        }
+    }
 }
--- test/DomainModelUnitTest/Models/IdentityTest.cs
+++ test/DomainModelUnitTest/Models/IdentityTest.cs
@@ -59,4 +59,59 @@
             yield return this.Properties.Select(x => x.Keys);
         }
     }
+
+    public class IdentityOfTKey1AndKey2Test : IdiomaticTest<Identity<object, object>>
+    {
+        [Test]
+        public void SutIsIndentity(Identity<object, object> sut)
+        {
+            Assert.IsAssignableFrom<IIndentity>(sut);
+        }
+
+        [Test]
+        public void KeysReturnsCorrectValues(Identity<object, object> sut)
+        {
+            var expected = new object[] { sut.Key1, sut.Key2 };
+            Assert.Equal(expected, sut.Keys);
+        }
+
+        [Test]
+        public void KeysAlwaysReturnsSameInstance(Identity<object, object> sut)
+        {
+            var actual = sut.Keys;
+            Assert.Same(sut.Keys, actual);
+        }
+
+        [Test]
+        public void EqualsWithSameKeyReturnsTrue(Identity<object, object> sut)
+        {
+            var other = new Identity<object, object>(sut.Key1, sut.Key2);
+            var actual = sut.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsWithNotSameKeyReturnsFalse(
+            Identity<object, object> sut, Identity<object, object> other)
+        {
+            var actual = sut.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void GetHashCodeWithSameKeyReturnsSameValue(Identity<object, object> sut)
+        {
+            var other = new Identity<object, object>(sut.Key1, sut.Key2);
+            var expected = other.GetHashCode();
+
+            var actual = sut.GetHashCode();
+
+            Assert.Equal(expected, actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Keys);
+        }
+    }
 }
