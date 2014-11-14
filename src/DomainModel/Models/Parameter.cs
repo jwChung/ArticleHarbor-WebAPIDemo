84ecaad633c6c6759121a0d85f5bfce42263583b
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-14 12:57:51 +0900

    Implemented Parameter.

---
+++ src/DomainModel/Models/Parameter.cs
@@ -0,0 +1,56 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+
+    public class Parameter : IParameter
+    {
+        private readonly string name;
+        private readonly object value;
+
+        public Parameter(string name, object value)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            this.name = name;
+            this.value = value;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public object Value
+        {
+            get { return this.value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return this.Equals((Parameter)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.name.GetHashCode() * 397) ^ this.value.GetHashCode();
+            }
+        }
+
+        protected bool Equals(Parameter other)
+        {
+            return string.Equals(this.name, other.name) && this.value.Equals(other.value);
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/ParameterTest.cs
@@ -0,0 +1,36 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class ParameterTest : IdiomaticTest<Parameter>
+    {
+        [Test]
+        public void SutIsParameter(Parameter sut)
+        {
+            Assert.IsAssignableFrom<IParameter>(sut);
+        }
+
+        [Test]
+        public void EqualsParameterWithSameValuesReturnsTrue(Parameter sut)
+        {
+            var other = new Parameter(sut.Name, sut.Value);
+            var actual = sut.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsParameterWithNotSameValuesReturnsTrue(Parameter sut, Parameter other)
+        {
+            var actual = sut.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void GetHashCodeWithSameParameterReturnsSameValue(Parameter sut)
+        {
+            var other = new Parameter(sut.Name, sut.Value);
+            var actual = sut.Equals(other);
+            Assert.True(actual);
+        }
+    }
+}
