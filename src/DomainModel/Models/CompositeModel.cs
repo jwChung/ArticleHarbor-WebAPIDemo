d71f6e449ab73bbb934bf510a161d46a9e687726
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-16 04:16:37 +0900

    Introduced the Model property.

--- src/DomainModel/Models/CompositeModel.cs
+++ src/DomainModel/Models/CompositeModel.cs
@@ -1,9 +1,25 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
     using System;
+    using System.Collections.Generic;
 
     public class CompositeModel : IModel
     {
+        private readonly IEnumerable<IModel> models;
+
+        public CompositeModel(IEnumerable<IModel> models)
+        {
+            if (models == null)
+                throw new ArgumentNullException("models");
+
+            this.models = models;
+        }
+
+        public IEnumerable<IModel> Models
+        {
+            get { return this.models; }
+        }
+
         public IModelCommand<TResult> ExecuteCommand<TResult>(IModelCommand<TResult> command)
         {
             if (command == null)
