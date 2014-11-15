d598b6b611b570f2c816c2b625f767a4b9898a7e
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-15 17:57:08 +0900

    Introduced methods of IModelCommand.

--- src/DomainModel/Models/IModelCommand.cs
+++ src/DomainModel/Models/IModelCommand.cs
@@ -1,9 +1,25 @@
 namespace ArticleHarbor.DomainModel.Models
 {
+    using System.Collections.Generic;
+
     public interface IModelCommand<TResult>
     {
         TResult Result { get; }
 
-        IModelCommand<TResult> Execute(IModel model);
+        IModelCommand<TResult> Execute(IEnumerable<User> users);
+
+        IModelCommand<TResult> Execute(User user);
+
+        IModelCommand<TResult> Execute(IEnumerable<Article> articles);
+
+        IModelCommand<TResult> Execute(Article article);
+
+        IModelCommand<TResult> Execute(IEnumerable<Keyword> keywords);
+
+        IModelCommand<TResult> Execute(Keyword keywork);
+
+        IModelCommand<TResult> Execute(IEnumerable<Bookmark> bookmarks);
+
+        IModelCommand<TResult> Execute(Bookmark bookmark);
     }
 }
