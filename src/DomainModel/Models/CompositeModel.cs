4784e2ec4018e7860b4eda0b6575193962c05982
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-16 04:15:21 +0900

    Introduced the CompositeModel class.

---
+++ src/DomainModel/Models/CompositeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+
+    public class CompositeModel : IModel
+    {
+        public IModelCommand<TResult> ExecuteCommand<TResult>(IModelCommand<TResult> command)
+        {
+            if (command == null)
+                throw new ArgumentNullException("command");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/CompositeModelTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class CompositeModelTest : IdiomaticTest<CompositeModel>
+    {
+        [Test]
+        public void SutIsModel(CompositeModel sut)
+        {
+            Assert.IsAssignableFrom<IModel>(sut);
+        }
+    }
+}
