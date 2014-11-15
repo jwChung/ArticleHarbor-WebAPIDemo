6ca42e22068bbe2ea314d828b141c03fec1c492a
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-16 04:12:05 +0900

    Implemented ModelCommand.

---
+++ src/DomainModel/Models/ModelCommand.cs
@@ -0,0 +1,67 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public abstract class ModelCommand<TResult> : IModelCommand<TResult>
+    {
+        public abstract TResult Result { get; }
+
+        public virtual IModelCommand<TResult> Execute(IEnumerable<User> users)
+        {
+            if (users == null)
+                throw new ArgumentNullException("users");
+
+            return users.Aggregate<User, IModelCommand<TResult>>(
+                this, (c, u) => c.Execute(u));
+        }
+
+        public virtual IModelCommand<TResult> Execute(User user)
+        {
+            return this;
+        }
+
+        public virtual IModelCommand<TResult> Execute(IEnumerable<Article> articles)
+        {
+            if (articles == null)
+                throw new ArgumentNullException("articles");
+
+            return articles.Aggregate<Article, IModelCommand<TResult>>(
+                this, (c, a) => c.Execute(a));
+        }
+
+        public virtual IModelCommand<TResult> Execute(Article article)
+        {
+            return this;
+        }
+
+        public virtual IModelCommand<TResult> Execute(IEnumerable<Keyword> keywords)
+        {
+            if (keywords == null)
+                throw new ArgumentNullException("keywords");
+
+            return keywords.Aggregate<Keyword, IModelCommand<TResult>>(
+               this, (c, k) => c.Execute(k));
+        }
+
+        public virtual IModelCommand<TResult> Execute(Keyword keywords)
+        {
+            return this;
+        }
+
+        public virtual IModelCommand<TResult> Execute(IEnumerable<Bookmark> bookmarks)
+        {
+            if (bookmarks == null)
+                throw new ArgumentNullException("bookmarks");
+
+            return bookmarks.Aggregate<Bookmark, IModelCommand<TResult>>(
+                this, (c, b) => c.Execute(b));
+        }
+
+        public virtual IModelCommand<TResult> Execute(Bookmark bookmark)
+        {
+            return this;
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/ModelCommandTest.cs
@@ -0,0 +1,113 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Jwc.Experiment.Xunit;
+    using Xunit;
+
+    public class ModelCommandTest : IdiomaticTest<ModelCommand<object>>
+    {
+        [Test]
+        public void SutIsModelCommand(ModelCommand<object> sut)
+        {
+            Assert.IsAssignableFrom<IModelCommand<object>>(sut);
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> ExecuteModelReturnSut(ModelCommand<object> sut)
+        {
+            yield return TestCase.WithAuto<User>().Create(
+                x =>
+                {
+                    var actual = sut.Execute(x);
+                    Assert.Equal(sut, actual);
+                });
+            yield return TestCase.WithAuto<Article>().Create(
+                x =>
+                {
+                    var actual = sut.Execute(x);
+                    Assert.Equal(sut, actual);
+                });
+            yield return TestCase.WithAuto<Bookmark>().Create(
+                x =>
+                {
+                    var actual = sut.Execute(x);
+                    Assert.Equal(sut, actual);
+                });
+            yield return TestCase.WithAuto<Keyword>().Create(
+               x =>
+               {
+                   var actual = sut.Execute(x);
+                   Assert.Equal(sut, actual);
+               });
+        }
+
+        [Test]
+        public void ExecuteUsersRespectivelyCallsExecuteUser(
+            ModelCommand<object> sut,
+            User[] users,
+            IModelCommand<object>[] commands)
+        {
+            sut.Of(x => x.Execute(users[0]) == commands[0]);
+            commands[0].Of(x => x.Execute(users[1]) == commands[1]);
+            commands[1].Of(x => x.Execute(users[2]) == commands[2]);
+
+            var actual = sut.Execute(users);
+
+            Assert.Equal(commands[2], actual);
+        }
+
+        [Test]
+        public void ExecuteArticlesRespectivelyCallsExecuteArticle(
+            ModelCommand<object> sut,
+            Article[] articles,
+            IModelCommand<object>[] commands)
+        {
+            sut.Of(x => x.Execute(articles[0]) == commands[0]);
+            commands[0].Of(x => x.Execute(articles[1]) == commands[1]);
+            commands[1].Of(x => x.Execute(articles[2]) == commands[2]);
+
+            var actual = sut.Execute(articles);
+
+            Assert.Equal(commands[2], actual);
+        }
+
+        [Test]
+        public void ExecuteBookmarksRespectivelyCallsExecuteBookmark(
+            ModelCommand<object> sut,
+            Bookmark[] bookmarks,
+            IModelCommand<object>[] commands)
+        {
+            sut.Of(x => x.Execute(bookmarks[0]) == commands[0]);
+            commands[0].Of(x => x.Execute(bookmarks[1]) == commands[1]);
+            commands[1].Of(x => x.Execute(bookmarks[2]) == commands[2]);
+
+            var actual = sut.Execute(bookmarks);
+
+            Assert.Equal(commands[2], actual);
+        }
+
+        [Test]
+        public void ExecuteKeywordsRespectivelyCallsExecuteKeyword(
+            ModelCommand<object> sut,
+            Keyword[] keywords,
+            IModelCommand<object>[] commands)
+        {
+            sut.Of(x => x.Execute(keywords[0]) == commands[0]);
+            commands[0].Of(x => x.Execute(keywords[1]) == commands[1]);
+            commands[1].Of(x => x.Execute(keywords[2]) == commands[2]);
+
+            var actual = sut.Execute(keywords);
+
+            Assert.Equal(commands[2], actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.Execute(default(User)));
+            yield return this.Methods.Select(x => x.Execute(default(Article)));
+            yield return this.Methods.Select(x => x.Execute(default(Bookmark)));
+            yield return this.Methods.Select(x => x.Execute(default(Keyword)));
+        }
+    }
+}
