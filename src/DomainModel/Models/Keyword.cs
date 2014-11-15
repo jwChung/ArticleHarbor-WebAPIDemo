976e3696d2ae08333a11a65cf0726150ba0b91fe
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-16 03:43:13 +0900

    Implemented the IModel interface in Keyword.

--- src/DomainModel/Models/Keyword.cs
+++ src/DomainModel/Models/Keyword.cs
@@ -2,7 +2,7 @@
 {
     using System;
 
-    public class Keyword
+    public class Keyword : IModel
     {
         private readonly string word;
         private readonly int articleId;
@@ -24,6 +24,14 @@
         public string Word
         {
             get { return this.word; }
+        }
+
+        public IModelCommand<TResult> ExecuteCommand<TResult>(IModelCommand<TResult> command)
+        {
+            if (command == null)
+                throw new ArgumentNullException("command");
+
+            return command.Execute(this);
         }
     }
 }
--- test/DomainModelUnitTest/Models/KeywordTest.cs
+++ test/DomainModelUnitTest/Models/KeywordTest.cs
@@ -1,6 +1,24 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using Xunit;
+
     public class KeywordTest : IdiomaticTest<Keyword>
     {
+        [Test]
+        public void SutIsModel(Keyword sut)
+        {
+            Assert.IsAssignableFrom<IModel>(sut);
+        }
+
+        [Test]
+        public void ExecuteCommandReturnsCorrectResult(
+            Keyword sut,
+            IModelCommand<object> command,
+            IModelCommand<object> expected)
+        {
+            command.Of(x => x.Execute(sut) == expected);
+            var actual = sut.ExecuteCommand(command);
+            Assert.Equal(expected, actual);
+        }
     }
 }
