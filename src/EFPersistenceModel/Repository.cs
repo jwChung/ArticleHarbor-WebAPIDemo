dd9c7d77186f1e0133e4ddb7aa6b87b7944e0d80
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-19 12:16:33 +0900

    Introduced the Repository class.

---
+++ src/EFPersistenceModel/Repository.cs
@@ -0,0 +1,67 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DomainModel.Models;
+    using DomainModel.Repositories;
+
+    public class Repository<TKeys, TModel, TPersistence>
+        : IRepository<TKeys, TModel>
+        where TKeys : IKeyCollection
+        where TModel : IModel
+    {
+        public Task<TModel> Find(TKeys keys)
+        {
+            if (keys == null)
+                throw new ArgumentNullException("keys");
+
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TModel>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> InsertAsync(TModel model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TModel model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TKeys keys)
+        {
+            if (keys == null)
+                throw new ArgumentNullException("keys");
+
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TModel>> ExecuteSelectCommandAsync(IPredicate predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            throw new NotImplementedException();
+        }
+
+        public Task ExecuteDeleteCommandAsync(IPredicate predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/RepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using DomainModel.Models;
+    using DomainModel.Repositories;
+    using Xunit;
+
+    public abstract class RepositoryTest<TKeys, TModel, TPersistence>
+        : IdiomaticTest<Repository<TKeys, TModel, TPersistence>>
+        where TKeys : IKeyCollection
+        where TModel : IModel
+    {
+        [Test]
+        public void SutIsRepository(Repository<TKeys, TModel, TPersistence> sut)
+        {
+            Assert.IsAssignableFrom<IRepository<TKeys, TModel>>(sut);
+        }
+    }
+
+    public class RepositoryOfFooAndBarTest : RepositoryTest<KeyCollection<int>, Article, EFDataAccess.Article>
+    {
+    }
+}
