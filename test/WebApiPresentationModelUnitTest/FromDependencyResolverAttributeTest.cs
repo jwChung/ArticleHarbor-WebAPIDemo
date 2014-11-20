90c0d883d9e6533b34a170921f52f96b0e6b582c
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 16:05:43 +0900

    Introduced the FromDependencyResolverAttribute class.

---
+++ test/WebApiPresentationModelUnitTest/FromDependencyResolverAttributeTest.cs
@@ -0,0 +1,16 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System;
+    using System.Web.Http;
+    using Xunit;
+
+    public class FromDependencyResolverAttributeTest
+        : IdiomaticTest<FromDependencyResolverAttribute>
+    {
+        [Test]
+        public void SutIsParameterBindingAttribute(FromDependencyResolverAttribute sut)
+        {
+            Assert.IsAssignableFrom<ParameterBindingAttribute>(sut);
+        }
+    }
+}
