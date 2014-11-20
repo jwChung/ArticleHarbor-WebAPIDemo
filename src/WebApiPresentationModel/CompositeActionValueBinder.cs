ca96d7c01d4feb7e975994a34513c6fcd26251d7
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 14:57:03 +0900

    Introduced the CompositeActionValueBinder class.

---
+++ src/WebApiPresentationModel/CompositeActionValueBinder.cs
@@ -0,0 +1,16 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System;
+    using System.Web.Http.Controllers;
+
+    public class CompositeActionValueBinder : IActionValueBinder
+    {
+        public HttpActionBinding GetBinding(HttpActionDescriptor actionDescriptor)
+        {
+            if (actionDescriptor == null)
+                throw new ArgumentNullException("actionDescriptor");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/CompositeActionValueBinderTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Web.Http.Controllers;
+    using Xunit;
+
+    public class CompositeActionValueBinderTest : IdiomaticTest<CompositeActionValueBinder>
+    {
+        [Test]
+        public void SutIsActionValueBinder(CompositeActionValueBinder sut)
+        {
+            Assert.IsAssignableFrom<IActionValueBinder>(sut);
+        }
+    }
+}
