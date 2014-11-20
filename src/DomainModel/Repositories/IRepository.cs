faebb7d52e5a0edcc74fb2ba9cb5402cd4c28e60
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 17:35:24 +0900

    Addressed splitting IRepository<TKeys, TModel>.

--- src/DomainModel/Repositories/IRepository.cs
+++ src/DomainModel/Repositories/IRepository.cs
@@ -4,22 +4,26 @@
     using System.Threading.Tasks;
     using Models;
 
-    public interface IRepository<TKeys, TModel>
-        where TKeys : IKeys
+    public interface IRepository<TModel>
         where TModel : IModel
     {
-        Task<TModel> FindAsync(TKeys keys);
-
         Task<IEnumerable<TModel>> SelectAsync();
 
         Task<TModel> InsertAsync(TModel model);
 
         Task UpdateAsync(TModel model);
 
-        Task DeleteAsync(TKeys keys);
-
         Task<IEnumerable<TModel>> ExecuteSelectCommandAsync(IPredicate predicate);
 
         Task ExecuteDeleteCommandAsync(IPredicate predicate);
+    }
+
+    public interface IRepository<TKeys, TModel> : IRepository<TModel>
+        where TKeys : IKeys
+        where TModel : IModel
+    {
+        Task<TModel> FindAsync(TKeys keys);
+
+        Task DeleteAsync(TKeys keys);
     }
 }
