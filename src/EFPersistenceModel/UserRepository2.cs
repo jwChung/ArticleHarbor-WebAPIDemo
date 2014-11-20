6a96432c2b094ff471f7d38d46fc00fa175ccc4d
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 23:23:12 +0900

    Implemented the ConvertToModelAsync method.

--- src/EFPersistenceModel/UserRepository2.cs
+++ src/EFPersistenceModel/UserRepository2.cs
@@ -2,14 +2,21 @@
 {
     using System;
     using System.Data.Entity;
+    using System.Globalization;
+    using System.Linq;
     using System.Threading.Tasks;
     using DomainModel.Models;
+    using EFDataAccess;
+    using User = DomainModel.Models.User;
 
     public class UserRepository2 : Repository<Keys<string>, User, EFDataAccess.User>
     {
-        public UserRepository2(DbContext context, DbSet<EFDataAccess.User> dbSet)
+        private readonly ArticleHarborDbContext context;
+
+        public UserRepository2(ArticleHarborDbContext context, DbSet<EFDataAccess.User> dbSet)
             : base(context, dbSet)
         {
+            this.context = context;
         }
 
         public override Task<User> ConvertToModelAsync(EFDataAccess.User persistence)
@@ -17,7 +24,7 @@
             if (persistence == null)
                 throw new ArgumentNullException("persistence");
 
-            throw new NotImplementedException();
+            return this.ConvertToModelAsyncWith(persistence);
         }
 
         public override Task<EFDataAccess.User> ConvertToPersistenceAsync(User model)
@@ -26,6 +33,25 @@
                 throw new ArgumentNullException("model");
 
             throw new NotImplementedException();
+        }
+
+        private async Task<User> ConvertToModelAsyncWith(EFDataAccess.User persistence)
+        {
+            var user = await this.context.UserManager.FindByIdAsync(persistence.Id);
+            if (user == null)
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "There is no user matched with the id '{0}'.",
+                        persistence.Id),
+                    "persistence");
+
+            var roleNames = await this.context.UserManager.GetRolesAsync(persistence.Id);
+
+            return new User(
+                user.UserName,
+                (Role)Enum.Parse(typeof(Role), roleNames.Single()),
+                persistence.ApiKey);
         }
     }
 }
--- test/EFPersistenceModelUnitTest/UserRepository2Test.cs
+++ test/EFPersistenceModelUnitTest/UserRepository2Test.cs
@@ -1,7 +1,12 @@
 ﻿namespace ArticleHarbor.EFPersistenceModel
 {
+    using System;
+    using System.Linq;
     using DomainModel.Models;
+    using EFDataAccess;
+    using Ploeh.SemanticComparison.Fluent;
     using Xunit;
+    using User = DomainModel.Models.User;
 
     public class UserRepository2Test : IdiomaticTest<UserRepository2>
     {
@@ -9,6 +14,45 @@
         public void SutIsRepository(UserRepository2 sut)
         {
             Assert.IsAssignableFrom<Repository<Keys<string>, User, EFDataAccess.User>>(sut);
+        }
+
+        [Test]
+        public void ConvertToModelAsyncReturnsCorrectResult(
+            ArticleHarborDbContext context,
+            UserRepository2 sut)
+        {
+            EFDataAccess.User persistence = context.UserManager.FindByNameAsync("user2").Result;
+
+            var actual = sut.ConvertToModelAsync(persistence).Result;
+
+            Assert.Equal("user2", actual.Id);
+            Assert.Equal(Guid.Parse("232494f5670943dfac807226449fe795"), actual.ApiKey);
+            Assert.Equal(Role.Author, actual.Role);
+        }
+
+        [Test]
+        public void ConvertToModelAsyncWithIncorrectUserIdThrows(
+            UserRepository2 sut,
+             EFDataAccess.User persistence)
+        {
+            var e = Assert.Throws<AggregateException>(() => sut.ConvertToModelAsync(persistence).Result);
+            Assert.IsType<ArgumentException>(e.InnerException);
+        }
+
+        [Test(Skip = "NotImplementedException")]
+        public void ConvertToPersistenceAsyncReturnsCorrectResult(
+            ArticleHarborDbContext context,
+            UserRepository2 sut)
+        {
+            var user = new User(
+                "user1", Role.Administrator, Guid.Parse("692c7798206844b88ba9a660e3374eef"));
+
+            var actual = sut.ConvertToPersistenceAsync(user).Result;
+
+            actual.AsSource().OfLikeness<User>()
+                .With(x => x.Id).EqualsWhen((a, b) => a.UserName == b.Id)
+                .With(x => x.Role).EqualsWhen((a, b) => Role.Administrator == b.Role)
+                .ShouldEqual(user);
         }
     }
 }
