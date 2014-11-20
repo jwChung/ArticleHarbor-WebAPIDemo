939a3d616ce4285f441ee17fddbf21c5dcece4a0
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 14:57:59 +0900

    Introduced the Binders property.

--- src/WebApiPresentationModel/CompositeActionValueBinder.cs
+++ src/WebApiPresentationModel/CompositeActionValueBinder.cs
@@ -1,10 +1,26 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel
 {
     using System;
+    using System.Collections.Generic;
     using System.Web.Http.Controllers;
 
     public class CompositeActionValueBinder : IActionValueBinder
     {
+        private readonly IActionValueBinder[] binders;
+
+        public CompositeActionValueBinder(params IActionValueBinder[] binders)
+        {
+            if (binders == null)
+                throw new ArgumentNullException("binders");
+
+            this.binders = binders;
+        }
+
+        public IEnumerable<IActionValueBinder> Binders
+        {
+            get { return this.binders; }
+        }
+
         public HttpActionBinding GetBinding(HttpActionDescriptor actionDescriptor)
         {
             if (actionDescriptor == null)
