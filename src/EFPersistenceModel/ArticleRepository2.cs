e759e2bf992a6ea34cc5dbf7baabfa12c5cf582e
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 13:50:48 +0900

    Introduced the ArticleRepository2 class.

---
+++ src/EFPersistenceModel/ArticleRepository2.cs
@@ -0,0 +1,30 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using System.Data.Entity;
+    using DomainModel.Models;
+
+    public class ArticleRepository2 : Repository<Keys<int>, Article, EFDataAccess.Article>
+    {
+        public ArticleRepository2(DbContext context, DbSet<EFDataAccess.Article> dbSet)
+            : base(context, dbSet)
+        {
+        }
+
+        public override Article ConvertToModel(EFDataAccess.Article persistence)
+        {
+            if (persistence == null)
+                throw new ArgumentNullException("persistence");
+
+            throw new NotImplementedException();
+        }
+
+        public override EFDataAccess.Article ConvertToPersistence(Article model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("model");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/ArticleRepository2Test.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using ArticleHarbor.DomainModel.Models;
+    using Xunit;
+
+    public class ArticleRepository2Test : IdiomaticTest<ArticleRepository2>
+    {
+        [Test]
+        public void SutIsRepository(ArticleRepository2 sut)
+        {
+            Assert.IsAssignableFrom<Repository<Keys<int>, Article, EFDataAccess.Article>>(sut);
+        }
+    }
+}
