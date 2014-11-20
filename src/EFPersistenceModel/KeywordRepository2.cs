69e1e285c2fba59c557f37f043934856f701af77
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 21:44:35 +0900

    Introduced the KeywordRepository2 class.

---
+++ src/EFPersistenceModel/KeywordRepository2.cs
@@ -0,0 +1,31 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using System.Data.Entity;
+    using System.Threading.Tasks;
+    using DomainModel.Models;
+
+    public class KeywordRepository2 : Repository<Keys<int, string>, Keyword, EFDataAccess.Keyword>
+    {
+        public KeywordRepository2(DbContext context, DbSet<EFDataAccess.Keyword> dbSet)
+            : base(context, dbSet)
+        {
+        }
+
+        public override Task<Keyword> ConvertToModelAsync(EFDataAccess.Keyword persistence)
+        {
+            if (persistence == null)
+                throw new ArgumentNullException("persistence");
+
+            throw new NotImplementedException();
+        }
+
+        public override Task<EFDataAccess.Keyword> ConvertToPersistenceAsync(Keyword model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("model");
+            
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/KeywordRepository2Test.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using DomainModel.Models;
+    using Xunit;
+
+    public class KeywordRepository2Test : IdiomaticTest<KeywordRepository2>
+    {
+        [Test]
+        public void SutIsRepository(KeywordRepository2 sut)
+        {
+            Assert.IsAssignableFrom<Repository<Keys<int, string>, Keyword, EFDataAccess.Keyword>>(sut);
+        }
+    }
+}
