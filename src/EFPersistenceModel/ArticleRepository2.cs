0c8d727c2c36967875effa8e1aa7da054a61ecdd
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 14:32:45 +0900

    Implemented the ArticleRepository2 class.

--- src/EFPersistenceModel/ArticleRepository2.cs
+++ src/EFPersistenceModel/ArticleRepository2.cs
@@ -2,29 +2,83 @@
 {
     using System;
     using System.Data.Entity;
+    using System.Globalization;
+    using System.Threading.Tasks;
     using DomainModel.Models;
+    using EFDataAccess;
+    using Article = DomainModel.Models.Article;
 
     public class ArticleRepository2 : Repository<Keys<int>, Article, EFDataAccess.Article>
     {
-        public ArticleRepository2(DbContext context, DbSet<EFDataAccess.Article> dbSet)
+        private readonly ArticleHarborDbContext context;
+
+        public ArticleRepository2(
+            ArticleHarborDbContext context, DbSet<EFDataAccess.Article> dbSet)
             : base(context, dbSet)
         {
+            this.context = context;
         }
 
-        public override Article ConvertToModel(EFDataAccess.Article persistence)
+        public override Task<Article> ConvertToModelAsync(EFDataAccess.Article persistence)
         {
             if (persistence == null)
                 throw new ArgumentNullException("persistence");
 
-            throw new NotImplementedException();
+            return this.ConvertToModelAsyncWith(persistence);
         }
 
-        public override EFDataAccess.Article ConvertToPersistence(Article model)
+        public override Task<EFDataAccess.Article> ConvertToPersistenceAsync(Article model)
         {
             if (model == null)
                 throw new ArgumentNullException("model");
 
-            throw new NotImplementedException();
+            return this.ConvertToPersistenceAsyncWith(model);
+        }
+
+        private async Task<Article> ConvertToModelAsyncWith(EFDataAccess.Article persistence)
+        {
+            var user = await this.context.UserManager.FindByIdAsync(persistence.UserId);
+            if (user == null)
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "There is no user matched with the id '{0}'.",
+                        persistence.UserId),
+                    "persistence");
+
+            return new Article(
+                persistence.Id,
+                persistence.Provider,
+                persistence.Guid,
+                persistence.Subject,
+                persistence.Body,
+                persistence.Date,
+                persistence.Url,
+                user.UserName);
+        }
+
+        private async Task<EFDataAccess.Article> ConvertToPersistenceAsyncWith(Article model)
+        {
+            var user = await this.context.UserManager.FindByNameAsync(model.UserId);
+            if (user == null)
+                throw new ArgumentException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        "There is no user matched with the name '{0}'.",
+                        model.UserId),
+                    "persistence");
+
+            return new EFDataAccess.Article
+            {
+                Id = model.Id,
+                Provider = model.Provider,
+                Guid = model.Guid,
+                Subject = model.Subject,
+                Body = model.Body,
+                Date = model.Date,
+                Url = model.Url,
+                UserId = user.Id
+            };
         }
     }
 }
