c2cc4270adabb51c1d880a35a0ad5f1fa29eb2fe
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-21 00:27:30 +0900

    Introduced the CreatePermissionInspectingCommand class.

---
+++ src/DomainModel/Models/CreatePermissionInspectingCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class CreatePermissionInspectingCommand : ModelCommand<IEnumerable<Task>>
+    {
+        public override IEnumerable<Task> Result
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/CreatePermissionInspectingCommandTest.cs
@@ -0,0 +1,22 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CreatePermissionInspectingCommandTest
+        : IdiomaticTest<CreatePermissionInspectingCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(CreatePermissionInspectingCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<Task>>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Result);
+        }
+    }
+}
