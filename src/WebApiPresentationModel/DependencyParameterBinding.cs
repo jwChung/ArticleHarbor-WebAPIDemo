3d13de18146332c8a07baf057ebbea2f161c0be1
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-20 16:17:09 +0900

    Inttroduced the DependencyParameterBinding class.

---
+++ src/WebApiPresentationModel/DependencyParameterBinding.cs
@@ -0,0 +1,29 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Metadata;
+
+    public class DependencyParameterBinding : HttpParameterBinding
+    {
+        public DependencyParameterBinding(HttpParameterDescriptor descriptor) : base(descriptor)
+        {
+        }
+
+        public override Task ExecuteBindingAsync(
+            ModelMetadataProvider metadataProvider,
+            HttpActionContext actionContext,
+            CancellationToken cancellationToken)
+        {
+            if (metadataProvider == null)
+                throw new ArgumentNullException("metadataProvider");
+
+            if (actionContext == null)
+                throw new ArgumentNullException("actionContext");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/WebApiPresentationModelUnitTest/DependencyParameterBindingTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel
+{
+    using System.Web.Http.Controllers;
+    using Xunit;
+
+    public class DependencyParameterBindingTest : IdiomaticTest<DependencyParameterBinding>
+    {
+        [Test]
+        public void SutIsHttpParameterBinding(DependencyParameterBinding sut)
+        {
+            Assert.IsAssignableFrom<HttpParameterBinding>(sut);
+        }
+    }
+}
