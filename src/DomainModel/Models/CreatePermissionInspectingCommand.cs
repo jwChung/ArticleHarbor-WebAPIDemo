83e8d1d6cd8702c787ae6eacce4c033d679f24ee
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-21 12:02:24 +0900

    Introduced the Principal property.

--- src/DomainModel/Models/CreatePermissionInspectingCommand.cs
+++ src/DomainModel/Models/CreatePermissionInspectingCommand.cs
@@ -1,13 +1,30 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
     using System.Collections.Generic;
+    using System.Security.Principal;
     using System.Threading.Tasks;
 
     public class CreatePermissionInspectingCommand : ModelCommand<IEnumerable<Task>>
     {
+        private readonly IPrincipal principal;
+
+        public CreatePermissionInspectingCommand(IPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException("principal");
+
+            this.principal = principal;
+        }
+
         public override IEnumerable<Task> Result
         {
-            get { throw new System.NotImplementedException(); }
+            get { return null; }
+        }
+
+        public IPrincipal Principal
+        {
+            get { return this.principal; }
         }
     }
 }
