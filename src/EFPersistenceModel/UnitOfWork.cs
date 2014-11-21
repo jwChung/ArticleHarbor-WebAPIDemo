426c489a6583139dba45cb9076aeaa82ded0a0f3
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-21 17:08:18 +0900

    Implemented the repository properties of UnitOfWork.

--- src/EFPersistenceModel/UnitOfWork.cs
+++ src/EFPersistenceModel/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿namespace ArticleHarbor.EFPersistenceModel
 {
     using System;
+    using System.Data.Entity;
     using System.Threading.Tasks;
     using DomainModel.Models;
     using DomainModel.Repositories;
@@ -29,22 +30,27 @@
 
         public IRepository<Keys<int>, Article> Articles
         {
-            get { throw new NotImplementedException(); }
+            get { return new ArticleRepository2(this.context, this.context.Articles); }
         }
 
         public IRepository<Keys<int, string>, Keyword> Keywords
         {
-            get { throw new NotImplementedException(); }
+            get { return new KeywordRepository2(this.context, this.context.Keywords); }
         }
 
         public IRepository<Keys<string, int>, Bookmark> Bookmarks
         {
-            get { throw new NotImplementedException(); }
+            get { return new BookmarkRepository2(this.context, this.context.Bookmarks); }
         }
 
         public IRepository<Keys<string>, User> Users
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return new UserRepository2(
+                    this.context,
+                    (DbSet<EFDataAccess.User>)this.context.Users);
+            }
         }
 
         public Task SaveAsync()
--- test/EFPersistenceModelUnitTest/UnitOfWorkTest.cs
+++ test/EFPersistenceModelUnitTest/UnitOfWorkTest.cs
@@ -31,5 +31,53 @@
 
             context.Verify(x => x.SaveChangesAsync());
         }
+        
+        [Test]
+        public void ArticlesIsCorrect(UnitOfWork sut)
+        {
+            var actual = sut.Articles;
+
+            var articles = Assert.IsAssignableFrom<ArticleRepository2>(actual);
+            Assert.Equal(sut.Context, articles.Context);
+            Assert.Equal(sut.Context.Articles, articles.DbSet);
+        }
+
+        [Test]
+        public void KeywordsIsCorrect(UnitOfWork sut)
+        {
+            var actual = sut.Keywords;
+
+            var articles = Assert.IsAssignableFrom<KeywordRepository2>(actual);
+            Assert.Equal(sut.Context, articles.Context);
+            Assert.Equal(sut.Context.Keywords, articles.DbSet);
+        }
+
+        [Test]
+        public void BookmarksIsCorrect(UnitOfWork sut)
+        {
+            var actual = sut.Bookmarks;
+
+            var articles = Assert.IsAssignableFrom<BookmarkRepository2>(actual);
+            Assert.Equal(sut.Context, articles.Context);
+            Assert.Equal(sut.Context.Bookmarks, articles.DbSet);
+        }
+
+        [Test]
+        public void UsersIsCorrect(UnitOfWork sut)
+        {
+            var actual = sut.Users;
+
+            var articles = Assert.IsAssignableFrom<UserRepository2>(actual);
+            Assert.Equal(sut.Context, articles.Context);
+            Assert.Equal(sut.Context.Users, articles.DbSet);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Articles);
+            yield return this.Properties.Select(x => x.Keywords);
+            yield return this.Properties.Select(x => x.Bookmarks);
+            yield return this.Properties.Select(x => x.Users);
+        }
     }
 }
