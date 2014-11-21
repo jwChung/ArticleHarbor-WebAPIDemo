12d6546ca668c59cb0f34e34dd9c9df4502e3b31
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-21 14:01:17 +0900

    Introduced the repository properties.

--- src/DomainModel/Repositories/IUnitOfWork.cs
+++ src/DomainModel/Repositories/IUnitOfWork.cs
@@ -1,9 +1,18 @@
 ﻿namespace ArticleHarbor.DomainModel.Repositories
 {
     using System.Threading.Tasks;
+    using Models;
 
     public interface IUnitOfWork
     {
+        IRepository<Keys<int>, Article> Articles { get; }
+
+        IRepository<Keys<int, string>, Keyword> Keywords { get; }
+
+        IRepository<Keys<string, int>, Bookmark> Bookmarks { get; }
+
+        IRepository<Keys<string>, User> Users { get; }
+
         Task SaveAsync();
     }
 }
--- src/EFPersistenceModel/UnitOfWork.cs
+++ src/EFPersistenceModel/UnitOfWork.cs
@@ -2,8 +2,13 @@
 {
     using System;
     using System.Threading.Tasks;
-    using ArticleHarbor.EFDataAccess;
+    using DomainModel.Models;
     using DomainModel.Repositories;
+    using EFDataAccess;
+    using Article = DomainModel.Models.Article;
+    using Bookmark = DomainModel.Models.Bookmark;
+    using Keyword = DomainModel.Models.Keyword;
+    using User = DomainModel.Models.User;
 
     public class UnitOfWork : IUnitOfWork
     {
@@ -20,6 +25,26 @@
         public ArticleHarborDbContext Context
         {
             get { return this.context; }
+        }
+
+        public IRepository<Keys<int>, Article> Articles
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<int, string>, Keyword> Keywords
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<string, int>, Bookmark> Bookmarks
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<string>, User> Users
+        {
+            get { throw new NotImplementedException(); }
         }
 
         public Task SaveAsync()
