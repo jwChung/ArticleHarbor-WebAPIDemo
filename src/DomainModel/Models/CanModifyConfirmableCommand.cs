22081a147d0c32f424a4021555638edab477971f
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-21 13:35:26 +0900

    Introduced the CanModifyConfirmableCommand class.

---
+++ src/DomainModel/Models/CanModifyConfirmableCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    public class CanModifyConfirmableCommand : ModelCommand<object>
+    {
+        public override object Result
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
--- test/DomainModelUnitTest/Models/CanModifyConfirmableCommandTest.cs
+++ test/DomainModelUnitTest/Models/CanModifyConfirmableCommandTest.cs
@@ -1,6 +1,13 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using Xunit;
+
     public class CanModifyConfirmableCommandTest
     {
+        [Test]
+        public void SutIsModelCommand(CanModifyConfirmableCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<object>>(sut);
+        }
     }
 }
