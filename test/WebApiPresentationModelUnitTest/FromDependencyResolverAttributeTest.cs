fb7bf6a8359853f1fd84c91d292b649f64566c6b
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 23:39:25 +0900

    Addressed passing the type of the As property.

--- test/WebApiPresentationModelUnitTest/FromDependencyResolverAttributeTest.cs
+++ test/WebApiPresentationModelUnitTest/FromDependencyResolverAttributeTest.cs
@@ -5,6 +5,8 @@
     using System.Reflection;
     using System.Web.Http;
     using System.Web.Http.Controllers;
+    using Jwc.Experiment.Xunit;
+    using Ploeh.AutoFixture.Xunit;
     using Xunit;
 
     public class FromDependencyResolverAttributeTest
@@ -24,8 +26,8 @@
         }
 
         [Test]
-        public void GetBindingReturnsCorrectBinding(
-            FromDependencyResolverAttribute sut,
+        public void GetBindingReturnsCorrectBindingWhenAsIsNull(
+            [NoAutoProperties] FromDependencyResolverAttribute sut,
             HttpParameterDescriptor descriptor,
             Type parameterType)
         {
@@ -36,6 +38,41 @@
             var binding = Assert.IsAssignableFrom<DependencyParameterBinding>(actual);
             Assert.Equal(parameterType, binding.ParameterType);
             Assert.Equal(descriptor, binding.Descriptor);
+        }
+
+        [Test]
+        public void GetBindingReturnsCorrectBindingWhenAsIsNotNull(
+            FromDependencyResolverAttribute sut,
+            HttpParameterDescriptor descriptor,
+            Type parameterType)
+        {
+            var type = typeof(string);
+            sut.As = type;
+            descriptor.Of(x => x.ParameterType == parameterType);
+
+            var actual = sut.GetBinding(descriptor);
+
+            var binding = Assert.IsAssignableFrom<DependencyParameterBinding>(actual);
+            Assert.Equal(type, binding.ParameterType);
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> GetBindingWithIncorrectAsThrows()
+        {
+            var testData = new[]
+            {
+                new { As = typeof(object), ParameterType = typeof(int) },
+                new { As = typeof(object), ParameterType = typeof(string) },
+            };
+
+            return TestCases.WithArgs(testData)
+                .WithAuto<FromDependencyResolverAttribute, HttpParameterDescriptor>()
+                .Create((data, sut, descriptor) =>
+                {
+                    sut.As = data.As;
+                    descriptor.Of(x => x.ParameterType == data.ParameterType);
+                    Assert.Throws<InvalidCastException>(() => sut.GetBinding(descriptor));
+                });
         }
 
         protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
