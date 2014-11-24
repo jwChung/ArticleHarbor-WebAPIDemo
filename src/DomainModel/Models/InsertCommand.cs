da7ec6d4a89ce77a54a58ee274ea7558ae5eb2af
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 15:59:43 +0900

    Introduced the InsertCommand class.

---
+++ src/DomainModel/Models/InsertCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class InsertCommand : ModelCommand<Task<IModel>>
+    {
+        public override IEnumerable<Task<IModel>> Result
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/InsertCommandTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class InsertCommandTest : IdiomaticTest<InsertCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(InsertCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<Task<IModel>>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Result);
+        }
+    }
+}
