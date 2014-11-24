662a66b41f15183adf8285fd111578fb4f74f7c0
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 15:23:55 +0900

    Introduced the read-only properties of CanDeleteConfirmableCommand.

--- src/DomainModel/Models/CanDeleteConfirmableCommand.cs
+++ src/DomainModel/Models/CanDeleteConfirmableCommand.cs
@@ -2,13 +2,40 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Security.Principal;
     using System.Threading.Tasks;
+    using Repositories;
 
     public class CanDeleteConfirmableCommand : ModelCommand<Task>
     {
+        private readonly IPrincipal principal;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CanDeleteConfirmableCommand(IPrincipal principal, IUnitOfWork unitOfWork)
+        {
+            if (principal == null)
+                throw new ArgumentNullException("principal");
+
+            if (unitOfWork == null)
+                throw new ArgumentNullException("unitOfWork");
+            
+            this.principal = principal;
+            this.unitOfWork = unitOfWork;
+        }
+
         public override IEnumerable<Task> Result
         {
             get { yield break; }
+        }
+
+        public IPrincipal Principal
+        {
+            get { return this.principal; }
+        }
+
+        public IUnitOfWork UnitOfWork
+        {
+            get { return this.unitOfWork; }
         }
     }
 }
