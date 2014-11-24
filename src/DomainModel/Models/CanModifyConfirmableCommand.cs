ca5ac137b267c5193dc84028493a47170f5ded8b
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 13:20:19 +0900

    Implemented the Execute(Article) method of
    the CanModifyConfirmableCommand class.

--- src/DomainModel/Models/CanModifyConfirmableCommand.cs
+++ src/DomainModel/Models/CanModifyConfirmableCommand.cs
@@ -2,12 +2,50 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Security.Principal;
+    using Repositories;
 
     public class CanModifyConfirmableCommand : ModelCommand<object>
     {
+        private readonly IPrincipal principal;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CanModifyConfirmableCommand(IPrincipal principal, IUnitOfWork unitOfWork)
+        {
+            if (principal == null)
+                throw new ArgumentNullException("principal");
+
+            if (unitOfWork == null)
+                throw new ArgumentNullException("unitOfWork");
+
+            this.principal = principal;
+            this.unitOfWork = unitOfWork;
+        }
+
         public override IEnumerable<object> Result
         {
             get { throw new NotImplementedException(); }
+        }
+
+        public IPrincipal Principal
+        {
+            get { return this.principal; }
+        }
+
+        public IUnitOfWork UnitOfWork
+        {
+            get { return this.unitOfWork; }
+        }
+
+        public override IModelCommand<object> Execute(Article article)
+        {
+            if (this.Principal.IsInRole(Role.Administrator.ToString()))
+                return base.Execute(article);
+
+            if (this.Principal.IsInRole(Role.Author.ToString()) && article.UserId.Equals(this.principal.Identity.Name, StringComparison.CurrentCultureIgnoreCase))
+                return base.Execute(article);
+
+            throw new UnauthorizedException();
         }
     }
 }
--- test/DomainModelUnitTest/Models/CanModifyConfirmableCommandTest.cs
+++ test/DomainModelUnitTest/Models/CanModifyConfirmableCommandTest.cs
@@ -1,13 +1,82 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Ploeh.Albedo;
     using Xunit;
 
-    public class CanModifyConfirmableCommandTest
+    public class CanModifyConfirmableCommandTest : IdiomaticTest<CanModifyConfirmableCommand>
     {
         [Test]
         public void SutIsModelCommand(CanModifyConfirmableCommand sut)
         {
             Assert.IsAssignableFrom<ModelCommand<object>>(sut);
+        }
+
+        [Test]
+        public void ExecuteArticleWithInvalidRoleThrows(
+            CanModifyConfirmableCommand sut,
+            Article article)
+        {
+            Assert.Throws<UnauthorizedException>(() => sut.Execute(article));
+        }
+
+        [Test]
+        public void ExecuteArticleWithAdministratorRoleDoesNotThrow(
+            CanModifyConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Administrator") == true);
+            var actual = sut.Execute(article);
+            Assert.Equal(sut, actual);
+        }
+
+        [Test]
+        public void ExecuteArticleWithAuthorRoleAndOwnIdDoesNotThrow(
+            CanModifyConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            sut.Principal.Identity.Of(x => x.Name == article.UserId);
+
+            var actual = sut.Execute(article);
+
+            Assert.Equal(sut, actual);
+        }
+
+        [Test]
+        public void ExecuteArticleWithAuthorRoleAndIncorrectOwnIdThrow(
+            CanModifyConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            Assert.Throws<UnauthorizedException>(() => sut.Execute(article));
+        }
+
+        [Test]
+        public void ExecuteArticleIgnoresUserNameCase(
+            CanModifyConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            sut.Principal.Identity.Of(x => x.Name == article.UserId.ToUpper());
+
+            var actual = sut.Execute(article);
+
+            Assert.Equal(sut, actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Result);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.Execute(default(Article)));
+            yield return this.Methods.Select(x => x.Execute(default(Keyword)));
+            yield return this.Methods.Select(x => x.Execute(default(Bookmark)));
+            yield return this.Methods.Select(x => x.Execute(default(User)));
         }
     }
 }
