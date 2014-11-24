c81247a443e487b68d488e2744dd1880dc6a863d
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 16:21:25 +0900

    Implemented the Execute(User) method.

--- src/DomainModel/Models/InsertCommand.cs
+++ src/DomainModel/Models/InsertCommand.cs
@@ -2,29 +2,55 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Repositories;
 
     public class InsertCommand : ModelCommand<Task<IModel>>
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly IEnumerable<Task<IModel>> result;
 
         public InsertCommand(IUnitOfWork unitOfWork)
+            : this(unitOfWork, Enumerable.Empty<Task<IModel>>())
+        {
+        }
+
+        public InsertCommand(IUnitOfWork unitOfWork, IEnumerable<Task<IModel>> result)
         {
             if (unitOfWork == null)
                 throw new ArgumentNullException("unitOfWork");
 
+            if (result == null)
+                throw new ArgumentNullException("result");
+
             this.unitOfWork = unitOfWork;
+            this.result = result;
         }
 
         public override IEnumerable<Task<IModel>> Result
         {
-            get { yield break; }
+            get { return this.result; }
         }
 
         public IUnitOfWork UnitOfWork
         {
             get { return this.unitOfWork; }
+        }
+
+        public override IModelCommand<Task<IModel>> Execute(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException("user");
+
+            var task = Task.Run<IModel>(async () =>
+            {
+                return await this.unitOfWork.Users.InsertAsync(user);
+            });
+
+            return new InsertCommand(
+                this.unitOfWork,
+                this.result.Concat(new Task<IModel>[] { task }));
         }
     }
 }
--- test/DomainModelUnitTest/Models/InsertCommandTest.cs
+++ test/DomainModelUnitTest/Models/InsertCommandTest.cs
@@ -1,8 +1,10 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
+    using Ploeh.AutoFixture.Xunit;
     using Xunit;
 
     public class InsertCommandTest : IdiomaticTest<InsertCommand>
@@ -20,9 +22,29 @@
             Assert.Empty(actual);
         }
 
-        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        [Test]
+        public void ExecuteUserAddsUserToRepository(InsertCommand sut, User user, User newUser)
         {
-            yield return this.Properties.Select(x => x.Result);
+            sut.UnitOfWork.Users.Of(x => x.InsertAsync(user) == Task.FromResult(newUser));
+            
+            var actual = sut.Execute(user);
+
+            var command = Assert.IsAssignableFrom<InsertCommand>(actual);
+            var model = command.Result.Single().Result;
+            Assert.Equal(newUser, model);
+        }
+
+        [Test]
+        public void ExecuteUserReturnsCorrectResult(
+            [Frozen] IEnumerable<Task<IModel>> result,
+            [Greedy] InsertCommand sut,
+            User user)
+        {
+            var actual = sut.Execute(user);
+
+            var command = Assert.IsAssignableFrom<InsertCommand>(actual);
+            Assert.Equal(sut.UnitOfWork, command.UnitOfWork);
+            Assert.True(result.All(x => command.Result.Contains(x)));
         }
     }
 }
