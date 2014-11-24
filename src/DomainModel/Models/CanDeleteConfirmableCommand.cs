f45c3cc3a65883a46db8e28cfb5eab72949f760a
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 15:17:27 +0900

    Introduced the CanDeleteConfirmableCommand class.

---
+++ src/DomainModel/Models/CanDeleteConfirmableCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class CanDeleteConfirmableCommand : ModelCommand<Task>
+    {
+        public override IEnumerable<Task> Result
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/CanDeleteConfirmableCommandTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CanDeleteConfirmableCommandTest : IdiomaticTest<CanDeleteConfirmableCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(CanDeleteConfirmableCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<Task>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Result);
+        }
+    }
+}
