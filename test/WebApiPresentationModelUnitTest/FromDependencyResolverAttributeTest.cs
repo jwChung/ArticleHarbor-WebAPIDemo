141b137c62619614ef5cc5699ea7111a9b1fd741
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 23:18:21 +0900

    Introduced the As property.

--- test/WebApiPresentationModelUnitTest/FromDependencyResolverAttributeTest.cs
+++ test/WebApiPresentationModelUnitTest/FromDependencyResolverAttributeTest.cs
@@ -1,6 +1,8 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel
 {
     using System;
+    using System.Collections.Generic;
+    using System.Reflection;
     using System.Web.Http;
     using System.Web.Http.Controllers;
     using Xunit;
@@ -12,6 +14,13 @@
         public void SutIsParameterBindingAttribute(FromDependencyResolverAttribute sut)
         {
             Assert.IsAssignableFrom<ParameterBindingAttribute>(sut);
+        }
+
+        [Test]
+        public void AsIsReadWritable(ReadWritablePropertyAssertion assertion)
+        {
+            var asProperty = this.Properties.Select(x => x.As);
+            assertion.Verify(asProperty);
         }
 
         [Test]
@@ -27,6 +36,11 @@
             var binding = Assert.IsAssignableFrom<DependencyParameterBinding>(actual);
             Assert.Equal(parameterType, binding.ParameterType);
             Assert.Equal(descriptor, binding.Descriptor);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.As);
         }
     }
 }
