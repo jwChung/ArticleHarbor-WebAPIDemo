7eef02c3c5186e7f8c4700b33a61ec8817db5d12
Jin-Wook Chung <jwChung@hotmail.com>
2014-11-24 16:03:37 +0900

    Introduced the UnitOfWork property.

--- src/DomainModel/Models/InsertCommand.cs
+++ src/DomainModel/Models/InsertCommand.cs
@@ -3,12 +3,28 @@
     using System;
     using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Repositories;
 
     public class InsertCommand : ModelCommand<Task<IModel>>
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public InsertCommand(IUnitOfWork unitOfWork)
+        {
+            if (unitOfWork == null)
+                throw new ArgumentNullException("unitOfWork");
+
+            this.unitOfWork = unitOfWork;
+        }
+
         public override IEnumerable<Task<IModel>> Result
         {
             get { yield break; }
+        }
+
+        public IUnitOfWork UnitOfWork
+        {
+            get { return this.unitOfWork; }
         }
     }
 }
