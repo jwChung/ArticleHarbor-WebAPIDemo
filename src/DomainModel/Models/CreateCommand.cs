055047963f0fef47bb7e347e878dc5c11dd3f37b
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 00:12:05 +0900

    Introduced the properties of CreateCommand.

--- src/DomainModel/Models/CreateCommand.cs
+++ src/DomainModel/Models/CreateCommand.cs
@@ -1,8 +1,34 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
     using System.Collections.Generic;
+    using Repositories;
 
     public class CreateCommand : ModelCommand<IEnumerable<IModel>>
     {
+        private readonly IRepositories repositories;
+        private readonly IEnumerable<IModel> value;
+
+        public CreateCommand(IRepositories repositories, IEnumerable<IModel> value)
+        {
+            if (repositories == null)
+                throw new ArgumentNullException("repositories");
+
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            this.repositories = repositories;
+            this.value = value;
+        }
+
+        public IRepositories Repositories
+        {
+            get { return this.repositories; }
+        }
+
+        public override IEnumerable<IModel> Value
+        {
+            get { return this.value; }
+        }
     }
 }
