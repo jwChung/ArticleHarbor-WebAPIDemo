06b8c88983dbbef2737d619ad8f777e75a822f91
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-03 22:43:45 +0900

    Implemented the ExecuteAsync methods of CreateConfirmableCommand.

--- src/DomainModel/Models/CreateConfirmableCommand.cs
+++ src/DomainModel/Models/CreateConfirmableCommand.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Security.Principal;
+    using System.Threading.Tasks;
 
     public class CreateConfirmableCommand : ModelCommand<object>
     {
@@ -18,6 +19,35 @@
         public IPrincipal Principal
         {
             get { return this.principal; }
+        }
+
+        public override Task<IModelCommand<object>> ExecuteAsync(Article article)
+        {
+            this.ConfirmCanCreate();
+            return base.ExecuteAsync(article);
+        }
+
+        public override Task<IModelCommand<object>> ExecuteAsync(Bookmark bookmark)
+        {
+            this.ConfirmCanCreate();
+            return base.ExecuteAsync(bookmark);
+        }
+
+        public override Task<IModelCommand<object>> ExecuteAsync(Keyword keywords)
+        {
+            this.ConfirmCanCreate();
+            return base.ExecuteAsync(keywords);
+        }
+
+        public override Task<IModelCommand<object>> ExecuteAsync(User user)
+        {
+            throw new UnauthorizedException();
+        }
+
+        private void ConfirmCanCreate()
+        {
+            if (!this.principal.IsInRole("Author") && !this.principal.IsInRole("Administrator"))
+                throw new UnauthorizedException();
         }
     }
 }
