8a0dec64390befeaa49286b5906baa641fb3dfb3
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-03 23:35:24 +0900

    Introduced the Repositories class.

---
+++ src/EFPersistenceModel/Repositories.cs
@@ -0,0 +1,29 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using DomainModel.Models;
+    using DomainModel.Repositories;
+
+    public class Repositories : IRepositories
+    {
+        public IRepository<Keys<int>, Article> Articles
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<int, string>, Keyword> Keywords
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<string, int>, Bookmark> Bookmarks
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IRepository<Keys<string>, User> Users
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/RepositoriesTest.cs
@@ -0,0 +1,24 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using DomainModel.Repositories;
+    using Xunit;
+
+    public class RepositoriesTest : IdiomaticTest<Repositories>
+    {
+        [Test]
+        public void SutIsRepositories(Repositories sut)
+        {
+            Assert.IsAssignableFrom<IRepositories>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Articles);
+            yield return this.Properties.Select(x => x.Keywords);
+            yield return this.Properties.Select(x => x.Bookmarks);
+            yield return this.Properties.Select(x => x.Users);
+        }
+    }
+}
