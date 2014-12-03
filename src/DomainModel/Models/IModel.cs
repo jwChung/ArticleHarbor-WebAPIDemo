ace12e7a6c3ef6ab9fb796cbd37f2723933cbc54
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-03 22:49:59 +0900

    Improved the justification description.

--- src/DomainModel/Models/IModel.cs
+++ src/DomainModel/Models/IModel.cs
@@ -4,7 +4,7 @@
 
     public interface IModel
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "The method is appropriate as property can be serialized.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "The method is appropriate because properties can be serialized.")]
         IKeys GetKeys();
 
         Task<IModelCommand<TValue>> ExecuteAsync<TValue>(IModelCommand<TValue> command);
