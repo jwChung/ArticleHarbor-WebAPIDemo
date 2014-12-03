11dedea708ac9d4b1e95cc7e7d1be261380ea008
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-03 22:34:09 +0900

    Introduced the Principal property.

--- src/DomainModel/Models/CreateConfirmableCommand.cs
+++ src/DomainModel/Models/CreateConfirmableCommand.cs
@@ -1,6 +1,23 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
+    using System.Security.Principal;
+
     public class CreateConfirmableCommand : ModelCommand<object>
     {
+        private readonly IPrincipal principal;
+
+        public CreateConfirmableCommand(IPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException("principal");
+
+            this.principal = principal;
+        }
+
+        public IPrincipal Principal
+        {
+            get { return this.principal; }
+        }
     }
 }
