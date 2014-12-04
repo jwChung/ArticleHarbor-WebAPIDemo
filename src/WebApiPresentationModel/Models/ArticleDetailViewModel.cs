0402162b1dd7a474513159911e808281bbbcdf4d
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 15:41:19 +0900

    Introduced new properties.

--- src/WebApiPresentationModel/Models/ArticleDetailViewModel.cs
+++ src/WebApiPresentationModel/Models/ArticleDetailViewModel.cs
@@ -1,6 +1,34 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using DomainModel.Models;
+
     public class ArticleDetailViewModel
     {
+        private readonly Article article;
+        private readonly IEnumerable<Keyword> keywords;
+
+        public ArticleDetailViewModel(Article article, IEnumerable<Keyword> keywords)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            if (keywords == null)
+                throw new ArgumentNullException("keywords");
+
+            this.article = article;
+            this.keywords = keywords;
+        }
+
+        public Article Article
+        {
+            get { return this.article; }
+        }
+
+        public IEnumerable<Keyword> Keywords
+        {
+            get { return this.keywords; }
+        }
     }
 }
