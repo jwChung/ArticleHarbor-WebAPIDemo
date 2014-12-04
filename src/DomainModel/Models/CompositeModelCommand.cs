972d9c080c55d1e108c8950e228f7ff195ae5306
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 10:03:34 +0900

    Introduced the new properties and constructor.

--- src/DomainModel/Models/CompositeModelCommand.cs
+++ src/DomainModel/Models/CompositeModelCommand.cs
@@ -7,18 +7,49 @@
     public class CompositeModelCommand<TValue> : IModelCommand<TValue>
     {
         private readonly TValue value;
+        private readonly Func<TValue, TValue, TValue> composer;
+        private readonly IEnumerable<IModelCommand<TValue>> commands;
 
-        public CompositeModelCommand(TValue value)
+        public CompositeModelCommand(
+            TValue value,
+            Func<TValue, TValue, TValue> composer,
+            params IModelCommand<TValue>[] commands)
+            : this(value, composer, (IEnumerable<IModelCommand<TValue>>)commands)
+        {
+        }
+
+        public CompositeModelCommand(
+            TValue value,
+            Func<TValue, TValue, TValue> composer,
+            IEnumerable<IModelCommand<TValue>> commands)
         {
             if (value == null)
                 throw new ArgumentNullException("value");
 
+            if (composer == null)
+                throw new ArgumentNullException("composer");
+
+            if (commands == null)
+                throw new ArgumentNullException("commands");
+
             this.value = value;
+            this.composer = composer;
+            this.commands = commands;
         }
 
         public TValue Value
         {
             get { return this.value; }
+        }
+
+        public Func<TValue, TValue, TValue> Composer
+        {
+            get { return this.composer; }
+        }
+
+        public IEnumerable<IModelCommand<TValue>> Commands
+        {
+            get { return this.commands; }
         }
 
         public Task<IModelCommand<TValue>> ExecuteAsync(IEnumerable<User> users)
