6b339879d4f1edf7f5434039c2a8a9f133238433
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 09:58:23 +0900

    Introduced the CompositeModelCommand class.

---
+++ src/DomainModel/Models/CompositeModelCommand.cs
@@ -0,0 +1,64 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class CompositeModelCommand<TValue> : IModelCommand<TValue>
+    {
+        private readonly TValue value;
+
+        public CompositeModelCommand(TValue value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            this.value = value;
+        }
+
+        public TValue Value
+        {
+            get { return this.value; }
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(IEnumerable<User> users)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(IEnumerable<Article> articles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(IEnumerable<Keyword> keywords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(Keyword keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(IEnumerable<Bookmark> bookmarks)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IModelCommand<TValue>> ExecuteAsync(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/CompositeModelCommandTest.cs
@@ -0,0 +1,32 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public abstract class CompositeModelCommandTest<TValue> : IdiomaticTest<CompositeModelCommand<TValue>>
+    {
+        [Test]
+        public void SutIsModelCommand(CompositeModelCommand<TValue> sut)
+        {
+            Assert.IsAssignableFrom<IModelCommand<TValue>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(IEnumerable<User>)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(IEnumerable<Article>)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(IEnumerable<Keyword>)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(IEnumerable<Bookmark>)));
+
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(User)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Article)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Keyword)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Bookmark)));
+        }
+    }
+
+    public class CompositeModelCommandOfObjectTest : CompositeModelCommandTest<object>
+    {
+    }
+}
