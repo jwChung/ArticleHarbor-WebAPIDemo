23606a80db21d95b1dd10d47dae58a478a320257
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 11:49:04 +0900

    Implemented the CompositeEnumerableCommand class.

---
+++ src/DomainModel/Models/CompositeEnumerableCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CompositeEnumerableCommand<TValueElement> : CompositeModelCommand<IEnumerable<TValueElement>>
+    {
+        public CompositeEnumerableCommand(
+            params IModelCommand<IEnumerable<TValueElement>>[] commands)
+            : this((IEnumerable<IModelCommand<IEnumerable<TValueElement>>>)commands)
+        {
+        }
+
+        public CompositeEnumerableCommand(
+            IEnumerable<IModelCommand<IEnumerable<TValueElement>>> commands)
+            : base(new TValueElement[0], (x, y) => x.Concat(y), commands)
+        {
+        }
+    }
+}
