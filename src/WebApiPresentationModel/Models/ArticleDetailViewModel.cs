418602b0eb88f3231c31f30c301697ba790183f0
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-04 15:56:03 +0900

    Implemented properties of ArticleDetailViewModel.

--- src/WebApiPresentationModel/Models/ArticleDetailViewModel.cs
+++ src/WebApiPresentationModel/Models/ArticleDetailViewModel.cs
@@ -32,5 +32,46 @@
         {
             get { return this.keywords; }
         }
+
+        public int Id
+        {
+            get { return this.article.Id; }
+        }
+
+        public string Provider
+        {
+            get { return this.article.Provider; }
+        }
+
+        public string Guid
+        {
+            get { return this.article.Guid; }
+        }
+
+        public string Subject
+        {
+            get { return this.article.Subject; }
+        }
+
+        public string Body
+        {
+            get { return this.article.Body; }
+        }
+
+        public DateTime Date
+        {
+            get { return this.article.Date; }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1056:UriPropertiesShouldNotBeStrings", Justification = "To contract to Database.")]
+        public string Url
+        {
+            get { return this.article.Url; }
+        }
+
+        public string UserId
+        {
+            get { return this.article.UserId; }
+        }
     }
 }
--- test/WebApiPresentationModelUnitTest/Models/ArticleDetailViewModelTest.cs
+++ test/WebApiPresentationModelUnitTest/Models/ArticleDetailViewModelTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel.Models
 {
+    using System.Collections.Generic;
     using System.Reflection;
     using Newtonsoft.Json;
     using Xunit;
@@ -12,6 +13,74 @@
             var attribute = this.Properties.Select(x => x.Article)
                 .GetCustomAttribute<JsonIgnoreAttribute>();
             Assert.NotNull(attribute);
+        }
+
+        [Test]
+        public void IdIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Id;
+            Assert.Equal(sut.Article.Id, actual);
+        }
+
+        [Test]
+        public void ProviderIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Provider;
+            Assert.Equal(sut.Article.Provider, actual);
+        }
+
+        [Test]
+        public void GuidIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Guid;
+            Assert.Equal(sut.Article.Guid, actual);
+        }
+
+        [Test]
+        public void SubjectIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Subject;
+            Assert.Equal(sut.Article.Subject, actual);
+        }
+
+        [Test]
+        public void BodyIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Body;
+            Assert.Equal(sut.Article.Body, actual);
+        }
+
+        [Test]
+        public void DateIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Date;
+            Assert.Equal(sut.Article.Date, actual);
+        }
+
+        [Test]
+        public void UrlIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.Url;
+            Assert.Equal(sut.Article.Url, actual);
+        }
+
+        [Test]
+        public void UserIdIsCorrect(ArticleDetailViewModel sut)
+        {
+            var actual = sut.UserId;
+            Assert.Equal(sut.Article.UserId, actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Id);
+            yield return this.Properties.Select(x => x.Provider);
+            yield return this.Properties.Select(x => x.Guid);
+            yield return this.Properties.Select(x => x.Subject);
+            yield return this.Properties.Select(x => x.Body);
+            yield return this.Properties.Select(x => x.Date);
+            yield return this.Properties.Select(x => x.Url);
+            yield return this.Properties.Select(x => x.UserId);
         }
     }
 }
