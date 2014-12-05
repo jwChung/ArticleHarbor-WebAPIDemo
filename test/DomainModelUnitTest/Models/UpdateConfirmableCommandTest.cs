f65a774f76f9567b3a80b87fa0f4b797c6b5a2d1
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-05 14:17:27 +0900

    Introduced the UpdateConfirmableCommand class.

---
+++ test/DomainModelUnitTest/Models/UpdateConfirmableCommandTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class UpdateConfirmableCommandTest : IdiomaticTest<UpdateConfirmableCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(UpdateConfirmableCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Value);
+        }
+    }
+}
