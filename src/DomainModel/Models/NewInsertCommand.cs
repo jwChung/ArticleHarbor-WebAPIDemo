3cf0b7b5dd6d918106889719ed1c83982f682a64
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 15:28:24 +0900

    Introduced the Repositories property.

--- src/DomainModel/Models/NewInsertCommand.cs
+++ src/DomainModel/Models/NewInsertCommand.cs
@@ -2,24 +2,36 @@
 {
     using System;
     using System.Collections.Generic;
+    using Repositories;
 
     public class NewInsertCommand : ModelCommand<IEnumerable<IModel>>
     {
+        private readonly IRepositories repositories;
         private readonly IModelCommand<IEnumerable<IModel>> innerCommand;
 
         public NewInsertCommand(
+            IRepositories repositories,
             IModelCommand<IEnumerable<IModel>> innerCommand,
             IEnumerable<IModel> value) : base(value)
         {
+            if (repositories == null)
+                throw new ArgumentNullException("repositories");
+
             if (innerCommand == null)
                 throw new ArgumentNullException("innerCommand");
 
+            this.repositories = repositories;
             this.innerCommand = innerCommand;
         }
 
         public IModelCommand<IEnumerable<IModel>> InnerCommand
         {
             get { return this.innerCommand; }
+        }
+
+        public IRepositories Repositories
+        {
+            get { return this.repositories; }
         }
     }
 }
