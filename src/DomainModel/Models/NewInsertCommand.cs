934921f5fd882798bfb16dae43ea46f36d708a74
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 15:25:17 +0900

    Introduced the InnerCommand property.

--- src/DomainModel/Models/NewInsertCommand.cs
+++ src/DomainModel/Models/NewInsertCommand.cs
@@ -1,11 +1,25 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
     using System.Collections.Generic;
 
     public class NewInsertCommand : ModelCommand<IEnumerable<IModel>>
     {
-        public NewInsertCommand(IEnumerable<IModel> value) : base(value)
+        private readonly IModelCommand<IEnumerable<IModel>> innerCommand;
+
+        public NewInsertCommand(
+            IModelCommand<IEnumerable<IModel>> innerCommand,
+            IEnumerable<IModel> value) : base(value)
         {
+            if (innerCommand == null)
+                throw new ArgumentNullException("innerCommand");
+
+            this.innerCommand = innerCommand;
+        }
+
+        public IModelCommand<IEnumerable<IModel>> InnerCommand
+        {
+            get { return this.innerCommand; }
         }
     }
 }
