da66443e0128032ca78b8fd868720d9f442a4cf4
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 00:35:03 +0900

    Introduced the ModelCondition class.

---
+++ src/DomainModel/Models/ModelCodition.cs
@@ -0,0 +1,28 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class ModelCodition : IModelCondition
+    {
+        public virtual Task<bool> CanExecuteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<bool> CanExecuteAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<bool> CanExecuteAsync(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<bool> CanExecuteAsync(Keyword keyword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/ModelCoditionTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class ModelCoditionTest
+    {
+        [Test]
+        public void SutIsModelCondition(ModelCodition sut)
+        {
+            Assert.IsAssignableFrom<IModelCondition>(sut);
+        }
+    }
+}
