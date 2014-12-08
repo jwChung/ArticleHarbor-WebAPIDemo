949d13635af82126b72fcb6736f431d1df4f0267
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 07:58:50 +0900

    Introduced the Repositories property.

--- src/DomainModel/Models/UpdateCommand.cs
+++ src/DomainModel/Models/UpdateCommand.cs
@@ -2,12 +2,28 @@
 {
     using System;
     using System.Collections.Generic;
+    using Repositories;
 
     public class UpdateCommand : ModelCommand<IEnumerable<IModel>>
     {
+        private readonly IRepositories repositories;
+
+        public UpdateCommand(IRepositories repositories)
+        {
+            if (repositories == null)
+                throw new ArgumentNullException("repositories");
+
+            this.repositories = repositories;
+        }
+
         public override IEnumerable<IModel> Value
         {
             get { throw new NotImplementedException(); }
+        }
+
+        public IRepositories Repositories
+        {
+            get { return this.repositories; }
         }
     }
 }
