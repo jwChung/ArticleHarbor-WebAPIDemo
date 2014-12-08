2fc9b6f85aef846faf94ca2aaf0415b82a349125
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 16:48:11 +0900

    Implemented NullCommand.

---
+++ test/DomainModelUnitTest/Models/NullCommandTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class NullCommandTest : IdiomaticTest<NullCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(NullCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+
+        [Test]
+        public void ValueIsEmpty(NullCommand sut)
+        {
+            Assert.Empty(sut.Value);
+        }
+    }
+}
