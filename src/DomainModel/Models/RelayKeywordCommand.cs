d4451a4feb0731dd7831c4f36ded9426b8b2c8f6
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 16:25:00 +0900

    Implemented RelayKeywordCommand.

---
+++ src/DomainModel/Models/RelayKeywordCommand.cs
@@ -0,0 +1,59 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class RelayKeywordCommand : ModelCommand<IEnumerable<IModel>>
+    {
+        private readonly IModelCommand<IEnumerable<IModel>> innerCommand;
+        private readonly Func<string, IEnumerable<string>> nounExtractor;
+
+        public RelayKeywordCommand(
+            IModelCommand<IEnumerable<IModel>> innerCommand,
+            Func<string, IEnumerable<string>> nounExtractor,
+            IEnumerable<IModel> value) : base(value)
+        {
+            if (innerCommand == null)
+                throw new ArgumentNullException("innerCommand");
+
+            if (nounExtractor == null)
+                throw new ArgumentNullException("nounExtractor");
+
+            this.innerCommand = innerCommand;
+            this.nounExtractor = nounExtractor;
+        }
+
+        public IModelCommand<IEnumerable<IModel>> InnerCommand
+        {
+            get { return this.innerCommand; }
+        }
+
+        public Func<string, IEnumerable<string>> NounExtractor
+        {
+            get { return this.nounExtractor; }
+        }
+
+        public override Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsync(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            return this.ExecuteAsyncWith(article);
+        }
+
+        private async Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsyncWith(Article article)
+        {
+            var keywords = this.nounExtractor(article.Subject)
+                .Select(w => new Keyword(article.Id, w));
+
+            var newInnerComand = await this.innerCommand.ExecuteAsync(keywords);
+
+            return new RelayKeywordCommand(
+                this.innerCommand,
+                this.nounExtractor,
+                this.Value.Concat(newInnerComand.Value));
+        }
+    }
+}
