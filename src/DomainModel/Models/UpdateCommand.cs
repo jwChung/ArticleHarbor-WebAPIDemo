30cfe0bff7ba19c497e5b56b66dc9296cbd0323f
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 07:57:04 +0900

    Introduced the UpdateCommand class.

---
+++ src/DomainModel/Models/UpdateCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class UpdateCommand : ModelCommand<IEnumerable<IModel>>
+    {
+        public override IEnumerable<IModel> Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
