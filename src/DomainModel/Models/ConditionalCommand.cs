27a26f9fddd3981e45b649db6fe0427b1f60e48c
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 23:14:43 +0900

    Introduced the ConditionalCommand class.

---
+++ src/DomainModel/Models/ConditionalCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ConditionalCommand : ModelCommand<IEnumerable<IModel>>
+    {
+        private readonly IModelCommand<IEnumerable<IModel>> innerCommand;
+
+        public ConditionalCommand(IModelCommand<IEnumerable<IModel>> innerCommand)
+        {
+            if (innerCommand == null)
+                throw new ArgumentNullException("innerCommand");
+
+            this.innerCommand = innerCommand;
+        }
+
+        public IModelCommand<IEnumerable<IModel>> InnerCommand
+        {
+            get { return this.innerCommand; }
+        }
+
+        public override IEnumerable<IModel> Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
