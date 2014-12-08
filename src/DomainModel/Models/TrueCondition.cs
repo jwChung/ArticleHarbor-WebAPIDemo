35bd45032eb71cf07504af08691a68420207ed8f
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 00:40:48 +0900

    Implemented TrueCondition.

--- src/DomainModel/Models/TrueCondition.cs
+++ src/DomainModel/Models/TrueCondition.cs
@@ -3,26 +3,26 @@
     using System;
     using System.Threading.Tasks;
 
-    public class TrueCodition : IModelCondition
+    public class TrueCondition : IModelCondition
     {
         public virtual Task<bool> CanExecuteAsync(User user)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public virtual Task<bool> CanExecuteAsync(Article article)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public virtual Task<bool> CanExecuteAsync(Bookmark bookmark)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
 
         public virtual Task<bool> CanExecuteAsync(Keyword keyword)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(true);
         }
     }
 }
---
+++ test/DomainModelUnitTest/Models/TrueConditionTest.cs
@@ -0,0 +1,40 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Jwc.Experiment.Xunit;
+    using Xunit;
+
+    public class TrueConditionTest : IdiomaticTest<TrueCondition>
+    {
+        [Test]
+        public void SutIsModelCondition(TrueCondition sut)
+        {
+            Assert.IsAssignableFrom<IModelCondition>(sut);
+        }
+
+        [Test]
+        public IEnumerable<ITestCase> CanExecuteAsyncReturnsTrue()
+        {
+            yield return TestCase.WithAuto<TrueCondition, User>().Create(
+                (sut, user) => Assert.True(sut.CanExecuteAsync(user).Result));
+
+            yield return TestCase.WithAuto<TrueCondition, Article>().Create(
+                (sut, article) => Assert.True(sut.CanExecuteAsync(article).Result));
+
+            yield return TestCase.WithAuto<TrueCondition, Bookmark>().Create(
+                (sut, bookmark) => Assert.True(sut.CanExecuteAsync(bookmark).Result));
+
+            yield return TestCase.WithAuto<TrueCondition, Keyword>().Create(
+                (sut, keyword) => Assert.True(sut.CanExecuteAsync(keyword).Result));
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.CanExecuteAsync(default(User)));
+            yield return this.Methods.Select(x => x.CanExecuteAsync(default(Article)));
+            yield return this.Methods.Select(x => x.CanExecuteAsync(default(Bookmark)));
+            yield return this.Methods.Select(x => x.CanExecuteAsync(default(Keyword)));
+        }
+    }
+}
