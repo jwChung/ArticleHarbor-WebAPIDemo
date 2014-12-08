5bd243b9cd3627ec9895255abcdbea371c980a2a
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 15:22:04 +0900

    Introduced the NewInsertCommand class.

---
+++ src/DomainModel/Models/NewInsertCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+
+    public class NewInsertCommand : ModelCommand<IEnumerable<IModel>>
+    {
+        public NewInsertCommand(IEnumerable<IModel> value) : base(value)
+        {
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/NewInsertCommandTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class NewInsertCommandTest : IdiomaticTest<NewInsertCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(NewInsertCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+    }
+}
