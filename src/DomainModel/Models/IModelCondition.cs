d065b025aa75b3f49c7c54815cf2704c0588862c
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-08 23:11:01 +0900

    Introduced the IModelCondition interface.

---
+++ src/DomainModel/Models/IModelCondition.cs
@@ -0,0 +1,15 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Threading.Tasks;
+
+    public interface IModelCondition
+    {
+        Task<bool> CanExecuteAsync(User user);
+
+        Task<bool> CanExecuteAsync(Article article);
+
+        Task<bool> CanExecuteAsync(Bookmark bookmark);
+
+        Task<bool> CanExecuteAsync(Keyword keyword);
+    }
+}
