9e0e79985098e708e92ab804ebe688a829e9fc63
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 14:58:28 +0900

    Introduced the InClausePredicate class.

---
+++ src/DomainModel/Models/InClausePredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class InClausePredicate : IPredicate
+    {
+        public string SqlText
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public IEnumerable<IParameter> Parameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/InClausePredicateTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class InClausePredicateTest : IdiomaticTest<InClausePredicate>
+    {
+        [Test]
+        public void SutIsPredicate(InClausePredicate sut)
+        {
+            Assert.IsAssignableFrom<IPredicate>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.SqlText);
+            yield return this.Properties.Select(x => x.Parameters);
+        }
+    }
+}
