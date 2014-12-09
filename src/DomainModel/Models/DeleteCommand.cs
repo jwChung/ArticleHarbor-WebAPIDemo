06890eb5ab7bec587bdd25c8f06e3b3028f4f3b5
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 12:23:22 +0900

    Implemented ExecuteAsync(User).

--- src/DomainModel/Models/DeleteCommand.cs
+++ src/DomainModel/Models/DeleteCommand.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
     using Repositories;
 
     public class DeleteCommand : ModelCommand<IEnumerable<IModel>>
@@ -24,6 +25,20 @@
         public IRepositories Repositories
         {
             get { return this.repositories; }
+        }
+
+        public override Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsync(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException("user");
+
+            return this.ExecuteAsyncWith(user);
+        }
+
+        private async Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsyncWith(User user)
+        {
+            await this.Repositories.Users.DeleteAsync(new Keys<string>(user.Id));
+            return this;
         }
     }
 }
--- test/DomainModelUnitTest/Models/DeleteCommandTest.cs
+++ test/DomainModelUnitTest/Models/DeleteCommandTest.cs
@@ -2,6 +2,8 @@
 {
     using System.Collections.Generic;
     using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Xunit;
 
     public class DeleteCommandTest : IdiomaticTest<DeleteCommand>
@@ -17,6 +19,25 @@
         {
             var actual = sut.Value;
             Assert.Empty(actual);
+        }
+
+        [Test]
+        public void ExecuteAsyncUserCorrectlyDeletes(
+            DeleteCommand sut,
+            User user)
+        {
+            bool verifies = false;
+            var task = Task.Run(() =>
+            {
+                Thread.Sleep(300);
+                verifies = true;
+            });
+            sut.Repositories.Users.Of(x => x.DeleteAsync(new Keys<string>(user.Id)) == task);
+
+            var actual = sut.ExecuteAsync(user).Result;
+
+            Assert.Equal(actual, sut);
+            Assert.True(verifies);
         }
 
         protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
