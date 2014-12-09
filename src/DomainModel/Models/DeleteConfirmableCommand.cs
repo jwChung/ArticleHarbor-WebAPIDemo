50fa8cbe1f1db2f847af9d0bf9a0a6a3b29f8f09
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 11:46:31 +0900

    Implemented ExecuteAsync(Article).

--- src/DomainModel/Models/DeleteConfirmableCommand.cs
+++ src/DomainModel/Models/DeleteConfirmableCommand.cs
@@ -2,18 +2,49 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Security.Principal;
     using System.Threading.Tasks;
 
     public class DeleteConfirmableCommand : ModelCommand<IEnumerable<IModel>>
     {
+        private readonly IPrincipal principal;
+
+        public DeleteConfirmableCommand(IPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException("principal");
+
+            this.principal = principal;
+        }
+
         public override IEnumerable<IModel> Value
         {
             get { yield break; }
         }
 
+        public IPrincipal Principal
+        {
+            get { return this.principal; }
+        }
+
         public override Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsync(User user)
         {
             throw new NotSupportedException();
+        }
+
+        public override Task<IModelCommand<IEnumerable<IModel>>> ExecuteAsync(Article article)
+        {
+            if (article == null)
+                throw new ArgumentNullException("article");
+
+            if (this.principal.IsInRole("Administrator"))
+                return base.ExecuteAsync(article);
+
+            if (this.principal.IsInRole("Author") && article.UserId.Equals(
+                this.principal.Identity.Name, StringComparison.CurrentCultureIgnoreCase))
+                return base.ExecuteAsync(article);
+
+            throw new UnauthorizedException();
         }
     }
 }
--- test/DomainModelUnitTest/Models/DeleteConfirmableCommandTest.cs
+++ test/DomainModelUnitTest/Models/DeleteConfirmableCommandTest.cs
@@ -28,6 +28,59 @@
             Assert.Throws<NotSupportedException>(() => sut.ExecuteAsync(user).Result);
         }
 
+        [Test]
+        public void ExecuteAsyncArticleWithInvalidRoleThrows(
+            DeleteConfirmableCommand sut,
+            Article article)
+        {
+            Assert.Throws<UnauthorizedException>(() => sut.ExecuteAsync(article).Result);
+        }
+
+        [Test]
+        public void ExecuteAsyncArticleWithAdministratorRoleDoesNotThrow(
+            DeleteConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Administrator") == true);
+            var actual = sut.ExecuteAsync(article).Result;
+            Assert.Equal(sut, actual);
+        }
+        
+        [Test]
+        public void ExecuteAsyncArticleWithAuthorRoleAndCorrectOwnerIdDoesNotThrow(
+            DeleteConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            sut.Principal.Identity.Of(x => x.Name == article.UserId);
+
+            var actual = sut.ExecuteAsync(article).Result;
+
+            Assert.Equal(sut, actual);
+        }
+
+        [Test]
+        public void ExecuteAsyncArticleWithAuthorRoleAndIncorrectOwnerIdThrow(
+            DeleteConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            Assert.Throws<UnauthorizedException>(() => sut.ExecuteAsync(article).Result);
+        }
+
+        [Test]
+        public void ExecuteAsyncArticleIgnoresUserNameCase(
+            DeleteConfirmableCommand sut,
+            Article article)
+        {
+            sut.Principal.Of(x => x.IsInRole("Author") == true);
+            sut.Principal.Identity.Of(x => x.Name == article.UserId.ToUpper());
+
+            var actual = sut.ExecuteAsync(article).Result;
+
+            Assert.Equal(sut, actual);
+        }
+
         protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
         {
             yield return this.Properties.Select(x => x.Value);
