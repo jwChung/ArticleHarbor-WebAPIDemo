a2bb8c685a1c83667ad74254e13319d20e235eee
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 09:47:28 +0900

    Introduced the Repositories property.

--- src/DomainModel/Models/DeleteKeywordsCommand.cs
+++ src/DomainModel/Models/DeleteKeywordsCommand.cs
@@ -1,12 +1,29 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
     using System.Collections.Generic;
+    using Repositories;
 
     public class DeleteKeywordsCommand : ModelCommand<IEnumerable<IModel>>
     {
+        private readonly IRepositories repositories;
+
+        public DeleteKeywordsCommand(IRepositories repositories)
+        {
+            if (repositories == null)
+                throw new ArgumentNullException("repositories");
+
+            this.repositories = repositories;
+        }
+
         public override IEnumerable<IModel> Value
         {
             get { yield break; }
+        }
+
+        public IRepositories Repositories
+        {
+            get { return this.repositories; }
         }
     }
 }
