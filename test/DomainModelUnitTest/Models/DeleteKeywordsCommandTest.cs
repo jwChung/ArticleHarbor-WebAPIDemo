648d0e05a7b827c2e3e290576c9b521d47fbaedd
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 09:44:31 +0900

    Implemented Value property.

--- test/DomainModelUnitTest/Models/DeleteKeywordsCommandTest.cs
+++ test/DomainModelUnitTest/Models/DeleteKeywordsCommandTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
     using System.Collections.Generic;
+    using System.Reflection;
     using Xunit;
 
     public class DeleteKeywordsCommandTest : IdiomaticTest<DeleteKeywordsCommand>
@@ -9,6 +10,18 @@
         public void SutIsModelCommand(DeleteKeywordsCommand sut)
         {
             Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+
+        [Test]
+        public void ValueIsEmpty(DeleteKeywordsCommand sut)
+        {
+            var actual = sut.Value;
+            Assert.Empty(actual);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Value);
         }
     }
 }
