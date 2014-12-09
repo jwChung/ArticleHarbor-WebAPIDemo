3072e948c0b809e869751d7a8452b335c1674944
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 10:02:40 +0900

    Introduced the Repositories property.

--- src/DomainModel/Models/UpdateKeywordsCondition.cs
+++ src/DomainModel/Models/UpdateKeywordsCondition.cs
@@ -1,6 +1,23 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
+    using Repositories;
+
     public class UpdateKeywordsCondition : TrueCondition
     {
+        private readonly IRepositories repositories;
+
+        public UpdateKeywordsCondition(IRepositories repositories)
+        {
+            if (repositories == null)
+                throw new ArgumentNullException("repositories");
+
+            this.repositories = repositories;
+        }
+
+        public IRepositories Repositories
+        {
+            get { return this.repositories; }
+        }
     }
 }
