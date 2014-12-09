91ee02db1691bd4a0253d355160d0689face1366
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-09 09:43:33 +0900

    Introduced the DeleteKeywordsCommand class.

---
+++ test/DomainModelUnitTest/Models/DeleteKeywordsCommandTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class DeleteKeywordsCommandTest : IdiomaticTest<DeleteKeywordsCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(DeleteKeywordsCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+    }
+}
