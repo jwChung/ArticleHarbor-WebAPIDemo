a4d74105588dbcf980b9bfac608219aa37d46f74
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 17:42:34 +0900

    Introduced the Pridicates property.

--- src/DomainModel/Models/AndPredicate.cs
+++ src/DomainModel/Models/AndPredicate.cs
@@ -1,9 +1,20 @@
 ﻿namespace ArticleHarbor.DomainModel.Models
 {
+    using System;
     using System.Collections.Generic;
 
     public class AndPredicate : IPredicate
     {
+        private readonly IEnumerable<IPredicate> predicates;
+
+        public AndPredicate(IEnumerable<IPredicate> predicates)
+        {
+            if (predicates == null)
+                throw new ArgumentNullException("predicates");
+
+            this.predicates = predicates;
+        }
+
         public string SqlText
         {
             get { throw new System.NotImplementedException(); }
@@ -12,6 +23,11 @@
         public IEnumerable<IParameter> Parameters
         {
             get { throw new System.NotImplementedException(); }
+        }
+
+        public IEnumerable<IPredicate> Predicates
+        {
+            get { return this.predicates; }
         }
     }
 }
