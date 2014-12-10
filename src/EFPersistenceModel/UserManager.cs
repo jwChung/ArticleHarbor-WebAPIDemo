4529bdd861f9722449402caaf1e2f13c11a27483
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 19:13:59 +0900

    Introduced the UserManager class.

---
+++ src/EFPersistenceModel/UserManager.cs
@@ -0,0 +1,24 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System;
+    using System.Threading.Tasks;
+    using DomainModel.Models;
+    using DomainModel.Repositories;
+    
+    public class UserManager : IUserManager
+    {
+        public UserManager()
+        {
+        }
+
+        public Task<User> FindAsync(string id, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/EFPersistenceModelUnitTest/UserManagerTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.EFPersistenceModel
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using DomainModel.Repositories;
+    using Xunit;
+
+    public class UserManagerTest : IdiomaticTest<UserManager>
+    {
+        [Test]
+        public void SutIsUserManager(UserManager sut)
+        {
+            Assert.IsAssignableFrom<IUserManager>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.FindAsync(null, null));
+        }
+    }
+}
