2bd5179aae7d9ca5300130fb78a7e1d5986fbe34
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 17:40:58 +0900

    Introduced the AndPredicate class.

---
+++ src/DomainModel/Models/AndPredicate.cs
@@ -0,0 +1,17 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+
+    public class AndPredicate : IPredicate
+    {
+        public string SqlText
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public IEnumerable<IParameter> Parameters
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/AndPredicateTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class AndPredicateTest : IdiomaticTest<AndPredicate>
+    {
+        [Test]
+        public void SutIsPredicate(AndPredicate sut)
+        {
+            Assert.IsAssignableFrom<IPredicate>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.SqlText);
+            yield return this.Properties.Select(x => x.Parameters);
+        }
+    }
+}
