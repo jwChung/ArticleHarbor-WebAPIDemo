4e88e056c746e8156b6baf5f30c0fb5b6a100416
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 10:30:28 +0900

    Introduced the DeleteBookmarksCommand class.

---
+++ src/DomainModel/Models/DeleteBookmarksCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+
+    public class DeleteBookmarksCommand : ModelCommand<IEnumerable<IModel>>
+    {
+        public override IEnumerable<IModel> Value
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/DeleteBookmarksCommandTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class DeleteBookmarksCommandTest : IdiomaticTest<DeleteBookmarksCommand>
+    {
+        [Test]
+        public void SutIsModelCommand(DeleteBookmarksCommand sut)
+        {
+            Assert.IsAssignableFrom<ModelCommand<IEnumerable<IModel>>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Value);
+        }
+    }
+}
