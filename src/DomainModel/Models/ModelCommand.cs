2eb01a9154631785a42c4eba073c2d7cb99ac929
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 14:48:39 +0900

    Addressed ModelCommand.

--- src/DomainModel/Models/ModelCommand.cs
+++ src/DomainModel/Models/ModelCommand.cs
@@ -4,30 +4,26 @@
     using System.Linq;
     using System.Threading.Tasks;
 
-    public abstract class ModelCommand<TReturn> : IModelCommand<TReturn>
+    public class ModelCommand<TReturn> : IModelCommand<TReturn>
     {
         public virtual Task<IEnumerable<TReturn>> ExecuteAsync(User user)
         {
-            ////return Task.FromResult<IModelCommand<TReturn>>(this);
-            return null;
+            return Task.FromResult(Enumerable.Empty<TReturn>());
         }
 
         public virtual Task<IEnumerable<TReturn>> ExecuteAsync(Article article)
         {
-            ////return Task.FromResult<IModelCommand<TReturn>>(this);
-            return null;
+            return Task.FromResult(Enumerable.Empty<TReturn>());
         }
 
         public virtual Task<IEnumerable<TReturn>> ExecuteAsync(Keyword keyword)
         {
-            ////return Task.FromResult<IModelCommand<TReturn>>(this);
-            return null;
+            return Task.FromResult(Enumerable.Empty<TReturn>());
         }
 
         public virtual Task<IEnumerable<TReturn>> ExecuteAsync(Bookmark bookmark)
         {
-            ////return Task.FromResult<IModelCommand<TReturn>>(this);
-            return null;
+            return Task.FromResult(Enumerable.Empty<TReturn>());
         }
     }
 }
