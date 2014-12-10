6639feeb02dee9b71cb21b224f0c6e18243af0c4
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-10 20:08:21 +0900

    Implemetned FindAsync.

--- src/EFPersistenceModel/UserManager.cs
+++ src/EFPersistenceModel/UserManager.cs
@@ -1,24 +1,61 @@
 ﻿namespace ArticleHarbor.EFPersistenceModel
 {
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
     using DomainModel.Models;
     using DomainModel.Repositories;
-    
+    using EFDataAccess;
+    using User = DomainModel.Models.User;
+
     public class UserManager : IUserManager
     {
-        public UserManager()
+        private readonly ArticleHarborDbContext context;
+
+        public UserManager(ArticleHarborDbContext context)
         {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            this.context = context;
+        }
+
+        public ArticleHarborDbContext Context
+        {
+            get { return this.context; }
         }
 
         public Task<User> FindAsync(string id, string password)
         {
-            throw new NotImplementedException();
+            if (id == null)
+                throw new ArgumentNullException("id");
+
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            return this.FindAsyncWith(id, password);
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+        }
+
+        private async Task<User> FindAsyncWith(string id, string password)
+        {
+            var user = await this.context.UserManager.FindByIdAsync(id);
+            if (user == null)
+                return null;
+
+            var persistence = await this.context.UserManager.FindAsync(user.UserName, password);
+            if (persistence == null)
+                return null;
+
+            var roleNames = await this.context.UserManager.GetRolesAsync(persistence.Id);
+
+            return new User(
+                persistence.Id,
+                (Role)Enum.Parse(typeof(Role), roleNames.Single()),
+                persistence.ApiKey);
         }
     }
 }
--- test/EFPersistenceModelUnitTest/UserManagerTest.cs
+++ test/EFPersistenceModelUnitTest/UserManagerTest.cs
@@ -1,7 +1,9 @@
 ﻿namespace ArticleHarbor.EFPersistenceModel
 {
+    using System;
     using System.Collections.Generic;
     using System.Reflection;
+    using DomainModel.Models;
     using DomainModel.Repositories;
     using Xunit;
 
@@ -13,9 +15,33 @@
             Assert.IsAssignableFrom<IUserManager>(sut);
         }
 
+        [Test]
+        public void FindAsyncWithIdAndPasswordReturnsCorrectUser(UserManager sut)
+        {
+            var actual = sut.FindAsync("user1", "password1").Result;
+
+            Assert.Equal("user1", actual.Id);
+            Assert.Equal(Guid.Parse("692c7798206844b88ba9a660e3374eef"), actual.ApiKey);
+            Assert.Equal(Role.Administrator, actual.Role);
+        }
+
+        [Test]
+        public void FindAsyncWithIncorectIdReturnsNullUser(UserManager sut)
+        {
+            var actual = sut.FindAsync("user", "password1").Result;
+            Assert.Null(actual);
+        }
+
+        [Test]
+        public void FindAsyncWithIncorectPasswordReturnsNullUser(UserManager sut)
+        {
+            var actual = sut.FindAsync("user1", "password").Result;
+            Assert.Null(actual);
+        }
+
         protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
         {
-            yield return this.Methods.Select(x => x.FindAsync(null, null));
+            yield break;
         }
     }
 }
