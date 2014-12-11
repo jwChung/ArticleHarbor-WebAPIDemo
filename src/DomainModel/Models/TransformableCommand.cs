b61c00c2e3d164bfc91310e65ee1c6a51206139e
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-11 10:26:55 +0900

    Introduced some properties.

--- src/DomainModel/Models/TransformableCommand.cs
+++ src/DomainModel/Models/TransformableCommand.cs
@@ -6,6 +6,32 @@
 
     public class TransformableCommand<TReturn> : IModelCommand<TReturn>
     {
+        private readonly IModelTransformer transformer;
+        private readonly IModelCommand<TReturn> innerCommand;
+
+        public TransformableCommand(
+            IModelTransformer transformer, IModelCommand<TReturn> innerCommand)
+        {
+            if (transformer == null)
+                throw new ArgumentNullException("transformer");
+
+            if (innerCommand == null)
+                throw new ArgumentNullException("innerCommand");
+
+            this.transformer = transformer;
+            this.innerCommand = innerCommand;
+        }
+
+        public IModelTransformer Transformer
+        {
+            get { return this.transformer; }
+        }
+
+        public IModelCommand<TReturn> InnerCommand
+        {
+            get { return this.innerCommand; }
+        }
+
         public Task<IEnumerable<TReturn>> ExecuteAsync(User user)
         {
             throw new NotImplementedException();
