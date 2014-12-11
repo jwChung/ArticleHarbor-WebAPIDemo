2838fc9681b22f252384e21523cd830ef5555882
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-11 10:22:48 +0900

    Introduced the TransformableCommand class.

---
+++ src/DomainModel/Models/TransformableCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class TransformableCommand<TReturn> : IModelCommand<TReturn>
+    {
+        public Task<IEnumerable<TReturn>> ExecuteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TReturn>> ExecuteAsync(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TReturn>> ExecuteAsync(Keyword keyword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TReturn>> ExecuteAsync(Bookmark bookmark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Models/TransformableCommandTest.cs
@@ -0,0 +1,31 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public abstract class TransformableCommandTest<TReturn> : IdiomaticTest<TransformableCommand<TReturn>>
+    {
+        [Test]
+        public void SutIsModelCommand(TransformableCommand<TReturn> sut)
+        {
+            Assert.IsAssignableFrom<IModelCommand<TReturn>>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyGuardClause()
+        {
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(User)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Article)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Keyword)));
+            yield return this.Methods.Select(x => x.ExecuteAsync(default(Bookmark)));
+        }
+    }
+
+    public class TransformableCommandOfObjectTest : TransformableCommandTest<object>
+    {
+    }
+
+    public class TransformableCommandOfInt32Test : TransformableCommandTest<int>
+    {
+    }
+}
