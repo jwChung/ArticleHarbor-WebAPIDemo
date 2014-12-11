883f7c450b4fb8383a4ce577c184e2caf9894762
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-11 10:50:21 +0900

    Introduced the SubjectFromBodyTransformer class.

---
+++ src/DomainModel/Models/SubjectFromBodyTransformer.cs
@@ -0,0 +1,6 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    public class SubjectFromBodyTransformer : ModelTransformer
+    {
+    }
+}
---
+++ test/DomainModelUnitTest/Models/SubjectFromBodyTransformerTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Models
+{
+    using Xunit;
+
+    public class SubjectFromBodyTransformerTest : IdiomaticTest<SubjectFromBodyTransformer>
+    {
+        [Test]
+        public void SutIsModelTransformer(SubjectFromBodyTransformer sut)
+        {
+            Assert.IsAssignableFrom<IModelTransformer>(sut);
+        }
+    }
+}
