797d1ca0f34f8f11e777779e6a8f8a38d5cf0c41
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-11 13:56:21 +0900

    Renewed Program class.

--- src/ArticleCollectorDaemon/Program.cs
+++ src/ArticleCollectorDaemon/Program.cs
@@ -16,36 +16,55 @@
     {
         private static void Main()
         {
-            CollectFacebookArticles().Wait();
+            Task.WaitAll(
+                CollectHaniArticles("user1"),
+                CollectFacebookArticles("user2", /* ASP.NET Korea group */ "177323639028540"),
+                CollectFacebookArticles("user2", /* C# study group */ "200708093411111"));
         }
 
-        private static async Task CollectFacebookArticles()
+        private static async Task CollectHaniArticles(string author)
         {
-            var context = CreateDbContext();
-            var repositories = new Repositories(context);
+            using (var context = CreateDbContext())
+            {
+                var repositories = new Repositories(context);
+                var articles = await new HaniRssCollector(author).CollectAsync();
+                await new CompositeModel(articles).ExecuteAsync(CreateCommand("Hani", repositories));
+                context.SaveChanges();
+            }
+        }
 
-            var articles = await new CompositeCollector(
-                new FacebookRssCollector("user2", "177323639028540"),                 // ASP.NET Korea group
-                new FacebookRssCollector("user2", "200708093411111")).CollectAsync(); // C# study group
+        private static async Task CollectFacebookArticles(string author, string facebookId)
+        {
+            using (var context = CreateDbContext())
+            {
+                var repositories = new Repositories(context);
+                var articles = await new FacebookRssCollector(author, facebookId).CollectAsync();
+                await new CompositeModel(articles).ExecuteAsync(CreateFacebookCommand(repositories));
+                context.SaveChanges();
+            }
+        }
 
-            var command = new TransformableCommand<IModel>(
-                    new RemoveUnnecessaryContentTransformer(),
-                    new TransformableCommand<IModel>(
-                        new SubjectFromBodyTransformer(50),
-                        new CompositeCommand<IModel>(
-                            new BeforeInsertMessageCommand(),
-                            new InsertCommand(
-                                repositories,
-                                new RelayKeywordsCommand(
-                                    KoreanNounExtractor.Execute,
-                                    new InsertCommand(
-                                        repositories,
-                                        new ModelCommand<IModel>()))),
-                            new AfterInsertMessageCommand())));
-            
-            await new CompositeModel(articles).ExecuteAsync(command);
+        private static IModelCommand<IModel> CreateFacebookCommand(Repositories repositories)
+        {
+            return new TransformableCommand<IModel>(
+                new RemoveUnnecessaryContentTransformer(),
+                new TransformableCommand<IModel>(
+                    new SubjectFromBodyTransformer(50),
+                    CreateCommand("Facebook", repositories)));
+        }
 
-            context.SaveChanges();
+        private static IModelCommand<IModel> CreateCommand(string name, Repositories repositories)
+        {
+            return new CompositeCommand<IModel>(
+                new BeforeLogCommand(name),
+                new InsertCommand(
+                    repositories,
+                    new RelayKeywordsCommand(
+                        KoreanNounExtractor.Execute,
+                        new InsertCommand(
+                            repositories,
+                            new ModelCommand<IModel>()))),
+                new AfterLogCommand(name));
         }
 
         private static ArticleHarborDbContext CreateDbContext()
@@ -59,25 +78,39 @@
             public override Task<Article> TransformAsync(Article article)
             {
                 var index = article.Body.IndexOf(':');
-                var newBody = article.Body.Remove(0, index + 1);
+                var newBody = article.Body.Remove(0, index + 2);
                 return Task.FromResult(article.WithBody(newBody));
             }
         }
         
-        private class BeforeInsertMessageCommand : ModelCommand<IModel>
+        private class BeforeLogCommand : ModelCommand<IModel>
         {
+            private string name;
+
+            public BeforeLogCommand(string name)
+            {
+                this.name = name;
+            }
+
             public override Task<IEnumerable<IModel>> ExecuteAsync(Article article)
             {
-                Console.WriteLine("Adding: " + article.Subject + "...");
+                Console.WriteLine(string.Format("{0} (Adding): {1}", this.name.PadLeft(10), article.Subject));
                 return base.ExecuteAsync(article);
             }
         }
 
-        private class AfterInsertMessageCommand : ModelCommand<IModel>
+        private class AfterLogCommand : ModelCommand<IModel>
         {
+            private string name;
+
+            public AfterLogCommand(string name)
+            {
+                this.name = name;
+            }
+
             public override Task<IEnumerable<IModel>> ExecuteAsync(Article article)
             {
-                Console.WriteLine("Added: " + article.Subject);
+                Console.WriteLine(string.Format("{0}  (Added): {1}", this.name.PadLeft(10), article.Subject));
                 return base.ExecuteAsync(article);
             }
         }
