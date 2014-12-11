1fd620656083d19fb00fa3e1ffdf86b4922c248d
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-11 10:51:57 +0900

    Introduced the Length property.

--- src/DomainModel/Models/SubjectFromBodyTransformer.cs
+++ src/DomainModel/Models/SubjectFromBodyTransformer.cs
@@ -2,5 +2,16 @@
 {
     public class SubjectFromBodyTransformer : ModelTransformer
     {
+        private readonly int length;
+
+        public SubjectFromBodyTransformer(int length)
+        {
+            this.length = length;
+        }
+
+        public int Length
+        {
+            get { return this.length; }
+        }
     }
 }
