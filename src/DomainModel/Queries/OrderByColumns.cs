42d1fc203d7931cdfe03a95a218e5830e6f8a0bb
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-12 16:30:27 +0900

    Introduced the OrderByColumns class.

---
+++ src/DomainModel/Queries/OrderByColumns.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
+    public class OrderByColumns : IOrderByColumns
+    {
+        public IEnumerator<IOrderByColumn> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/OrderByColumnsTest.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using Xunit;
+
+    public class OrderByColumnsTest : IdiomaticTest<OrderByColumns>
+    {
+        [Test]
+        public void SutIsOrderByColumns(OrderByColumns sut)
+        {
+            Assert.IsAssignableFrom<IOrderByColumns>(sut);
+        }
+    }
+}
