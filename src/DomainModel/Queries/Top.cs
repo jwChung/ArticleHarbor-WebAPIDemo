bcfaa91e81dcdb975528dd07d7da70c453e36445
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-12 16:00:22 +0900

    Implemented the Top class.

---
+++ src/DomainModel/Queries/Top.cs
@@ -0,0 +1,39 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    public class Top : ITop
+    {
+        public static readonly ITop None = new Top(int.MinValue);
+        private readonly int count;
+
+        public Top(int count)
+        {
+            this.count = count;
+        }
+        
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            return this.Equals((Top)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.count;
+        }
+
+        protected bool Equals(Top other)
+        {
+            return this.count == other.count;
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/TopTest.cs
@@ -0,0 +1,41 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using Xunit;
+
+    public class TopTest : IdiomaticTest<Top>
+    {
+        [Test]
+        public void SutIsTop(Top sut)
+        {
+            Assert.IsAssignableFrom<ITop>(sut);
+        }
+
+        [Test]
+        public void NoneIsCorrect()
+        {
+            var actual = Top.None;
+            var top = Assert.IsAssignableFrom<ITop>(actual);
+            Assert.Equal(int.MinValue, top.Count);
+        }
+
+        [Test]
+        public void NoneAlwasReturnsSameInstance()
+        {
+            var actual = Top.None;
+            Assert.Same(Top.None, actual);
+        }
+
+        [Test]
+        public void EqualsEqualsWithSameCount(Top sut)
+        {
+            var other = new Top(sut.Count);
+            Assert.Equal(other, sut);
+        }
+
+        [Test]
+        public void EqualsDoesNotEqualWithOtherCount(Top sut, Top other)
+        {
+            Assert.NotEqual(other, sut);
+        }
+    }
+}
