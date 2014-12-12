544163c554b57f858577d38615cda13217c2f5f6
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-12 16:34:45 +0900

    Introduced the Columns property.

--- src/DomainModel/Queries/OrderByColumns.cs
+++ src/DomainModel/Queries/OrderByColumns.cs
@@ -7,6 +7,21 @@
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
     public class OrderByColumns : IOrderByColumns
     {
+        private readonly IEnumerable<IOrderByColumn> columns;
+
+        public OrderByColumns(IEnumerable<IOrderByColumn> columns)
+        {
+            if (columns == null)
+                throw new ArgumentNullException("columns");
+
+            this.columns = columns;
+        }
+
+        public IEnumerable<IOrderByColumn> Columns
+        {
+            get { return this.columns; }
+        }
+
         public IEnumerator<IOrderByColumn> GetEnumerator()
         {
             throw new NotImplementedException();
