677c2096e3857121569c1bb3869200965a3c7dc3
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 12:56:22 +0900

    Introduced the OperablePredicate class.

---
+++ src/DomainModel/Queries/OperablePredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class OperablePredicate : IPredicate
+    {
+        public string SqlText
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<IParameter> Parameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/OperablePredicateTest.cs
@@ -0,0 +1,21 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class OperablePredicateTest : IdiomaticTest<OperablePredicate>
+    {
+        [Test]
+        public void SutIsPredicate(OperablePredicate sut)
+        {
+            Assert.IsAssignableFrom<IPredicate>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.SqlText);
+            yield return this.Properties.Select(x => x.Parameters);
+        }
+    }
+}
