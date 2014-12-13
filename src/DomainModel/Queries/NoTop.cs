ba9d765077a7ed142c5eb956f16a29b23bbdfd01
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 14:36:24 +0900

    Introduced the NoTop class.

---
+++ src/DomainModel/Queries/NoTop.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    public class NoTop : ITop
+    {
+        public int Count
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/NoTopTest.cs
@@ -0,0 +1,20 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Xunit;
+
+    public class NoTopTest : IdiomaticTest<NoTop>
+    {
+        [Test]
+        public void SutIsTop(NoTop sut)
+        {
+            Assert.IsAssignableFrom<ITop>(sut);
+        }
+
+        protected override IEnumerable<MemberInfo> ExceptToVerifyInitialization()
+        {
+            yield return this.Properties.Select(x => x.Count);
+        }
+    }
+}
