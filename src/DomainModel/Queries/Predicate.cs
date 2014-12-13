d01e70d302591bf8aa613cdd590dc87e7d0b119b
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 13:46:24 +0900

    Implemented Equal method.

---
+++ src/DomainModel/Queries/Predicate.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    public class Predicate
+    {
+        public static IPredicate Equal(string columnName, object value)
+        {
+            return new OperablePredicate(columnName, "=", value);
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/PredicateTest.cs
@@ -0,0 +1,15 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using Xunit;
+
+    public class PredicateTest : IdiomaticTest<Predicate>
+    {
+        [Test]
+        public void EqualReturnsCorrectResult(string columnName, object value)
+        {
+            var expected = new OperablePredicate(columnName, "=", value);
+            var actual = Predicate.Equal(columnName, value);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
