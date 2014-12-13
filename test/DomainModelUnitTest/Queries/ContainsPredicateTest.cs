fcec55c21813d889b2fef37c4c188f7cd3bd2bbb
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 22:11:42 +0900

    Implemented ContainsPredicate.

---
+++ test/DomainModelUnitTest/Queries/ContainsPredicateTest.cs
@@ -0,0 +1,33 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using Xunit;
+
+    public class ContainsPredicateTest : IdiomaticTest<ContainsPredicate>
+    {
+        [Test]
+        public void SutIsOperablePredicate(ContainsPredicate sut)
+        {
+            Assert.IsAssignableFrom<OperablePredicate>(sut);
+        }
+
+        [Test]
+        public void OperatorNameIsCorrect(ContainsPredicate sut)
+        {
+            var actual = sut.OperatorName;
+            Assert.Equal("LIKE", actual);
+        }
+
+        [Test]
+        public void ValueIsCorrect(string columnName, string[] values)
+        {
+            var sut = new ContainsPredicate(
+                columnName,
+                string.Join("%", values));
+            var expected = string.Format("%{0}%", string.Join("[%]", values));
+
+            var actual = sut.Value;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
