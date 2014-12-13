e1986b88fcf1a62665f54977659aa09acfdfd19f
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 14:30:08 +0900

    Implemented NoneOrderByColumns.

---
+++ src/DomainModel/Queries/NoneOrderByColumns.cs
@@ -0,0 +1,30 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "The main responsibility of this class isn't to be a 'collection' (which, by the way, it isn't - it's just an Iterator).")]
+    public sealed class NoneOrderByColumns : IOrderByColumns
+    {
+        public IEnumerator<IOrderByColumn> GetEnumerator()
+        {
+            yield break;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield break;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj.GetType() == this.GetType();
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
---
+++ test/DomainModelUnitTest/Queries/NoneOrderByColumnsTest.cs
@@ -0,0 +1,59 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System.Linq;
+    using Xunit;
+
+    public class NoneOrderByColumnsTest : IdiomaticTest<NoneOrderByColumns>
+    {
+        [Test]
+        public void SutIsOrderByColumns(NoneOrderByColumns sut)
+        {
+            Assert.IsAssignableFrom<IOrderByColumns>(sut);
+        }
+
+        [Test]
+        public void GetEnumeratorReturnsCorrectResult(NoneOrderByColumns sut)
+        {
+            Assert.Empty(sut.ToArray());
+        }
+
+        [Test]
+        public void ExplicitGetEnumeratorReturnsCorrectResult(NoneOrderByColumns sut)
+        {
+            Assert.Empty(sut);
+        }
+
+        [Test]
+        public void EqualsDoesNotEqualOtherOfDiffrentType(
+            NoneOrderByColumns sut,
+            OrderByColumns other)
+        {
+            var actual = sut.Equals(other);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void EqualsEqualsOtherOfSameType(
+            NoneOrderByColumns sut,
+            NoneOrderByColumns other)
+        {
+            var actual = sut.Equals(other);
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void EqualsDoesNotEqualNull(
+            NoneOrderByColumns sut)
+        {
+            var actual = sut.Equals(null);
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void GetHashCodeReturnsZero(NoneOrderByColumns sut)
+        {
+            var actual = sut.GetHashCode();
+            Assert.Equal(0, actual);
+        }
+    }
+}
