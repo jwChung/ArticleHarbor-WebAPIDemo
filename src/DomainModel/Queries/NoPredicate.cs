2c26a40dd6b42bdbb2318a9603fea38cd0b2147a
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 14:47:09 +0900

    Introduced the NoPredicate class.

---
+++ src/DomainModel/Queries/NoPredicate.cs
@@ -0,0 +1,18 @@
+﻿namespace ArticleHarbor.DomainModel.Queries
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class NoPredicate : IPredicate
+    {
+        public string SqlText
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<IParameter> Parameters
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
