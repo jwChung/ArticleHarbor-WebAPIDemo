a3f6b19c96cb0ceae025337f0454101ae7eaae24
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 18:42:56 +0900

    Introduced the ArticleQueryViewModel class.

---
+++ src/WebApiPresentationModel/Models/ArticleQueryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel.Models
+{
+    using System;
+    using DomainModel.Queries;
+
+    public class ArticleQueryViewModel : ISqlQueryable
+    {
+        public ISqlQuery ProvideQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
