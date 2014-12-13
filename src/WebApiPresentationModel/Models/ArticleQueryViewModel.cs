52b5e60de5e5f49c2c59b3baa8607c47fcf85924
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 18:58:41 +0900

    Implemented the Count property.

--- src/WebApiPresentationModel/Models/ArticleQueryViewModel.cs
+++ src/WebApiPresentationModel/Models/ArticleQueryViewModel.cs
@@ -1,10 +1,33 @@
 ﻿namespace ArticleHarbor.WebApiPresentationModel.Models
 {
     using System;
+    using System.Globalization;
     using DomainModel.Queries;
 
     public class ArticleQueryViewModel : ISqlQueryable
     {
+        private const int MaxCount = 50;
+        private int count = MaxCount;
+        
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+
+            set
+            {
+                if (value > MaxCount)
+                    throw new ArgumentException(string.Format(
+                        CultureInfo.CurrentCulture,
+                        "The count cannot exceed '{0}'.",
+                        MaxCount));
+
+                this.count = value;
+            }
+        }
+
         public ISqlQuery ProvideQuery()
         {
             throw new NotImplementedException();
