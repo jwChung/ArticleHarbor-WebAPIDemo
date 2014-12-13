2d2c08604f921bf6ef304568a98c6ae104885bef
Jin-Wook Chung <jwChung@hotmail.com>
2014-12-13 18:42:56 +0900

    Introduced the ArticleQueryViewModel class.

---
+++ test/WebApiPresentationModelUnitTest/Models/ArticleQueryViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ArticleHarbor.WebApiPresentationModel.Models
+{
+    using DomainModel.Queries;
+    using Xunit;
+
+    public class ArticleQueryViewModelTest : IdiomaticTest<ArticleQueryViewModel>
+    {
+        [Test]
+        public void SutIsSqlQueryable(ArticleQueryViewModel sut)
+        {
+            Assert.IsAssignableFrom<ISqlQueryable>(sut);
+        }
+    }
+}
