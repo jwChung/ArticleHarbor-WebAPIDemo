4436a9c49648ff19192b9d29582d5872410468f0
Jin-Wook Chung <jwChung@hotmail.com>
2015-02-12 13:28:34 +0900

    Addressed a test as sturcture inspection.

---
+++ test/WebsiteUnitTest/DependencyRegistrationsTest.cs
@@ -0,0 +1,33 @@
+﻿namespace ArticleHarbor.Website
+{
+    using System.Linq;
+    using ArticleHarbor.DomainModel.Commands;
+    using ArticleHarbor.WebApiPresentationModel.Controllers;
+    using DomainModel.Models;
+    using Jwc.Funz;
+    using Xunit;
+
+    public class DependencyRegistrationsTest
+    {
+        [Test]
+        public void DeleteCommandIsCorrectlyStructured(
+            DependencyRegistrations sut,
+            Container container)
+        {
+            container.Accept(sut);
+            var expected = new[]
+            {
+                typeof(DeleteConfirmableCommand),
+                typeof(DeleteKeywordsCommand),
+                typeof(DeleteBookmarksCommand),
+                typeof(DeleteCommand),
+            };
+
+            var controller = container.Resolve<ArticlesController>();
+
+            var compositeCommands = Assert.IsAssignableFrom<CompositeCommand<IModel>>(
+                controller.DeleteCommand);
+            Assert.Equal(expected, compositeCommands.Commands.Select(c => c.GetType()));
+        }
+    }
+}
---
+++ test/WebsiteUnitTest/TestAttribute.cs
@@ -0,0 +1,75 @@
+﻿namespace ArticleHarbor.Website
+{
+    using System.Security.Principal;
+    using Jwc.Experiment;
+    using Jwc.Experiment.AutoFixture;
+    using Jwc.Experiment.Xunit;
+    using Jwc.Funz;
+    using Moq;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoMoq;
+
+    public class TestAttribute : TestBaseAttribute
+    {
+        private readonly RunOn runOn;
+
+        public TestAttribute()
+            : this(RunOn.Local)
+        {
+        }
+
+        public TestAttribute(RunOn runOn)
+        {
+            this.runOn = runOn;
+        }
+
+        public RunOn RunOn
+        {
+            get { return this.runOn; }
+        }
+
+        public override string Skip
+        {
+            get
+            {
+#if !CI
+                if (base.Skip == null && this.runOn == RunOn.CI)
+                    return "Run this test only on CI server.";
+#else
+            if (base.Skip == null && this.runOn == RunOn.Local)
+                return "Run this test only on Local machine.";
+#endif
+
+                return base.Skip;
+            }
+
+            set
+            {
+                base.Skip = value;
+            }
+        }
+
+        protected override ITestFixture Create(ITestMethodContext context)
+        {
+            var customization = new CompositeCustomization(
+                new AutoMoqCustomization(),
+                new ContainerCustomization(),
+                new TestParametersCustomization(context.ActualMethod.GetParameters()));
+
+            return new TestFixture(new Fixture().Customize(customization));
+        }
+
+        private class ContainerCustomization : ICustomization
+        {
+            public void Customize(IFixture fixture)
+            {
+                fixture.Register(() =>
+                {
+                    var container = new Container();
+                    container.Register(c => fixture.Create<IPrincipal>());
+                    return container;
+                });
+            }
+        }
+    }
+}
